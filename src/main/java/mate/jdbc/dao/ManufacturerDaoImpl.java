b06ab87c61bbc3fd40a6d27cad2adaea2b29e088
Vlad-Andr <andreev.v99@gmail.com>
2021-05-12 11:15:36 +0300

    fixed tables naming

--- src/main/java/mate/jdbc/dao/ManufacturerDaoImpl.java
+++ src/main/java/mate/jdbc/dao/ManufacturerDaoImpl.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import mate.jdbc.connection.ConnectionUtil;
-import mate.jdbc.exception.RecordDoesNotExistException;
+import mate.jdbc.exception.DataProcessingException;
 import mate.jdbc.lib.Dao;
 import mate.jdbc.model.Manufacturer;
 
@@ -17,7 +17,7 @@ import mate.jdbc.model.Manufacturer;
 public class ManufacturerDaoImpl implements ManufacturerDao {
     @Override
     public Manufacturer create(Manufacturer manufacturer) {
-        String insertQuery = "INSERT INTO manufacturers (manufacturer_name, manufacturer_country)"
+        String insertQuery = "INSERT INTO manufacturers (name, country)"
                 + " VALUES (?, ?)";
         try (Connection connection = ConnectionUtil.getConnection();
                 PreparedStatement createManufactureStatement = connection
@@ -31,34 +31,30 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
                 manufacturer.setId(id);
             }
         } catch (SQLException e) {
-            throw new RuntimeException("Can't insert new manufacture to DB", e);
+            throw new DataProcessingException("Can't insert new manufacture to DB", e);
         }
         return manufacturer;
     }
 
     @Override
     public Optional<Manufacturer> get(Long id) {
-        String selectQuery = "SELECT * FROM manufacturers WHERE manufacturer_id = ? "
+        String selectQuery = "SELECT * FROM manufacturers WHERE id = ? "
                 + "AND is_deleted <> true;";
         Manufacturer manufacturer = new Manufacturer();
         try (Connection connection = ConnectionUtil.getConnection();
                 PreparedStatement getManufacturer = connection.prepareStatement(selectQuery)) {
-            if (isRecordExistInDB(id) > 0) {
-                getManufacturer.setLong(1, id);
-                ResultSet resultSet = getManufacturer
-                        .executeQuery();
-                if (!resultSet.isBeforeFirst()) {
-                    return Optional.empty();
-                } else {
-                    while (resultSet.next()) {
-                        manufacturer = getResultSet(resultSet);
-                    }
-                }
+            getManufacturer.setLong(1, id);
+            ResultSet resultSet = getManufacturer
+                    .executeQuery();
+            if (!resultSet.isBeforeFirst()) {
+                return Optional.empty();
             } else {
-                throw new RecordDoesNotExistException(id);
+                while (resultSet.next()) {
+                    manufacturer = getResultSet(resultSet);
+                }
             }
         } catch (SQLException e) {
-            throw new RuntimeException("Can't execute get", e);
+            throw new DataProcessingException("Can't execute get", e);
         }
         return Optional.of(manufacturer);
     }
@@ -76,27 +72,23 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
                 manufacturerList.add(newManufacturer);
             }
         } catch (SQLException e) {
-            throw new RuntimeException("Can't execute get all", e);
+            throw new DataProcessingException("Can't execute get all", e);
         }
         return manufacturerList;
     }
 
     @Override
     public Manufacturer update(Manufacturer manufacturer) {
-        String updateQuery = "UPDATE manufacturers SET manufacturer_name = ?, "
-                + "manufacturer_country = ? WHERE manufacturer_id = ?;";
+        String updateQuery = "UPDATE manufacturers SET name = ?, "
+                + "country = ? WHERE id = ?;";
         try (Connection connection = ConnectionUtil.getConnection();
                 PreparedStatement updateStatement = connection.prepareStatement(updateQuery)) {
-            if (isRecordExistInDB(manufacturer.getId()) > 0) {
-                updateStatement.setLong(3, manufacturer.getId());
-                updateStatement.setString(1, manufacturer.getName());
-                updateStatement.setString(2, manufacturer.getCountry());
-                updateStatement.executeUpdate();
-            } else {
-                throw new RecordDoesNotExistException(manufacturer.getId());
-            }
+            updateStatement.setLong(3, manufacturer.getId());
+            updateStatement.setString(1, manufacturer.getName());
+            updateStatement.setString(2, manufacturer.getCountry());
+            updateStatement.executeUpdate();
         } catch (SQLException e) {
-            throw new RuntimeException("Can't update record with id: "
+            throw new DataProcessingException("Can't update record with id: "
                     + manufacturer.getId(), e);
         }
         return manufacturer;
@@ -105,60 +97,28 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
     @Override
     public boolean delete(Long id) {
         String deleteRequest = "UPDATE manufacturers SET is_deleted = true "
-                + "WHERE manufacturer_id = ?";
+                + "WHERE id = ?";
         try (Connection connection = ConnectionUtil.getConnection();
                 PreparedStatement deleteManufacturer = connection.prepareStatement(deleteRequest)) {
-            if (isRecordExistInDB(id) > 0) {
-                deleteManufacturer.setLong(1, id);
-                return deleteManufacturer.executeUpdate() >= 1;
-            } else {
-                throw new RecordDoesNotExistException(id);
-            }
+            deleteManufacturer.setLong(1, id);
+            return deleteManufacturer.executeUpdate() >= 1;
         } catch (SQLException e) {
-            throw new RuntimeException("Can't delete record with id: " + id, e);
+            throw new DataProcessingException("Can't delete record with id: " + id, e);
         }
-    }
-
-    private int isRecordExistInDB(Long id) {
-        List<Manufacturer> allRecords = getAllEvenDeleted();
-        int count = 0;
-        for (Manufacturer record : allRecords) {
-            if (record.getId().equals(id)) {
-                count++;
-                break;
-            }
-        }
-        return count;
-    }
-
-    private List<Manufacturer> getAllEvenDeleted() {
-        List<Manufacturer> allList = new ArrayList<>();
-        String selectQuery = "SELECT * FROM manufacturers";
-        try (Connection connection = ConnectionUtil.getConnection();
-                Statement getAllManufacturers = connection.createStatement()) {
-            ResultSet resultSet = getAllManufacturers.executeQuery(selectQuery);
-            while (resultSet.next()) {
-                Manufacturer newManufacturer = getResultSet(resultSet);
-                allList.add(newManufacturer);
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException("Can't execute get all even deleted", e);
-        }
-        return allList;
     }
 
     private Manufacturer getResultSet(ResultSet resultSet) {
         try {
-            Long id = resultSet.getObject("manufacturer_id", Long.class);
-            String name = resultSet.getString("manufacturer_name");
-            String country = resultSet.getString("manufacturer_country");
+            Long id = resultSet.getObject("id", Long.class);
+            String name = resultSet.getString("name");
+            String country = resultSet.getString("country");
             Manufacturer manufacturer = new Manufacturer();
             manufacturer.setId(id);
             manufacturer.setName(name);
             manufacturer.setCountry(country);
             return manufacturer;
         } catch (SQLException e) {
-            throw new RuntimeException("Can't execute get all", e);
+            throw new DataProcessingException("Can't make manufacturer from result set", e);
         }
     }
 }
--- src/main/java/mate/jdbc/exception/RecordDoesNotExistException.java
+++
@@ -1,7 +0,0 @@
-package mate.jdbc.exception;
-
-public class RecordDoesNotExistException extends RuntimeException {
-    public RecordDoesNotExistException(Long id) {
-        super("Record with id: " + id + " doesn't exist");
-    }
-}
