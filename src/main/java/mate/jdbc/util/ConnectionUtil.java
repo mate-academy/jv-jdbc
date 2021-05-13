c5d06cd6ee91833f207f21f142eb7903af34171c
Yevhenii Denysenko <iedenysenko@gmail.com>
2021-05-13 16:53:02 +0300

    add constants

--- src/main/java/mate/jdbc/util/ConnectionUtil.java
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -6,9 +6,14 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionUtil {
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String USER = "root";
+    private static final String PASSWORD = "123456";
+    private static final String URL = "jdbc:mysql://localhost:3306/manufacturer_db";
+
     static {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName(DRIVER);
         } catch (ClassNotFoundException e) {
             throw new RuntimeException("Can't load JDBC driver for MySQL", e);
         }
@@ -17,10 +22,9 @@ public class ConnectionUtil {
     public static Connection getConnection() {
         try {
             Properties dbProperties = new Properties();
-            dbProperties.put("user", "root");
-            dbProperties.put("password", "123456");
-            return DriverManager.getConnection("jdbc:mysql://localhost:3306/manufacturer_db",
-                    dbProperties);
+            dbProperties.put("user", USER);
+            dbProperties.put("password", PASSWORD);
+            return DriverManager.getConnection(URL, dbProperties);
         } catch (SQLException throwables) {
             throw new RuntimeException("Can't create connection to DB", throwables);
         }
