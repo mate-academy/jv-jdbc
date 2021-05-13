55152f0c96a5012fae6231e740ad6c0750e60c1e
Никита Фоминых <nikitafominyh@MacBook-Pro-Nikita.local>
2021-05-13 12:58:17 +0300

    travis done

--- src/main/java/mate/jdbc/util/ConnectionUtil.java
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -8,7 +8,7 @@ import java.util.Properties;
 public class ConnectionUtil {
     private static final String USER_NAME = "root";
     private static final String USER_PASSWORD = "RwMlQrQf";
-    public static final String URL = "jdbc:mysql://localhost:3306/library_db";
+    private static final String URL = "jdbc:mysql://localhost:3306/library_db";
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
 
     static {
