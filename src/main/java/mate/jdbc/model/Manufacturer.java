7dbdb45d5974cc031e966e02cfddbddc0aede463
docer <vlasovengineer@gmail.com>
2021-07-12 20:26:44 +0300

    create new model Manufacturer

---
+++ src/main/java/mate/jdbc/model/Manufacturer.java
@@ -0,0 +1,31 @@
+package mate.jdbc.model;
+
+public class Manufacturer {
+    private int id;
+    private String name;
+    private String country;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
