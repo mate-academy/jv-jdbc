30e4f84923a7cedb63f91645fe384e7b691569e4
Denis <deniskichan@gmail.com>
2021-07-12 19:52:15 +0300

    implemented dao level and its methods

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,25 @@
 package mate.jdbc;
 
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Manufacturer;
+
 public class Main {
     public static void main(String[] args) {
-
+        Injector injector = Injector.getInstance("mate.jdbc");
+        ManufacturerDao manufacturerDao = (ManufacturerDao)
+                injector.getInstance(ManufacturerDao.class);
+        Manufacturer cokeManufacturer = new Manufacturer("Coke", "USA");
+        Manufacturer fordManufacturer = new Manufacturer("Ford", "USA");
+        Manufacturer appleManufacturer = new Manufacturer("Apple", "USA");
+        manufacturerDao.create(cokeManufacturer);
+        manufacturerDao.create(fordManufacturer);
+        manufacturerDao.create(appleManufacturer);
+        Manufacturer samsungManufacturer = new Manufacturer("Samsung", "China");
+        samsungManufacturer.setId(20L);
+        manufacturerDao.update(samsungManufacturer);
+        manufacturerDao.get(18L);
+        manufacturerDao.delete(2L);
+        manufacturerDao.getAll().forEach(System.out::println);
     }
 }
