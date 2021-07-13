9f9d684781796efec127e72a30b3932e4c92d1d2
Veronika-Alekseienko <alekseienko.n@gmail.com>
2021-07-13 09:37:35 +0300

    added solution

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,30 @@
 package mate.jdbc;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Manufacturer;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
+    public static void main(String[] args) {
+        ManufacturerDao manufacturerDao = (ManufacturerDao) injector.getInstance(ManufacturerDao.class);
+        Manufacturer bmwCarManufacturer = new Manufacturer("BMW", "Germany");
+        Manufacturer fordCarManufacturer = new Manufacturer("Ford", "USA");
+        Manufacturer volkswagenCarManufacturer = new Manufacturer("Volkswagen", "Germany");
+
+        Manufacturer newBmwCarManufacturer = manufacturerDao.create(bmwCarManufacturer);
+        Manufacturer newFordCarManufacturer = manufacturerDao.create(fordCarManufacturer);
+        Manufacturer newVolkswagenCarManufacturer = manufacturerDao.create(volkswagenCarManufacturer);
+
+        System.out.println(manufacturerDao.update(newBmwCarManufacturer));
+        System.out.println(manufacturerDao.update(newVolkswagenCarManufacturer));
+        System.out.println(manufacturerDao.update(newFordCarManufacturer));
+
+        System.out.println(manufacturerDao.get(1L));
+        System.out.println(manufacturerDao.get(2L));
+        System.out.println(manufacturerDao.get(3L));
+
+        manufacturerDao.getAll().forEach(System.out::println);
     }
 }
---
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -0,0 +1,34 @@
+package mate.jdbc.util;
+
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionUtil {
+    private static final String URL;
+    private static final String NAME;
+    private static final String PASSWORD;
+
+    static {
+        try {
+            Properties dbProperties = new Properties();
+            dbProperties.load(new FileInputStream("src/main/resources/application.properties"));
+            Class.forName(dbProperties.getProperty("db.driver"));
+            URL = dbProperties.getProperty("db.url");
+            NAME = dbProperties.getProperty("db.name");
+            PASSWORD = dbProperties.getProperty("db.password");
+        } catch (Exception e) {
+            throw new RuntimeException("Can't load JDBC driver", e);
+        }
+    }
+
+    public static Connection getConnection() {
+        try {
+           return DriverManager.getConnection(URL, NAME, PASSWORD);
+        } catch (SQLException throwables) {
+            throw new RuntimeException("Can't create connection to DB", throwables);
+        }
+    }
+}
