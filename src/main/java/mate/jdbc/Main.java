2fa4e2967386b8d30d9bc3f633b09babd2c8a8f0
Basilius the Third <champfockes@gmail.com>
2021-07-13 15:47:39 +0300

    reworked sout messages

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -8,14 +8,19 @@ public class Main {
     public static void main(String[] args) {
         ManufacturerDao manufacturerDao =
                 (ManufacturerDao) injector.getInstance(ManufacturerDao.class);
+        System.out.println("Table: ");
+        manufacturerDao.getAll().forEach(System.out::println);
+        System.out.println("-------------------------------");
         Manufacturer bridgestone = new Manufacturer("Bridgestone", "Japan");
+        System.out.println("Created record: " + manufacturerDao.create(bridgestone));
         Manufacturer cooper = new Manufacturer(4L, "Cooper", "USA");
-
-        System.out.println(manufacturerDao.getAll());
-        System.out.println(manufacturerDao.create(bridgestone));
-        System.out.println(manufacturerDao.update(cooper));
-        System.out.println(manufacturerDao.get(cooper.getId()));
-        System.out.println(manufacturerDao.delete(cooper.getId()));
-        System.out.println(manufacturerDao.getAll());
+        System.out.println("Updated record: " + manufacturerDao.update(cooper));
+        System.out.println("Get by id " + cooper.getId()
+                + " record: " + manufacturerDao.get(cooper.getId()));
+        System.out.println("Record " + cooper.getName()
+                + " is deleted: " + manufacturerDao.delete(cooper.getId()));
+        System.out.println("-------------------------------");
+        System.out.println("Table: ");
+        manufacturerDao.getAll().forEach(System.out::println);
     }
 }
