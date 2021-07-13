558bf41d04ce4b1d9bf72a73893e120a7d619d3b
olesiasmitiukh <d.olleska@gmail.com>
2021-07-13 10:19:14 +0300

    Revert "added solution"
    This reverts commit 09bea506

--- src/main/java/mate/jdbc/exception/DataProcessingException.java
+++
@@ -1,8 +0,0 @@
-package mate.jdbc.exception;
-
-public class DataProcessingException extends RuntimeException {
-
-    public DataProcessingException(String message, Throwable ex) {
-        super(message, ex);
-    }
-}
--- src/main/java/resources/init_db.sql
+++
@@ -1,8 +0,0 @@
-CREATE SCHEMA `manufacturers_db` DEFAULT CHARACTER SET utf8 ;
-CREATE TABLE `manufacturers_db`.`manufacturers` (
-  `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(225) NULL DEFAULT NULL,
-  `country` VARCHAR(225) NULL DEFAULT NULL,
-  `is_deleted` TINYINT NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`));
-  SET GLOBAL time_zone = '+3:00';
