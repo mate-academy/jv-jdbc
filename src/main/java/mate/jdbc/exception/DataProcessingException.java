149ef0b94c244fd181fae2b654ca641501015103
PashaTerletskyi <p.terletsky@gmail.com>
2021-07-13 16:53:49 +0300

    Fixed checkstyle

--- src/main/java/mate/jdbc/exception/DataProcessingException.java
+++ src/main/java/mate/jdbc/exception/DataProcessingException.java
@@ -1,7 +1,6 @@
 package mate.jdbc.exception;
 
 public class DataProcessingException extends RuntimeException {
-
     public DataProcessingException(String message, Throwable throwable) {
         super(message, throwable);
     }
