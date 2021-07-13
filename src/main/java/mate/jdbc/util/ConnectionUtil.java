09bea506f786fb4c1844073132899438c203fa7f
olesiasmitiukh <d.olleska@gmail.com>
2021-07-13 10:15:07 +0300

    added solution

---
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -0,0 +1,29 @@
+package mate.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionUtil {
+    private static final String URL = "jdbc:mysql://localhost:3306/manufacturers_db";
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Can't load JDBC driver for MySQL", e);
+        }
+    }
+
+    public static Connection getConnection() {
+        try {
+            Properties dbProperties = new Properties();
+            dbProperties.put("user", "root");
+            dbProperties.put("password", "99887766");
+            return DriverManager.getConnection(URL, dbProperties);
+        } catch (SQLException throwable) {
+            throw new RuntimeException("Can't create connection to DB", throwable);
+        }
+    }
+}
