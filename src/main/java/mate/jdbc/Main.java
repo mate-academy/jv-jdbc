2ec76bc3d8b08081054f5785bca689f6b1879776
Борис Скворцов <dhakmar@gmail.com>
2021-07-13 08:06:42 +0300

    init commit

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,21 @@
 package mate.jdbc;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Manufacturer;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
+    public static void main(String[] args) {
+        ManufacturerDao manufacturerDao =
+                (ManufacturerDao) injector.getInstance(ManufacturerDao.class);
+        Manufacturer manufacturer = new Manufacturer("Saturn", "China");
+        Manufacturer m2 = manufacturerDao.get(2L).orElseThrow();
+        m2.setCountry("Ukraine");
+        manufacturerDao.update(m2);
+        for (Manufacturer manufacturer1 : manufacturerDao.getAll()) {
+            System.out.println(manufacturer1);
+        }
     }
 }
