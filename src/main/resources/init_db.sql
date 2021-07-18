4c4adeac14704724ad1a5fefe1984a5f33a22e8f
Tetiana Chupika <mtv1301@gmail.com>
2021-07-18 12:50:24 +0300

    changed int to bigint in init file

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -2,10 +2,9 @@ CREATE DATABASE 'taxi_db' DEFAULT CHARACTER SET utf8 DEFAULT ENCRYPTION= 'N';
 
 create table manufactures
 (
-    id int auto_increment
+    id bigint auto_increment
         primary key,
     name varchar(255) null,
     country varchar(255) null,
     is_deleted bigint null
 );
-
