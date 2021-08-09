dae19c69b3601de06ccabee337d90e06710570ea
Dana Khromenko <danadiadius@gmail.com>
2021-08-09 16:39:03 +0300

    Replaced "stream().forEach()" with "forEach()".

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -13,19 +13,19 @@ public class Main {
         manufacturerDao.create(new Manufacturer("Porsche", "Germany"));
 
         System.out.println("At first all manufacturers are:");
-        manufacturerDao.getAll().stream().forEach(System.out::println);
+        manufacturerDao.getAll().forEach(System.out::println);
 
         Manufacturer currentManufacturer = manufacturerDao.getAll().get(1);
         currentManufacturer.setName("Ferrari");
         manufacturerDao.update(currentManufacturer);
 
         System.out.println("After Ferrari updating manufacturers are:");
-        manufacturerDao.getAll().stream().forEach(System.out::println);
+        manufacturerDao.getAll().forEach(System.out::println);
 
         manufacturerDao.delete(2L);
 
         System.out.println("After Ferrari deleting manufacturers are:");
-        manufacturerDao.getAll().stream().forEach(System.out::println);
+        manufacturerDao.getAll().forEach(System.out::println);
 
         System.out.println("The manufacturer with index = 2 is:\n" + manufacturerDao.get(2L));
     }
