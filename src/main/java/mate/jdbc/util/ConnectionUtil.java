be70420db96a562df707ec8db40559024bdac902
Viktor Yukhno <pingvinomania@gmail.com>
2021-09-20 12:31:22 +0300

    added fix after student's review

--- src/main/java/mate/jdbc/util/ConnectionUtil.java
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -6,9 +6,14 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionUtil {
+    private static final String USER_NAME = "root";
+    private static final String USER_PASSWORD = "0258";
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String PATH_TO_DB = "jdbc:mysql://localhost:3306/manufacturers_db";
+
     static {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName(DRIVER);
         } catch (ClassNotFoundException e) {
             throw new RuntimeException("Can't load JDBC driver for MySQL", e);
         }
@@ -17,10 +22,9 @@ public class ConnectionUtil {
     public static Connection getConnection() {
         try {
             Properties dbProperties = new Properties();
-            dbProperties.put("user", "root");
-            dbProperties.put("password", "0258");
-            return DriverManager.getConnection("jdbc:mysql://localhost:3306/manufacturers_db",
-                    dbProperties);
+            dbProperties.put("user", USER_NAME);
+            dbProperties.put("password", USER_PASSWORD);
+            return DriverManager.getConnection(PATH_TO_DB, dbProperties);
         } catch (SQLException throwables) {
             throw new RuntimeException("Can't create connection to db", throwables);
         }
