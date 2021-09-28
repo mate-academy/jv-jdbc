968d2dd4136def804b8f884abecc8ad1a7cde5b2
Vladyslav Gural <vladgural@gmail.com>
2021-09-28 14:04:59 +0300

    created dao model util resource packages

---
+++ src/main/java/mate/jdbc/util/RemoteConnectionUtil.java
@@ -0,0 +1,27 @@
+package mate.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class RemoteConnectionUtil {
+    static {
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Can't load JDBC driver for MariaDB", e);
+        }
+    }
+    public static Connection getConnect() {
+        try {
+            Properties dbProperties = new Properties();
+            dbProperties.put("user", "root");
+            dbProperties.put("password", "MariaDB#10");
+            return DriverManager.getConnection("jdbc:mariadb://192.168.1.40:3306/library_db",
+                    dbProperties);
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't create connection to DB", e);
+        }
+    }
+}
