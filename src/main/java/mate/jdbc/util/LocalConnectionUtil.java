20195c7423aa70842cd58e291b1a8091a3ed703c
Vladyslav Gural <vladgural@gmail.com>
2021-09-29 11:45:06 +0300

    created constant variables

--- src/main/java/mate/jdbc/util/LocalConnectionUtil.java
+++ src/main/java/mate/jdbc/util/LocalConnectionUtil.java
@@ -6,9 +6,15 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class LocalConnectionUtil implements ConnectionUtil {
+    public static final String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
+    public static final String URL = "jdbc:mysql://localhost:3306/taxi_service_db";
+    public static final String USER = "user";
+    public static final String USER_NAME = "root";
+    public static final String PASSWORD = "password";
+    public static final String PASSWORD_NAME = "root";
     static {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName(DRIVER_NAME);
         } catch (ClassNotFoundException e) {
             throw new RuntimeException("Can't load JDBC driver for MySQL", e);
         }
@@ -17,10 +23,9 @@ public class LocalConnectionUtil implements ConnectionUtil {
     public Connection getConnect() {
         try {
             Properties dbProperties = new Properties();
-            dbProperties.put("user", "root");
-            dbProperties.put("password", "root");
-            return DriverManager.getConnection("jdbc:mysql://localhost:3306/taxi_service_db",
-                    dbProperties);
+            dbProperties.put(USER, USER_NAME);
+            dbProperties.put(PASSWORD, PASSWORD_NAME);
+            return DriverManager.getConnection(URL, dbProperties);
         } catch (SQLException e) {
             throw new RuntimeException("Can't create connection to DB", e);
         }
--- src/main/java/mate/jdbc/util/RemoteConnectionUtil.java
+++ src/main/java/mate/jdbc/util/RemoteConnectionUtil.java
@@ -6,9 +6,15 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class RemoteConnectionUtil implements ConnectionUtil {
+    public static final String DRIVER_NAME = "org.mariadb.jdbc.Driver";
+    public static final String URL = "jdbc:mariadb://192.168.1.40:3306/taxi_service_db";
+    public static final String USER = "user";
+    public static final String USER_NAME = "root";
+    public static final String PASSWORD = "password";
+    public static final String PASSWORD_NAME = "MariaDB#10";
     static {
         try {
-            Class.forName("org.mariadb.jdbc.Driver");
+            Class.forName(DRIVER_NAME);
         } catch (ClassNotFoundException e) {
             throw new RuntimeException("Can't load JDBC driver for MariaDB", e);
         }
@@ -17,10 +23,9 @@ public class RemoteConnectionUtil implements ConnectionUtil {
     public Connection getConnect() {
         try {
             Properties dbProperties = new Properties();
-            dbProperties.put("user", "root");
-            dbProperties.put("password", "MariaDB#10");
-            return DriverManager.getConnection("jdbc:mariadb://192.168.1.40:3306/taxi_service_db",
-                    dbProperties);
+            dbProperties.put(USER, USER_NAME);
+            dbProperties.put(PASSWORD, PASSWORD_NAME);
+            return DriverManager.getConnection(URL, dbProperties);
         } catch (SQLException e) {
             throw new RuntimeException("Can't create connection to DB", e);
         }
