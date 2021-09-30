abd9692d3bafceedcd5558097717cb5c432470ca
Denys Smei <denys.smei@artcodevision.com>
2021-09-30 18:46:55 +0300

    Done remarks from student review

--- src/main/java/mate/jdbc/models/Manufacturer.java
+++ src/main/java/mate/jdbc/models/Manufacturer.java
@@ -32,14 +32,9 @@ public class Manufacturer {
     @Override
     public String toString() {
         return "Manufacturer{"
-                + "id="
-                + id
-                + ", name='"
-                + name
-                + '\''
-                + ", country='"
-                + country
-                + '\''
+                + "id=" + id
+                + ", name='" + name + '\''
+                + ", country='" + country + '\''
                 + '}';
     }
 }
