4c31818dd4439173e464f90935ed56edf48a98cf
Rostyslav Fostyk <rostyslavfostykjob@gmail.com>
2021-10-03 20:00:38 +0300

    implemented ManufacturedDao, Manufactured model, ConnectionUtil

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,30 @@
 package mate.jdbc;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Manufacturer;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
+    public static void main(String[] args) {
+        ManufacturerDao manufacturerDao = (ManufacturerDao) injector
+                .getInstance(ManufacturerDao.class);
+
+        Manufacturer manufacturer = new Manufacturer();
+        manufacturer.setCountry("Germany");
+        manufacturer.setName("BMW");
+
+        Manufacturer updateManufacturer = new Manufacturer();
+        updateManufacturer.setCountry("England");
+        updateManufacturer.setName("Rolls");
+        updateManufacturer.setId(3L);
+
+        System.out.println(manufacturerDao.update(updateManufacturer));
+
+        //System.out.println(manufacturerDao.create(manufacturer));
+        //System.out.println(manufacturerDao.delete(4L));
+        System.out.println(manufacturerDao.getAll());
+        //System.out.println(manufacturerDao.get(1L));
     }
 }
