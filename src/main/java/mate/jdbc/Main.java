db1c3f538e84860a58657efb97fda9c924c316f5
Konstantin <www.kosta.kon@gmail.com>
2021-11-29 11:40:20 +0200

    Connector usage was changed.

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,8 +1,6 @@
 package mate.jdbc;
 
-import java.lang.reflect.Field;
 import mate.jdbc.dao.manufacturer.ManufacturerDao;
-import mate.jdbc.dao.manufacturer.ManufacturerDaoImpl;
 import mate.jdbc.lib.Injector;
 import mate.jdbc.models.User;
 import mate.jdbc.models.db.models.Manufacturer;
@@ -10,13 +8,17 @@ import mate.jdbc.services.connection.Connector;
 import mate.jdbc.services.connection.ConnectorImpl;
 
 public class Main {
+    public static final Connector connector;
     private static final Injector injector = Injector.getInstance("mate.jdbc.dao");
 
+    static {
+        User user = new User("testUser", "12345678");
+        connector = new ConnectorImpl(user);
+    }
+
     public static void main(String[] args) {
-        Connector connector = new ConnectorImpl(new User("testUser", "12345678"));
         ManufacturerDao manufacturerDao =
                 (ManufacturerDao) injector.getInstance(ManufacturerDao.class);
-        initializeConnectorInDao(manufacturerDao, connector);
         Manufacturer manufacturer = new Manufacturer();
         manufacturer.setName("volkswagen");
         manufacturer.setCountry("Ukraine");
@@ -26,18 +28,5 @@ public class Main {
         manufacturer.setName("Germany");
         System.out.println(manufacturerDao.update(manufacturer));
         manufacturerDao.delete(manufacturer.getId());
-    }
-
-    public static void initializeConnectorInDao(ManufacturerDao dao, Connector connector) {
-        try {
-            for (Field field : ManufacturerDaoImpl.class.getDeclaredFields()) {
-                if (field.getName().equals("connector")) {
-                    field.setAccessible(true);
-                    field.set(dao, connector);
-                }
-            }
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException("Can't initialize field.");
-        }
     }
 }
--- src/main/java/mate/jdbc/dao/manufacturer/ManufacturerDaoImpl.java
+++ src/main/java/mate/jdbc/dao/manufacturer/ManufacturerDaoImpl.java
@@ -7,22 +7,21 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
 import java.util.Optional;
+import mate.jdbc.Main;
 import mate.jdbc.exceptions.DataProcessingException;
 import mate.jdbc.lib.Dao;
 import mate.jdbc.models.db.models.Manufacturer;
-import mate.jdbc.services.connection.Connector;
 import mate.jdbc.services.parsers.ManufacturerParser;
 
 @Dao
 public class ManufacturerDaoImpl implements ManufacturerDao {
     private static final String TABLE_NAME = "manufactures";
-    private Connector connector;
 
     @Override
     public Manufacturer create(Manufacturer dbModel) {
         String query = String.format("INSERT INTO `%s` "
                 + "(`name`, `country`) VALUES (?, ?);", TABLE_NAME);
-        try (Connection connection = connector.connect();
+        try (Connection connection = Main.connector.connect();
                 PreparedStatement statement
                         = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
             statement.setString(1, dbModel.getName());
@@ -45,7 +44,7 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
         String getQuery = String.format(
                 "SELECT * FROM %s "
                         + "WHERE id = ?;", TABLE_NAME);
-        try (Connection connection = connector.connect();
+        try (Connection connection = Main.connector.connect();
                 PreparedStatement statement = connection.prepareStatement(getQuery)) {
             statement.setString(1, String.valueOf(id));
             ResultSet resultSet = statement.executeQuery();
@@ -62,7 +61,7 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
         String query = String.format(
                 "SELECT * FROM %s "
                 + "WHERE is_deleted = 0;", TABLE_NAME);
-        try (Connection connection = connector.connect();
+        try (Connection connection = Main.connector.connect();
                 Statement statement = connection.createStatement()) {
             return new ManufacturerParser().parse(statement.executeQuery(query));
         } catch (SQLException e) {
@@ -76,7 +75,7 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
         String deleteQuery = String.format(
                 "UPDATE %s SET name = ?, country = ? "
                 + "WHERE id = ?;", TABLE_NAME);
-        try (Connection connection = connector.connect();
+        try (Connection connection = Main.connector.connect();
                 PreparedStatement softDeleteStatement = connection.prepareStatement(deleteQuery)) {
             softDeleteStatement.setString(1, dbModel.getName());
             softDeleteStatement.setString(2, dbModel.getCountry());
@@ -94,7 +93,7 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
         String deleteQuery = String.format(
                 "UPDATE %s SET is_deleted = 1 "
                         + "WHERE id = ?;", TABLE_NAME);
-        try (Connection connection = connector.connect();
+        try (Connection connection = Main.connector.connect();
                 PreparedStatement softDeleteStatement = connection.prepareStatement(deleteQuery)) {
             softDeleteStatement.setString(1, String.valueOf(id));
             int updatedRows = softDeleteStatement.executeUpdate();
