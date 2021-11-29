7b495ae2b4bbdb718e0a02c10d609b9bd98ce5a0
Anton Volskyi <antonvolskyi1234@gmail.com>
2021-11-29 20:05:16 +0200

    connect to DB was success

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,9 @@
 package mate.jdbc;
 
+import mate.jdbc.util.ConnectionToDbUtil;
+
 public class Main {
     public static void main(String[] args) {
-
+        ConnectionToDbUtil.getConnection();
     }
 }
---
+++ src/main/java/mate/jdbc/util/ConnectionToDbUtil.java
@@ -0,0 +1,29 @@
+package mate.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionToDbUtil {
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Failed load JDBC driver for MySQL", e);
+        }
+    }
+
+    public static Connection getConnection() {
+        Properties dbProperties = new Properties();
+        dbProperties.put("user", "root");
+        dbProperties.put("password", "20010226");
+
+        try {
+            return DriverManager.getConnection("jdbc:mysql://localhost:3306/taxi_company",
+                    dbProperties);
+        } catch (SQLException throwables) {
+            throw new RuntimeException("Can`t create connection to DB", throwables);
+        }
+    }
+}
