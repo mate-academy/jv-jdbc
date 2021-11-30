ca24f24cb50998be021ac9064876b43cae926243
Pavlo_Martsiniv <imitator2010@gmail.com>
2021-11-30 16:26:17 +0200

    My solution v1.0

---
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -0,0 +1,33 @@
+package mate.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import mate.jdbc.exceptions.DataProcessingException;
+
+public class ConnectionUtil {
+    public static final String URL = "jdbc:mysql://localhost:3306/db_taxi_service";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "**************";
+    public static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    static {
+        try {
+            Class.forName(JDBC_DRIVER);
+        } catch (ClassNotFoundException e) {
+            throw new DataProcessingException("Can't load JDBC driver for MySQL", e);
+        }
+    }
+
+    public static Connection getConnection() {
+        try {
+            Properties dbProperties = new Properties();
+            dbProperties.put("user", USERNAME);
+            dbProperties.put("password", PASSWORD);
+            return DriverManager.getConnection(URL, dbProperties);
+        } catch (SQLException e) {
+            throw new DataProcessingException("Can't create connection to DB", e);
+        }
+    }
+}
