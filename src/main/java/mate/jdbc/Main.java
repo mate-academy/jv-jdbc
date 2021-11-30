21427dbd3b87299726ba4cece9d55fd3395ea477
Yaroslav Kavun <yka40znp@gmail.com>
2021-11-30 16:42:11 +0200

    created CRUD impl with jdbc connection and test it in main

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,21 @@
 package mate.jdbc;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Manufacturer;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
+    public static void main(String[] args) {
+        ManufacturerDao manufacturerDao =
+                (ManufacturerDao) injector.getInstance(ManufacturerDao.class);
+        System.out.println(manufacturerDao.get(7L));
+        System.out.println(manufacturerDao.delete(20L));
+        System.out.println(manufacturerDao.create(new Manufacturer("Tesla", "USA")));
+        System.out.println(manufacturerDao.update(new Manufacturer(8L, "Skoda", "Czech republic")));
+        System.out.println(manufacturerDao.create(new Manufacturer(1L,"Mercedes", "Germany")));
+        System.out.println(manufacturerDao.delete(1L));
+        manufacturerDao.getAll().forEach(System.out::println);
     }
 }
---
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -0,0 +1,33 @@
+package mate.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionUtil {
+    private static final String USER_NAME = "DentalRaid";
+    private static final String PASSWORD = "LittleBaldBruce4891";
+    private static final String LOCAL_HOST = "jdbc:mysql://localhost:3306/manufacturer.db";
+    private static final String TIME_ZONE = "?useUnicode=true&serverTimezone=UTC";
+    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    static {
+        try {
+            Class.forName(JDBC_DRIVER);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Can't load JDBC for MySql", e);
+        }
+    }
+
+    public static Connection getConnection() {
+        try {
+            Properties dbProperties = new Properties();
+            dbProperties.put("user", USER_NAME);
+            dbProperties.put("password", PASSWORD);
+            return DriverManager.getConnection(LOCAL_HOST + TIME_ZONE, dbProperties);
+        } catch (SQLException throwables) {
+            throw new RuntimeException("Can't create connection to DB", throwables);
+        }
+    }
+}
