9a1e84ca4859aed36553e90552cef23ea3310b35
Tyvoniuk Dmytro <dmytro.tyvoniuk@gmail.com>
2021-12-01 19:50:35 +0200

    some changes, part 2

--- src/main/java/mate/jdbc/util/ConnectionUtil.java
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -4,13 +4,14 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
+import mate.jdbc.exceptions.DataProcessingException;
 
 public class ConnectionUtil {
     static {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
-            throw new RuntimeException("Can't load JDBC driver for MySQL", e);
+            throw new DataProcessingException("Can't load JDBC driver for MySQL", e);
         }
     }
 
@@ -22,7 +23,7 @@ public class ConnectionUtil {
             return DriverManager.getConnection("jdbc:mysql://localhost:3306/manufacturers_db",
                     dbProperties);
         } catch (SQLException e) {
-            throw new RuntimeException("Can't create connection to DB", e);
+            throw new DataProcessingException("Can't create connection to DB", e);
         }
     }
 }
