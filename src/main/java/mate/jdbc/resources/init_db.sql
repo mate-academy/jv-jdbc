4ec730a63554f35161d534a73a6f903b3557bee1
Dany Nesterov <nesterov1200997@gmail.com>
2021-12-01 15:48:26 +0200

    final solution

--- src/main/java/mate/jdbc/resources/init_db.sql
+++ src/main/java/mate/jdbc/resources/init_db.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE `taxi_service_db` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE `taxi_service_db` DEFAULT CHARACTER SET utf8  DEFAULT ENCRYPTION='N';
 CREATE TABLE `manufacturers` (
                                  `id` bigint NOT NULL AUTO_INCREMENT,
                                  `name` varchar(100) DEFAULT NULL,
