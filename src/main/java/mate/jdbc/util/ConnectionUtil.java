e16434462d9ee3e0ab0e57301fd968e7b3b55cf5
Alexsis Moskovchuk <alekssiuss@gmail.com>
2021-12-01 09:52:42 +0200

    forget...

--- src/main/java/mate/jdbc/util/ConnectionUtil.java
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -10,7 +10,7 @@ public class ConnectionUtil {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
-            throw new RuntimeException("Can't load JDBC Driver for MySQL", e);
+            throw new RuntimeException("Can't load JDBC Driver for MySQL ", e);
         }
     }
 
@@ -22,7 +22,7 @@ public class ConnectionUtil {
             return DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/study_db", dbProps);
         } catch (SQLException e) {
-            throw new RuntimeException("Cant open connection to DB");
+            throw new RuntimeException("Cant open connection to DB ", e);
         }
     }
 }
