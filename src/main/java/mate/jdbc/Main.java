3fe05b478b329dca8ac36a8e54fabf2cc95ee5a3
Dzmitry Kozel <dmitrokoz88@gmail.com>
2021-12-01 11:19:20 +0200

    mentor requested changes made

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -5,7 +5,7 @@ import mate.jdbc.lib.Injector;
 import mate.jdbc.models.Manufacturer;
 
 public class Main {
-    private static final Injector injector = Injector.getInstance("mate.jdbc.dao");
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
 
     public static void main(String[] args) {
         ManufacturerDao manufacturerDao =
