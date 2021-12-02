d7e5ef6609d703ec3972d8297d259c1189fd6030
Vladyslav Chyver <lione2009lione@gmail.com>
2021-12-02 17:41:30 +0200

    mistakes was fixed

--- src/main/java/mate/jdbc/dao/ManufacturerDaoImpl.java
+++ src/main/java/mate/jdbc/dao/ManufacturerDaoImpl.java
@@ -18,9 +18,9 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
     @Override
     public Manufacturer create(Manufacturer manufacturer) {
         String insertRequest = "INSERT INTO manufacturers(name, country) VALUES(?, ?);";
-        try (Connection connection = ConnectionUtil.getConnection()) {
+        try (Connection connection = ConnectionUtil.getConnection();
             PreparedStatement createStatement = connection
-                    .prepareStatement(insertRequest, Statement.RETURN_GENERATED_KEYS);
+                    .prepareStatement(insertRequest, Statement.RETURN_GENERATED_KEYS)) {
             createStatement.setString(1, manufacturer.getName());
             createStatement.setString(2, manufacturer.getCountry());
             createStatement.executeUpdate();
@@ -40,8 +40,8 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
     public Optional<Manufacturer> get(Long id) {
         String query = "SELECT * FROM manufacturers WHERE id = ? AND is_deleted = 'FALSE'";
         Manufacturer result = null;
-        try (Connection connection = ConnectionUtil.getConnection()) {
-            PreparedStatement prepareStatement = connection.prepareStatement(query);
+        try (Connection connection = ConnectionUtil.getConnection();
+            PreparedStatement prepareStatement = connection.prepareStatement(query)) {
             prepareStatement.setLong(1, id);
             ResultSet resultSet = prepareStatement.executeQuery();
             if (resultSet.next()) {
@@ -75,25 +75,22 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
     @Override
     public Manufacturer update(Manufacturer manufacturer) {
         String query = "UPDATE manufacturers SET name = ? WHERE id = ? AND is_deleted = 'FALSE'";
-        try (Connection connection = ConnectionUtil.getConnection()) {
-            PreparedStatement prepareStatement = connection.prepareStatement(query);
+        try (Connection connection = ConnectionUtil.getConnection();
+            PreparedStatement prepareStatement = connection.prepareStatement(query)) {
             prepareStatement.setString(1, manufacturer.getName());
             prepareStatement.setLong(2, manufacturer.getId());
-            if (prepareStatement.executeUpdate() == 1) {
-                return manufacturer;
-            }
         } catch (SQLException e) {
             throw new DataProcessingException(
                     "Update operation from DB was crashed for Manufacturer " + manufacturer, e);
         }
-        return null;
+        return manufacturer;
     }
 
     @Override
     public boolean delete(Long id) {
         String query = "UPDATE manufacturers SET is_deleted = 'TRUE' WHERE id = ?";
-        try (Connection connection = ConnectionUtil.getConnection()) {
-            PreparedStatement prepareStatement = connection.prepareStatement(query);
+        try (Connection connection = ConnectionUtil.getConnection();
+            PreparedStatement prepareStatement = connection.prepareStatement(query)) {
             prepareStatement.setLong(1, id);
             return prepareStatement.executeUpdate() != 0;
         } catch (SQLException e) {
