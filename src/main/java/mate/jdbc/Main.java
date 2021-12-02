04185563b7a82233761d9eb14eb6e05996974ed5
Eugene-exe <zhekacauselove@gmail.com>
2021-12-02 10:00:28 +0200

    My solution

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,41 @@
 package mate.jdbc;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Manufacturer;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
+    public static void main(String[] args) {
+        ManufacturerDao manufacturerDao = (ManufacturerDao)
+                injector.getInstance(ManufacturerDao.class);
+
+        Manufacturer ferrari = new Manufacturer();
+        ferrari.setCountry("Italy");
+        ferrari.setName("Ferrari");
+        manufacturerDao.create(ferrari);
+
+        Manufacturer porsche = new Manufacturer();
+        porsche.setCountry("Germany");
+        porsche.setName("Porsche");
+        manufacturerDao.create(porsche);
+
+        Manufacturer koenigsegg = new Manufacturer();
+        koenigsegg.setCountry("Sweden");
+        koenigsegg.setName("Koenigsegg");
+        manufacturerDao.create(koenigsegg);
+
+        System.out.println(manufacturerDao.get(3L));
+        manufacturerDao.getAll().forEach(System.out::println);
+
+        porsche.setCountry("Ukraine");
+        manufacturerDao.update(porsche);
+
+        manufacturerDao.getAll().forEach(System.out::println);
+
+        manufacturerDao.delete(2L);
+
+        manufacturerDao.getAll().forEach(System.out::println);
     }
 }
