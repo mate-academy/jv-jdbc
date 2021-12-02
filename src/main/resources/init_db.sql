b42e5a2ea716d1c1c4f2a481f231878b14902ef0
Admin Oleksandr <maslo5@meta.ua>
2021-12-02 08:44:44 +0200

    fixed changes

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE `taxi_service_db`  DEFAULT CHARACTER SET utf8 */  DEFAULT ENCRYPTION='N';
+CREATE DATABASE `taxi_service_db`  DEFAULT CHARACTER SET utf8   DEFAULT ENCRYPTION='N';
 CREATE TABLE `manufacturers` (
                                  `id` bigint NOT NULL AUTO_INCREMENT,
                                  `name` varchar(255) DEFAULT NULL,
