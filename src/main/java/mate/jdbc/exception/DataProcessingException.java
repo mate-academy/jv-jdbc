9be4fb86b8232e46df7cb487b790891af16fb60d
Stanislav <stassonich@icloud.com>
2022-02-01 15:48:51 +0200

    Data base(begining)

---
+++ src/main/java/mate/jdbc/exception/DataProcessingException.java
@@ -0,0 +1,9 @@
+package mate.jdbc.exception;
+
+public class DataProcessingException extends RuntimeException {
+
+    public DataProcessingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
