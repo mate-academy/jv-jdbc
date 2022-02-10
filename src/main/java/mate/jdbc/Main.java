0a569342dd1eb2d9d922b22790897333c2aea0c3
Serhii Petrenko <Poshtasp76@gmail.com>
2022-02-10 22:38:20 +0200

    Added init_db.sql Added Manufacturer Added ConnectionUtil Added ManufacturerDaoImpl

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,25 @@
 package mate.jdbc;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.Optional;
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Manufacturer;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
+    public static void main(String[] args) {
+        ManufacturerDao manufacturerDao = (ManufacturerDao) injector
+                .getInstance(ManufacturerDao.class);
+        Manufacturer skoda = new Manufacturer("Skoda", "Czech Republic");
+        Manufacturer zazTavria = new Manufacturer("ZAZ - Tavria", "Ukraine");
+        Manufacturer ford = new Manufacturer("Ford", "USA");
+        manufacturerDao.create(skoda);
+        manufacturerDao.create(zazTavria);
+        manufacturerDao.create(ford);
+        Optional<Manufacturer> manufacturer = manufacturerDao.get(2L);
+        manufacturerDao.delete(2L);
+        manufacturerDao.update(skoda);
+        manufacturerDao.getAll().forEach(System.out::println);
     }
 }
