a4410683724e4c1b9c43c14029efe86d07607395
sviat <svyatoslav.grinik@gmail.com>
2022-02-13 01:59:26 +0200

    updated Manufacturer class

--- src/main/java/mate/jdbc/model/Manufacturer.java
+++ src/main/java/mate/jdbc/model/Manufacturer.java
@@ -1,10 +1,14 @@
 package mate.jdbc.model;
 
 import lombok.Data;
+import lombok.NonNull;
 
 @Data
 public class Manufacturer {
+    @NonNull
     private Long id;
+    @NonNull
     private String name;
+    @NonNull
     private String country;
 }
