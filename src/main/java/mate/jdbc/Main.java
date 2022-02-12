4fed33db698c89395171cfe4205c3e5503cce0d2
LinkClink <shekaqwe@gmail.com>
2022-02-12 15:14:54 +0200

    main with test

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,31 @@
 package mate.jdbc;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Manufacturer;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.manufacturerDaoInit();
+    }
+
+    private void manufacturerDaoInit() {
+        ManufacturerDao manufacturerDao
+                = (ManufacturerDao) injector.getInstance(ManufacturerDao.class);
+        Manufacturer exitManufacturer = manufacturerDao.create(getNewManufacturer());
+        exitManufacturer.setCountry("New Germany");
+        manufacturerDao.update(exitManufacturer);
+        manufacturerDao.delete(1L);
+        System.out.println(manufacturerDao.gerAll());
+    }
+
+    private Manufacturer getNewManufacturer() {
+        Manufacturer manufacturer = new Manufacturer();
+        manufacturer.setCountry("Germany");
+        manufacturer.setName("Audi");
+        return manufacturer;
     }
 }
