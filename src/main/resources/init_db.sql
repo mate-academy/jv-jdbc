ce4d1c87554429d3a6ad5767ff04d13e6e03c48a
sviat <svyatoslav.grinik@gmail.com>
2022-02-13 04:31:23 +0200

    the default value for 'is_deleted' table was added

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -2,10 +2,10 @@ CREATE
 DATABASE `taxi_service_db` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
 CREATE TABLE `manufacturers`
 (
-    `id`         bigint NOT NULL AUTO_INCREMENT,
-    `name`       varchar(255) DEFAULT NULL,
-    `country`    varchar(255) DEFAULT NULL,
-    `is_deleted` tinyint      DEFAULT NULL,
+    `id`         bigint  NOT NULL AUTO_INCREMENT,
+    `name`       varchar(255)     DEFAULT NULL,
+    `country`    varchar(255)     DEFAULT NULL,
+    `is_deleted` tinyint NOT NULL DEFAULT '0',
     PRIMARY KEY (`id`),
     UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb3;
