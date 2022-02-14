4fbf3469e88ece3564c190f6a053b694e88fb9ab
LinkClink <shekaqwe@gmail.com>
2022-02-14 13:52:04 +0200

    Update init_db.sql

--- src/recources/init_db.sql
+++ src/recources/init_db.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE taxi_service;
 
 CREATE TABLE `taxi_service`.`manufacturer` (
-`id` BIGINT NOT NULL AUTO_INCREMENT ,
-`name` TEXT NOT NULL ,
-`country` TEXT NOT NULL ,
-`is_deleted` BOOLEAN NOT NULL DEFAULT FALSE ,
-PRIMARY KEY (`id`)) ENGINE = InnoDB;
+ `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+`name` VARCHAR(225) NOT NULL,
+`country` VARCHAR(225) NOT NULL,
+`is_deleted` TINYINT NOT NULL DEFAULT 0,
+PRIMARY KEY (`id`));
