051e87e14b0cbfbdf01fd1171ab755328b26263f
Sasha Bachynskyi <allfred12@gmail.com>
2022-02-15 16:39:26 +0200

    ConnectionUtil class was created and it establishes connection to DB

---
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -0,0 +1,27 @@
+package mate.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionUtil {
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Can't load JDBC driver for MySQL", e);
+        }
+    }
+
+    public static Connection getConnection() {
+        try {
+            Properties dbPreperties = new Properties();
+            dbPreperties.put("user", "root");
+            dbPreperties.put("password", "freedom");
+            return DriverManager.getConnection("jdbc:mysql://localhost:3306/taxi_service_db", dbPreperties);
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't create connection to DB", e);
+        }
+    }
+}
