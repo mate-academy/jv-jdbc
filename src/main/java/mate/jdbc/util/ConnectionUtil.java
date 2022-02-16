4880d0689c955dc3eb26d45994c8ccf08d732013
Sasha Bachynskyi <allfred12@gmail.com>
2022-02-16 13:59:48 +0200

    new constants were added in ConnectionUtil

--- src/main/java/mate/jdbc/util/ConnectionUtil.java
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -6,9 +6,14 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionUtil {
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String USER = "root";
+    private static final String PASSWORD = "freedom";
+    private static final String CONNECTION_ADDRESS = "jdbc:mysql://localhost:3306/taxi_service_db";
+
     static {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName(DRIVER);
         } catch (ClassNotFoundException e) {
             throw new RuntimeException("Can't load JDBC driver for MySQL", e);
         }
@@ -17,10 +22,9 @@ public class ConnectionUtil {
     public static Connection getConnection() {
         try {
             Properties dbPreperties = new Properties();
-            dbPreperties.put("user", "root");
-            dbPreperties.put("password", "freedom");
-            return DriverManager.getConnection("jdbc:mysql://localhost:3306/taxi_service_db",
-                    dbPreperties);
+            dbPreperties.put("user", USER);
+            dbPreperties.put("password", PASSWORD);
+            return DriverManager.getConnection(CONNECTION_ADDRESS, dbPreperties);
         } catch (SQLException e) {
             throw new RuntimeException("Can't create connection to DB", e);
         }
