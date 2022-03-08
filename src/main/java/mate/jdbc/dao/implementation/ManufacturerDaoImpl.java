4d96651c34d2575d0946c99c1d4263dd305e5a9d
Olexsii_Titov <drfrankinsheyn16@gmail.com>
2022-03-08 17:49:00 +0200

    ManufacturerDaoImpl.getAll() was implemented

--- src/main/java/mate/jdbc/dao/implementation/ManufacturerDaoImpl.java
+++ src/main/java/mate/jdbc/dao/implementation/ManufacturerDaoImpl.java
@@ -1,4 +1,58 @@
 package mate.jdbc.dao.implementation;
 
-public class ManufacturerDaoImpl {
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.exeptions.DataProcessingException;
+import mate.jdbc.lib.Dao;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.util.ConnectionUtil;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Dao
+public class ManufacturerDaoImpl implements ManufacturerDao {
+    @Override
+    public Manufacturer create(Manufacturer manufacturer) {
+        return null;
+    }
+
+    @Override
+    public Optional<Manufacturer> get(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Manufacturer> getAll() {
+        try (Connection connection = ConnectionUtil.getConnection();
+             Statement getAllManufacturersStatement = connection.createStatement()) {
+            List<Manufacturer> allManufacturers = new ArrayList<>();
+            String sqlStatement = "select * from manufacturers;";
+            ResultSet resultSet = getAllManufacturersStatement.executeQuery(sqlStatement);
+            while (resultSet.next()) {
+                Long id = resultSet.getObject("id", Long.class);
+                String name = resultSet.getString("name");
+                String country = resultSet.getString("country");
+                Manufacturer manufacturer = new Manufacturer(id, name, country);
+                allManufacturers.add(manufacturer);
+            }
+            return allManufacturers;
+        } catch (SQLException e) {
+            throw new DataProcessingException("Can't gat all manufacturers from DB.", e);
+        }
+    }
+
+    @Override
+    public Manufacturer update(Manufacturer manufacturer) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return false;
+    }
 }
