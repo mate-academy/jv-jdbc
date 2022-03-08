5407b8478c0e125e5da810ca24047db7922a594d
Olexsii_Titov <drfrankinsheyn16@gmail.com>
2022-03-08 18:08:25 +0200

    ManufacturerDaoImpl.create() was implemented

--- src/main/java/mate/jdbc/dao/implementation/ManufacturerDaoImpl.java
+++ src/main/java/mate/jdbc/dao/implementation/ManufacturerDaoImpl.java
@@ -6,10 +6,7 @@ import mate.jdbc.lib.Dao;
 import mate.jdbc.model.Manufacturer;
 import mate.jdbc.util.ConnectionUtil;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -18,7 +15,25 @@ import java.util.Optional;
 public class ManufacturerDaoImpl implements ManufacturerDao {
     @Override
     public Manufacturer create(Manufacturer manufacturer) {
-        return null;
+        String insertManufacturerRequest = "insert into manufacturers(name, country) values(?,?);";
+        try (Connection connection = ConnectionUtil.getConnection();
+             PreparedStatement createManufacturerStatement =
+                     connection.prepareStatement(insertManufacturerRequest, Statement.RETURN_GENERATED_KEYS)) {
+            createManufacturerStatement.setString(1, manufacturer.getName());
+            createManufacturerStatement.setString(2,manufacturer.getCountry());
+            createManufacturerStatement.executeUpdate();
+            ResultSet generatedKeys = createManufacturerStatement.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                Long id = generatedKeys.getObject(1, Long.class);
+                manufacturer.setId(id);
+            }
+        } catch (SQLException e) {
+            throw new DataProcessingException("Can't insert manufacturer ("
+                    + manufacturer.getName() + ", "
+                    + manufacturer.getCountry()
+                    + ")", e);
+        }
+        return manufacturer;
     }
 
     @Override
@@ -31,8 +46,8 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
         try (Connection connection = ConnectionUtil.getConnection();
              Statement getAllManufacturersStatement = connection.createStatement()) {
             List<Manufacturer> allManufacturers = new ArrayList<>();
-            String sqlStatement = "select * from manufacturers;";
-            ResultSet resultSet = getAllManufacturersStatement.executeQuery(sqlStatement);
+            String sqlRequest = "select * from manufacturers;";
+            ResultSet resultSet = getAllManufacturersStatement.executeQuery(sqlRequest);
             while (resultSet.next()) {
                 Long id = resultSet.getObject("id", Long.class);
                 String name = resultSet.getString("name");
