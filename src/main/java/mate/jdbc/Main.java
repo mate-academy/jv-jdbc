ac03078a0eb8bcf5a33a9c319547b317e307877b
Alex Drapikovsky <drapikovskyan@gmail.com>
2022-03-18 17:55:12 +0200

    JDBC-Homework. part1. first commit

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,38 @@
 package mate.jdbc;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Manufacturer;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
+    public static void main(String[] args) {
+        //add manufacturer1
+        Manufacturer manufacturer1 = new Manufacturer();
+        manufacturer1.setName("Bob");
+        manufacturer1.setCountry("USA");
+        //add manufacturer2
+        Manufacturer manufacturer2 = new Manufacturer();
+        manufacturer2.setName("Alice");
+        manufacturer2.setCountry("USA");
+        // check 'create' method work
+        ManufacturerDao manufacturerDao =
+                (ManufacturerDao) injector.getInstance(ManufacturerDao.class);
+        manufacturerDao.create(manufacturer1);
+        System.out.println("Add manufacturer1 success");
+        manufacturerDao.create(manufacturer2);
+        System.out.println("Add manufacturer2 success");
+        // check 'get' method work
+        System.out.println(manufacturerDao.get(manufacturer1.getId()));
+        // check 'update' method work
+        manufacturer1.setName("Chack");
+        manufacturer1.setCountry("Germany");
+        manufacturerDao.update(manufacturer1);
+        // check 'delete' method work
+        manufacturerDao.delete(manufacturer2.getId());
+        System.out.println("Delete manufacturer2 success");
+        // check 'getAll' method work
+        System.out.println(manufacturerDao.getAll());
     }
 }
