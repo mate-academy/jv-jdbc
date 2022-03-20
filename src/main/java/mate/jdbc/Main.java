549a9fe0cac265b69ac6f9648ad54f8776012441
AprioriKS <sounddummies@gmail.com>
2022-03-20 21:49:02 +0200

    add @Dao

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,11 +1,18 @@
 package mate.jdbc;
 
+import mate.jdbc.dao.ManufacturerDao;
 import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Manufacturer;
 
 public class Main {
-    private static Injector injector = Injector.getInstance("mate.jdbc");
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
 
     public static void main(String[] args) {
+        ManufacturerDao manufacturerDao = (ManufacturerDao) injector.getInstance(ManufacturerDao.class);
+        Manufacturer manufacturer = new Manufacturer();
+        // initialize field values using setters or constructor
+        manufacturerDao.create(manufacturer);
+        // test other methods from ManufacturerDao
 
     }
 }
