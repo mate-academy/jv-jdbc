5a477ed8c4c2f688ba6a36aaa0722f3c8d8a0f69
IgorZima90 <zimyshkaa@gmail.com>
2022-03-21 11:23:06 +0200

    fixed mistackes

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,21 +1,23 @@
 package mate.jdbc;
 
+import java.util.List;
 import mate.jdbc.dao.ManufacturerDao;
 import mate.jdbc.lib.Injector;
 import mate.jdbc.models.Manufacturer;
-
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         Injector injector = Injector.getInstance("mate.jdbc");
         ManufacturerDao manufacturerDao
                 = (ManufacturerDao) injector.getInstance(ManufacturerDao.class);
-        Manufacturer bmwManufacturer = manufacturerDao.creat(new Manufacturer(null, "BMW", "Germany"));
+        Manufacturer bmwManufacturer = manufacturerDao
+                .creat(new Manufacturer(null, "BMW", "Germany"));
         System.out.println(bmwManufacturer);
-        Manufacturer mercedesManufacturer = manufacturerDao.creat(new Manufacturer(null, "Mercedes", "Germany"));
+        Manufacturer mercedesManufacturer = manufacturerDao
+                .creat(new Manufacturer(null, "Mercedes", "Germany"));
         System.out.println(mercedesManufacturer);
-        Manufacturer fordManufacturer = manufacturerDao.creat(new Manufacturer(null, "FORD", "USA"));
+        Manufacturer fordManufacturer = manufacturerDao
+                .creat(new Manufacturer(null, "FORD", "USA"));
         System.out.println(fordManufacturer);
         manufacturerDao.deleted(mercedesManufacturer.getId());
         manufacturerDao.update(new Manufacturer(mercedesManufacturer.getId(), "toyota", "Japan"));
