4b3adca60cc954e03bc96c6182ac6f6e7e5310ab
Tatka <tlogatskaya96@gmail.com>
2022-04-05 12:04:56 +0300

    implemented first step

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,22 @@
 package mate.jdbc;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Manufacturer;
 
+public class Main {
+    private static final Injector injector = Injector
+            .getInstance("mate.jdbc");
+
+    public static void main(String[] args) {
+        ManufacturerDao manufacturerDao = (ManufacturerDao) injector
+                .getInstance(ManufacturerDao.class);
+        Manufacturer manufacturerToyota = new Manufacturer("Toyota", "Japan");
+        Manufacturer manufacturerMercedes = new Manufacturer("Mercedes", "Germany");
+        manufacturerDao.create(manufacturerToyota);
+        manufacturerDao.create(manufacturerMercedes);
+        manufacturerDao.delete(manufacturerMercedes.getId());
+        manufacturerDao.update(manufacturerToyota);
+        manufacturerDao.getAll().forEach(System.out::println);
     }
 }
