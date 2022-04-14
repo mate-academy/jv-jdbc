f0cd538360a4350cc19fd83e454fb55df52f6792
dippp <Dipppp84@gmail.com>
2022-04-14 19:56:49 +0300

    Ñfix 3

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,34 +1,15 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
 import model.dao.ManufacturerDao;
 import model.entity.Manufacturer;
 
 public class Main {
+    private static final Injector injector = Injector.getInstance("YOUR_PACKAGE");
+
     public static void main(String[] args) {
-        System.out.println("create manufacturer");
-        //create manufacturer1
-        Manufacturer manufacturer1 = new Manufacturer();
-        manufacturer1.setName("name1").setCountry("country1");
-        ManufacturerDao.getInstance().create(manufacturer1);
-        //create manufacturer2
-        Manufacturer manufacturer2 = new Manufacturer();
-        manufacturer2.setName("name2").setCountry("country2");
-        ManufacturerDao.getInstance().create(manufacturer2);
-        //getAll
-        System.out.println("getAll");
-        ManufacturerDao.getInstance().getAll().forEach(System.out::println);
-        //update
-        System.out.println("update");
-        manufacturer1.setName("updateName1").setCountry("updateCounty1");
-        ManufacturerDao.getInstance().update(manufacturer1);
-        //getAll
-        System.out.println("getAll");
-        ManufacturerDao.getInstance().getAll().forEach(System.out::println);
-        //deleted
-        System.out.println("deleted");
-        ManufacturerDao.getInstance().delete(manufacturer1.getId());
-        //getAll
-        System.out.println("getAll");
-        ManufacturerDao.getInstance().getAll().forEach(System.out::println);
+        ManufacturerDao manufacturerDao = (ManufacturerDao) injector.getInstance(ManufacturerDao.class);
+        Manufacturer manufacturer = new Manufacturer();
+        manufacturerDao.create(manufacturer);
     }
 }
