53e3154f5c23edbd1a3f8d66f233005cc991f0b7
Dmytro Skarzhynets <d.skarzh@protonmail.com>
2022-04-18 08:56:19 +0300

    Fix: replace line endings to Unix-style, fix init_db.sql

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -2,8 +2,8 @@ CREATE DATABASE `taxi_db` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAUL
 
 CREATE TABLE `manufacturers` (
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `country` varchar(255) DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `country` varchar(255) NOT NULL,
   `is_deleted` tinyint NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb3;
