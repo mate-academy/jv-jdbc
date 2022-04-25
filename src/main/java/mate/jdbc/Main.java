d92d457a15fb1e3313209eea369158c0b36b52e5
Vitalii Moisieiev <vitalij.mojsejiv@gmail.com>
2022-04-25 23:45:04 +0300

    Added files init_db.sql, classes ConnectionUtil, ManufacturerDao with implementation, created model Manufacturer, some testing performed

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,19 @@
 package mate.jdbc;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.util.ConnectionUtil;
 
+import java.sql.Connection;
+
+public class Main {
+    public static final Injector injector = Injector.getInstance("mate.jdbc");
+    public static void main(String[] args) {
+        Connection connection = ConnectionUtil.getConnection();
+        ManufacturerDao manufacturerDao =
+                (ManufacturerDao) injector.getInstance(ManufacturerDao.class);
+        Manufacturer manufacturer = new Manufacturer();
+        manufacturerDao.create(manufacturer);
     }
 }
