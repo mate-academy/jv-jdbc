511368e4a64205a963f1aee4fa6da852d446a620
Vitalii Moisieiev <vitalij.mojsejiv@gmail.com>
2022-04-26 23:33:11 +0300

    Added custom exception, added functionality to classes Manufacturer, ManufacturerDao

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -14,6 +14,5 @@ public class Main {
         ManufacturerDao manufacturerDao =
                 (ManufacturerDao) injector.getInstance(ManufacturerDao.class);
         Manufacturer manufacturer = new Manufacturer();
-        manufacturerDao.create(manufacturer);
     }
 }
--- src/main/java/mate/jdbc/dao/ManufacturerDaoImpl.java
+++ src/main/java/mate/jdbc/dao/ManufacturerDaoImpl.java
@@ -2,7 +2,14 @@ package mate.jdbc.dao;
 
 import mate.jdbc.lib.Dao;
 import mate.jdbc.model.Manufacturer;
+import mate.jdbc.util.ConnectionUtil;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,23 +17,74 @@ import java.util.Optional;
 public class ManufacturerDaoImpl implements ManufacturerDao {
     @Override
     public Manufacturer create(Manufacturer manufacturer) {
-        System.out.println("Create call");
-        return null;
+        String sqlQuery = "INSERT INTO manufacturers(name, country) values(?, ?);";
+        try (Connection connection = ConnectionUtil.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sqlQuery,
+                     Statement.RETURN_GENERATED_KEYS)) {
+            statement.setString(1, manufacturer.getName());
+            statement.setString(2, manufacturer.getCountry());
+            statement.executeUpdate();
+            ResultSet generatedKeys = statement.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                manufacturer.setId(generatedKeys.getObject(1, Long.class));
+            }
+        } catch (SQLException e) {
+            throw new DataProcessingException("Problem creating manufacturer in DB, name: "
+                    + manufacturer.getName() + ", country: " + manufacturer.getCountry(), e);
+        }
+        return manufacturer;
     }
 
     @Override
     public Optional<Manufacturer> get(Long id) {
+        String sqlQuery =
+                "SELECT name, country FROM manufacturers WHERE id = ? AND is_deleted = FALSE";
+        try (Connection connection = ConnectionUtil.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sqlQuery)) {
+            statement.setLong(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                return Optional.of(new Manufacturer(id, resultSet.getString("name"),
+                        resultSet.getString("country")));
+            }
+        } catch (SQLException e) {
+            throw new DataProcessingException("Problem getting manufacturer from DB, id: " + id, e);
+        }
         return Optional.empty();
     }
 
     @Override
     public List<Manufacturer> getAll() {
-        return null;
+        String sqlQuery = "SELECT * FROM manufacturers WHERE is_deleted = FALSE";
+        List<Manufacturer> manufacturerList = new ArrayList<>();
+        try (Connection connection = ConnectionUtil.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sqlQuery)) {
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                manufacturerList.add(new Manufacturer(resultSet.getObject("id", Long.class),
+                        resultSet.getString("name"),
+                        resultSet.getString("country")));
+            }
+        } catch (SQLException e) {
+            throw new DataProcessingException("Problem getting manufacturers list from DB", e);
+        }
+        return manufacturerList;
     }
 
     @Override
     public Manufacturer update(Manufacturer manufacturer) {
-        return null;
+        String sqlUpdateQuery = "UPDATE manufacturers SET name = ?, country = ? WHERE id = ?;";
+        try (Connection connection = ConnectionUtil.getConnection();
+             PreparedStatement updateStatement = connection.prepareStatement(sqlUpdateQuery);) {
+            updateStatement.setString(1, manufacturer.getName());
+            updateStatement.setString(2, manufacturer.getCountry());
+            updateStatement.setLong(3, manufacturer.getId());
+            updateStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataProcessingException("Problem creating manufacturer in DB, name: "
+                    + manufacturer.getName() + ", country: " + manufacturer.getCountry(), e);
+        }
+        return manufacturer;
     }
 
     @Override
