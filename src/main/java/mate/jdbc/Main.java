528247173ec193d909dbca5baab3d2d1c158f3d2
Iryna Hrynda <hryndairyna1@gmail.com>
2022-04-28 15:23:06 +0300

    - implemented taxi-service

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,21 @@
 package mate.jdbc;
 
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Manufacturer;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
+        ManufacturerDao manufacturerDao = (ManufacturerDao) injector
+                .getInstance(ManufacturerDao.class);
+        Manufacturer manufacturer = new Manufacturer(null, "Lexus", "Japan");
+        manufacturerDao.create(manufacturer);
+        manufacturer.setName("Porsche");
+        manufacturer.setCountry("Germany");
+        manufacturerDao.update(manufacturer);
+        manufacturerDao.delete(manufacturer.getId());
 
     }
 }
