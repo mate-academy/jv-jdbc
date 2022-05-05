db54d737f17c15a88782ac2cee2591696e2ced1f
bukoz <bukoz@meta.ua>
2022-05-05 17:48:37 +0300

    Established connection to DB and implemented CRUD methods

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,27 @@
 package mate.jdbc;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Manufacturer;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc.dao");
+
+    public static void main(String[] args) {
+        ManufacturerDao manufacturerDao =
+                (ManufacturerDao) injector.getInstance(ManufacturerDao.class);
+        Manufacturer bogdan = manufacturerDao.create(new Manufacturer("Bogdan", "Ukraine"));
+        Manufacturer bmw = manufacturerDao.create(new Manufacturer("BMW", "Estonia"));
+        Manufacturer mercedes = manufacturerDao.create(new Manufacturer("Mercedes", "Poland"));
+        Manufacturer porsche = manufacturerDao.create(new Manufacturer("Porsche", "Italy"));
+        Manufacturer wv = manufacturerDao.create(new Manufacturer("Volkswagen", "Spain"));
+        System.out.println("Delete porsche manufacturer by id: "
+                + manufacturerDao.delete(porsche.getId()));
+        System.out.println("Get wv manufacturer by id: "
+                + manufacturerDao.get(wv.getId()));
+        System.out.println("Update bmw manufacturer : "
+                + manufacturerDao.update(new Manufacturer(bmw.getId(),
+                "SuperBMW", "Germany")));
+        manufacturerDao.getAll().forEach(System.out::println);
     }
 }
