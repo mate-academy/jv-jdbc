a6a8c67bc530dc321528cf131ed815e3331d0870
Roman Kachmaryk <roman.kv98@gmail.com>
2022-05-12 02:27:37 +0300

    all of the goals have been achieved

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,28 @@
 package mate.jdbc;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.models.Manufacturer;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
+    public static void main(String[] args) {
+        ManufacturerDao manufacturerDao =
+                (ManufacturerDao) injector.getInstance(ManufacturerDao.class);
+
+        Manufacturer firstManufacturer = new Manufacturer("Volkswagen", "Germany");
+        Manufacturer secondManufacturer = new Manufacturer("Toyota", "Japan");
+
+        manufacturerDao.create(firstManufacturer);
+        manufacturerDao.create(secondManufacturer);
+
+        secondManufacturer.setId(11L);
+        secondManufacturer.setName("Honda");
+
+        manufacturerDao.update(secondManufacturer);
+        manufacturerDao.delete(11L);
+        System.out.println(manufacturerDao.get(10L));
+        manufacturerDao.getAll().forEach(System.out::println);
     }
 }
