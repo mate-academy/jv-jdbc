b0651509221fbdcdf42b975e89430a7a1bdd0a36
Nickolai Vakulich <nickolai.vakulich@gmail.com>
2022-05-11 18:12:42 +0300

    Establish connection to Database. Create init_db.sql file in src/main/resources folder. Create Manufacturer model. Create DAO layer for Manufacturer model. Below you can see the list of required methods. Create DataProcessingException;

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,18 @@
 package mate.jdbc;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Manufacturer;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+    public static void main(String[] args) {
+        ManufacturerDao manufacturerDao = (ManufacturerDao) injector.getInstance(ManufacturerDao.class);
+        Manufacturer manufacturer = new Manufacturer(0L,"test","testContr");
+        manufacturerDao.create(manufacturer);
+        manufacturer.setCountry("korea");
+        manufacturerDao.update(manufacturer);
+        manufacturerDao.delete(manufacturer.getId());
+        manufacturerDao.getAll().forEach(System.out::println);
     }
 }
