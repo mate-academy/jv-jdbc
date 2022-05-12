a71a21d0f12508d9f54238d72e38a4798735470c
Андрій Кирильчук <101740223+Gigawatt94@users.noreply.github.com>
2022-05-12 16:50:03 +0300

    project ready

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -8,18 +8,15 @@ public class Main {
     private static final Injector injector = Injector.getInstance("mate.jdbc");
 
     public static void main(String[] args) {
-
-        public static void main (String[]args){
-            ManufacturerDao manufacturerDao = (ManufacturerDao) injector
-                    .getInstance(ManufacturerDao.class);
-            Manufacturer manufacturerLexus = new Manufacturer("Lexus", "Japan");
-            Manufacturer manufacturerPorsche = new Manufacturer("Porsche", "Germany");
-            manufacturerDao.create(manufacturerLexus);
-            manufacturerDao.create(manufacturerPorsche);
-            manufacturerDao.delete(manufacturerPorsche.getId());
-            manufacturerDao.update(manufacturerLexus);
-            manufacturerDao.get(manufacturerLexus.getId());
-            manufacturerDao.getAll().forEach(System.out::println);
-        }
+        ManufacturerDao manufacturerDao = (ManufacturerDao) injector
+                .getInstance(ManufacturerDao.class);
+        Manufacturer manufacturerLexus = new Manufacturer("Lexus", "Japan");
+        Manufacturer manufacturerPorsche = new Manufacturer("Porsche", "Germany");
+        manufacturerDao.create(manufacturerLexus);
+        manufacturerDao.create(manufacturerPorsche);
+        manufacturerDao.delete(manufacturerPorsche.getId());
+        manufacturerDao.update(manufacturerLexus);
+        manufacturerDao.get(manufacturerLexus.getId());
+        manufacturerDao.getAll().forEach(System.out::println);
     }
 }
