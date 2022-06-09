90ababbdda86caf4933c5b51b7ed9db74f8ad78b
yaroslav <morozov.yrv@gmail.com>
2022-06-09 13:21:31 +0300

    spaces added

--- src/main/java/mate/jdbc/dao/ManufacturerDao.java
+++ src/main/java/mate/jdbc/dao/ManufacturerDao.java
@@ -6,8 +6,12 @@ import java.util.Optional;
 
 public interface ManufacturerDao {
     Manufacturer create(Manufacturer manufacturer);
+
     Optional<Manufacturer> get(Long id);
+
     List<Manufacturer> getAll();
+
     Manufacturer update(Manufacturer manufacturer);
+
     boolean delete(Long id);
 }
