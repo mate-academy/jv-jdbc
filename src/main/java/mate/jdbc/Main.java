58f110bcfdeddd0dea4962314db3813e2e303f39
yaroslav <morozov.yrv@gmail.com>
2022-06-09 13:21:39 +0300

    dao test

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -10,10 +10,28 @@ public class Main {
     private static final Injector injector = Injector.getInstance("mate.jdbc");
     public static void main(String[] args) {
         ManufacturerDao manufacturerDao = (ManufacturerDao) injector.getInstance(ManufacturerDao.class);
-        Manufacturer manufacturer = new Manufacturer();
-        manufacturer.setName("Ivan");
-        manufacturer.setCountry("Poland");
-        manufacturerDao.create(manufacturer);
-        System.out.println(manufacturer.getId());
+
+        Manufacturer manufacturer1 = new Manufacturer();
+        manufacturer1.setName("Volkswagen");
+        manufacturer1.setCountry("Germany");
+
+        Manufacturer manufacturer2 = new Manufacturer();
+        manufacturer2.setName("Toyota");
+        manufacturer2.setCountry("Japan");
+
+        manufacturer1 = manufacturerDao.create(manufacturer1);
+        manufacturer2 = manufacturerDao.create(manufacturer2);
+
+        System.out.println(manufacturerDao.getAll());
+
+        manufacturer1.setCountry("USA");
+
+        manufacturerDao.update(manufacturer1);
+
+        System.out.println(manufacturerDao.get(manufacturer1.getId()));
+
+        manufacturerDao.delete(manufacturer2.getId());
+
+        System.out.println(manufacturerDao.getAll());
     }
 }
