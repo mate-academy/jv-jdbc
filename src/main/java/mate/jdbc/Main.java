a6f6949e61684170f7a17b674b1b45b1e26d4df3
yaroslav <morozov.yrv@gmail.com>
2022-06-09 13:38:57 +0300

    checkstyle

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -8,18 +8,19 @@ public class Main {
     private static final Injector injector = Injector.getInstance("mate.jdbc");
 
     public static void main(String[] args) {
+        ManufacturerDao manufacturerDao =
+                (ManufacturerDao) injector.getInstance(ManufacturerDao.class);
+
         Manufacturer manufacturer1 = new Manufacturer();
         manufacturer1.setName("Volkswagen");
         manufacturer1.setCountry("Germany");
+
+        manufacturer1 = manufacturerDao.create(manufacturer1);
 
         Manufacturer manufacturer2 = new Manufacturer();
         manufacturer2.setName("Toyota");
         manufacturer2.setCountry("Japan");
 
-        ManufacturerDao manufacturerDao =
-                (ManufacturerDao) injector.getInstance(ManufacturerDao.class);
-
-        manufacturer1 = manufacturerDao.create(manufacturer1);
         manufacturer2 = manufacturerDao.create(manufacturer2);
 
         System.out.println(manufacturerDao.getAll());
