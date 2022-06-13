d07eb570a7304b01a2a6fae8b0de6b0234132902
Mykola Dubich <98731880+Sm0kk@users.noreply.github.com>
2022-06-13 15:42:24 +0300

    Fixed constants

--- src/main/java/mate/jdbc/util/ConnectionUtil.java
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -8,8 +8,10 @@ import java.util.Properties;
 public class ConnectionUtil {
     private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String CONNECTION_URL = "jdbc:mysql://localhost:3306/taxi_service_db";
-    private static final String USER = "user";
-    private static final String PASSWORD = "password";
+    private static final String USER_NAME = "user";
+    private static final String LOGIN = "root";
+    private static final String USER_PASSWORD = "password";
+    private static final String PASSWORD = "Madskiled1";
 
     static {
         try {
@@ -22,8 +24,8 @@ public class ConnectionUtil {
     public static Connection getConnection() {
         try {
             Properties properties = new Properties();
-            properties.put(USER, "root");
-            properties.put(PASSWORD, "Madskiled1");
+            properties.put(USER_NAME, LOGIN);
+            properties.put(USER_PASSWORD, PASSWORD);
             return DriverManager.getConnection(CONNECTION_URL, properties);
         } catch (SQLException e) {
             throw new RuntimeException("Can't create connection to DB", e);
