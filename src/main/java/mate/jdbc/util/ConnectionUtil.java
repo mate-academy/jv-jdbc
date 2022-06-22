42a1d0a3fdd5ba519cfe1a4bc1c5e69e60c69eea
Nikita Semisoshenko <jaderussell197@gmail.com>
2022-06-22 13:07:14 +0300

    Exception type was corrected @ConnectionUtil class

--- src/main/java/mate/jdbc/util/ConnectionUtil.java
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -4,13 +4,14 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
+import mate.jdbc.exception.DataProcessingException;
 
 public class ConnectionUtil {
     static {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
-            throw new RuntimeException("Can't load JDBC Driver for MySQL", e);
+            throw new DataProcessingException("Can't load JDBC Driver for MySQL", e);
         }
     }
 
@@ -21,7 +22,7 @@ public class ConnectionUtil {
         try {
             return DriverManager.getConnection("jdbc:mysql://localhost:3306/taxi_db", dbProperties);
         } catch (SQLException e) {
-            throw new RuntimeException("Can't create connection to DB", e);
+            throw new DataProcessingException("Can't create connection to DB", e);
         }
     }
 }
