e57f6279ddfa48983032f6c42856192986a2729a
Igor <ZIM20093@gmail.com>
2022-06-23 10:39:40 +0300

    changed user and pass to constant

--- src/main/java/mate/jdbc/util/ConnectionUtil.java
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -6,6 +6,9 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionUtil {
+    private static final String USER_NAME = "lrngsql";
+    private static final String USER_PASSWORD = "1234567890";
+
     static {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -16,12 +19,13 @@ public class ConnectionUtil {
 
     public static Connection getConnection() {
         Properties dbProperties = new Properties();
-        dbProperties.put("user", "lrngsql");
-        dbProperties.put("password", "1234567890");
+        dbProperties.put("user", USER_NAME);
+        dbProperties.put("password", USER_PASSWORD);
         try {
             return DriverManager.getConnection("jdbc:mysql://localhost:3306/taxi", dbProperties);
         } catch (SQLException e) {
-            throw new RuntimeException("Can`t create connection to DB", e);
+            throw new RuntimeException("Can`t create connection to DB with user name: "
+                    + USER_NAME, e);
         }
     }
 }
