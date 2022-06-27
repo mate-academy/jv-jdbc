94d4a9bab68ed5d3960adc36572f0262764caa2b
Oleksii Mospan <alexey.mospan@gmail.com>
2022-06-27 19:42:18 +0300

    Moved ConnectionUtil class implementation to ConnectionUtil separate file.

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -10,23 +10,6 @@ public class Main {
 
     }
 
-    public static class ConnectionUtil {
-        static {
-            try {
-                Class.forName("com.mysql.cj.jdbc.Driver");
-            } catch (ClassNotFoundException e) {
-                throw new RuntimeException("Can't load JDBC driver fo MySQL", e);
-            }
-        }
-        public static Connection getConnection() {
-            try {
-                Properties dbProperties = new Properties();
-                dbProperties.put("user", "root");
-                dbProperties.put("password", "1234");
-                return DriverManager.getConnection("jdbc:mysql://localhost:3306/library_db", dbProperties);
-            } catch (SQLException e) {
-                throw new RuntimeException("Can't create connection to DB", e);
-            }
-        }
     }
+
 }
