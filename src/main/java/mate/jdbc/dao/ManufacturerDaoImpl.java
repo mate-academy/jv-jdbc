128ede564d0888f550f5cac93b6b05a8e177a162
Oleksii Mospan <alexey.mospan@gmail.com>
2022-06-27 19:38:58 +0300

    Added implementation of create, get, getAll, delete methods in ManufacturerDaoImpl. Added some logging messages

--- src/main/java/mate/jdbc/dao/ManufacturerDaoImpl.java
+++ src/main/java/mate/jdbc/dao/ManufacturerDaoImpl.java
@@ -1,24 +1,88 @@
 package mate.jdbc.dao;
 
-import mate.jdbc.model.Manufacturer;
+import mate.jdbc.lib.Dao;
+import mate.jdbc.models.Manufacturer;
+import mate.jdbc.util.ConnectionUtil;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Dao
 public class ManufacturerDaoImpl implements ManufacturerDao {
+    private static final Logger logger = LogManager.getLogger(ManufacturerDaoImpl.class);
+
     @Override
     public Manufacturer create(Manufacturer manufacturer) {
-        return null;
+        logger.debug("Create method was called with manufacturer={}", manufacturer);
+        String insertFormatRequest = "INSERT INTO manufacturers(name, country) values(?, ?)";
+        try (Connection connection = ConnectionUtil.getConnection();
+             PreparedStatement createManufacturersStatement =
+                     connection.prepareStatement(insertFormatRequest,
+                             Statement.RETURN_GENERATED_KEYS)) {
+            createManufacturersStatement.setString(1, manufacturer.getName());
+            createManufacturersStatement.setString(2, manufacturer.getCountry());
+            logger.debug("String of DB request={}", createManufacturersStatement.toString());
+            createManufacturersStatement.executeUpdate();
+            ResultSet generatedKeys = createManufacturersStatement.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                Long id = generatedKeys.getObject(1, Long.class);
+                manufacturer.setId(id);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't insert manufacturer to DB", e);
+        }
+        return manufacturer;
     }
 
     @Override
     public Optional<Manufacturer> get(Long id) {
+        logger.debug("Get by id method called with parameter={}", id);
+        try (Connection connection = ConnectionUtil.getConnection();
+             Statement getAllFormatsStatement = connection.createStatement()) {
+            ResultSet resultSet = getAllFormatsStatement
+                    .executeQuery("SELECT * FROM manufacturers WHERE is_deleted = FALSE;");
+            while (resultSet.next()) {
+                if (resultSet.getObject("id", Long.class).equals(id)) {
+                    String name = resultSet.getString("name");
+                    String country = resultSet.getString("country");
+                    Manufacturer manufacturer = new Manufacturer();
+                    manufacturer.setId(id);
+                    manufacturer.setName(name);
+                    manufacturer.setCountry(country);
+                    return Optional.of(manufacturer);
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't get manufacturer from DB by id", e);
+        }
         return Optional.empty();
     }
 
     @Override
     public List<Manufacturer> getAll() {
-        return null;
+        List<Manufacturer> allFormats = new ArrayList<>();
+        try (Connection connection = ConnectionUtil.getConnection();
+             Statement getAllManufacturersStatement = connection.createStatement()) {
+            ResultSet resultSet = getAllManufacturersStatement
+                    .executeQuery("SELECT * FROM manufacturers WHERE is_deleted = FALSE;");
+            while (resultSet.next()) {
+                Long id = resultSet.getObject("id", Long.class);
+                String name = resultSet.getString("name");
+                String country = resultSet.getString("country");
+                Manufacturer manufacturer = new Manufacturer();
+                manufacturer.setId(id);
+                manufacturer.setName(name);
+                manufacturer.setCountry(country);
+                allFormats.add(manufacturer);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't get all manufacturers from DB", e);
+        }
+        return allFormats;
     }
 
     @Override
@@ -28,6 +92,15 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
 
     @Override
     public boolean delete(Long id) {
-        return false;
+        String deleteRequest = "UPDATE manufacturers SET is_deleted = true WHERE id = ?"; //more good
+        try (Connection connection = ConnectionUtil.getConnection();
+             PreparedStatement createManufacturersStatement =
+                     connection.prepareStatement(deleteRequest,
+                             Statement.RETURN_GENERATED_KEYS)) {
+            createManufacturersStatement.setLong(1, id);
+            return createManufacturersStatement.executeUpdate() >= 1;
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't delete manufacturer from DB", e);
+        }
     }
 }
