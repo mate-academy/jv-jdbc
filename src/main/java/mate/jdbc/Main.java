65d63ce7ccb80e12cecf774b60c8b751fa0f8b08
Oleksii Mospan <alexey.mospan@gmail.com>
2022-06-29 01:09:26 +0300

    Made main like test class

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,15 +1,47 @@
 package mate.jdbc;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.Properties;
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.models.Manufacturer;
+import java.util.List;
 
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
+        ManufacturerDao manufacturerDao = (ManufacturerDao) injector.getInstance(ManufacturerDao.class);
+        Manufacturer manufacturer = new Manufacturer();
+        // getAll data from DataBase
+        List<Manufacturer> manufacturerList = manufacturerDao.getAll();
+        for (Manufacturer m : manufacturerList) {
+            System.out.println(m);
+        }
+        // initialize field values using setters or constructor
+        manufacturer.setName("TestManufacturer");
+        manufacturer.setCountry("TestCountry");
+        manufacturerDao.create(manufacturer);
+        // test other methods from ManufacturerDao
+        // get by id
+        System.out.println(manufacturerDao.get(2L));
+        System.out.println(manufacturerDao.get(20L));
+        // update
+        manufacturer.setName("TestManufacturer1");
+        manufacturer.setCountry("TestCountry1");
+        manufacturer.setId(6L);
+        System.out.println(manufacturerDao.update(manufacturer));
 
+        manufacturer.setName("TestManufacturer2");
+        manufacturer.setCountry("TestCountry2");
+        manufacturer.setId(2L);
+        System.out.println(manufacturerDao.update(manufacturer));
+
+        //delete
+        System.out.println(manufacturerDao.delete(4L));
+
+        // getAll data from DataBase
+        manufacturerList = manufacturerDao.getAll();
+        for (Manufacturer m : manufacturerList) {
+            System.out.println(m);
+        }
     }
-
-    }
-
 }
