c5bd98f57361af7e5a42c3b6290a54b8bb677477
Oleksii Mospan <alexey.mospan@gmail.com>
2022-06-29 16:39:06 +0300

    Made test of get by id method more readable

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -13,19 +13,29 @@ public class Main {
                 (ManufacturerDao) injector.getInstance(ManufacturerDao.class);
         Manufacturer manufacturer = new Manufacturer();
         // getAll data from DataBase
+        System.out.println("GetAll method testing");
         List<Manufacturer> manufacturerList = manufacturerDao.getAll();
         for (Manufacturer m : manufacturerList) {
             System.out.println(m);
         }
         // initialize field values using setters or constructor
+        System.out.println("Initialize field values using setters or constructor");
         manufacturer.setName("TestManufacturer");
         manufacturer.setCountry("TestCountry");
-        manufacturerDao.create(manufacturer);
+        manufacturer = manufacturerDao.create(manufacturer);
         // test other methods from ManufacturerDao
         // get by id
-        System.out.println(manufacturerDao.get(2L));
-        System.out.println(manufacturerDao.get(20L));
+        System.out.println("Get by id method testing");
+        manufacturer = manufacturerDao.get(manufacturer.getId())
+                .orElse(manufacturer);
+        System.out.println(manufacturer);
+        Manufacturer nonExistIdManufacturer = new Manufacturer();
+        nonExistIdManufacturer.setId(5L);
+        nonExistIdManufacturer = manufacturerDao.get(nonExistIdManufacturer.getId())
+                                .orElse(nonExistIdManufacturer);
+        System.out.println(nonExistIdManufacturer);
         // update
+        System.out.println("Update method testing");
         manufacturer.setName("TestManufacturer1");
         manufacturer.setCountry("TestCountry1");
         manufacturer.setId(6L);
@@ -37,6 +47,8 @@ public class Main {
         System.out.println(manufacturerDao.update(manufacturer));
 
         //delete
+        System.out.println("Delete method testing");
+
         System.out.println(manufacturerDao.delete(4L));
 
         // getAll data from DataBase
