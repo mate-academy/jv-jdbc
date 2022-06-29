9f120458cf8edc2a77cbac951a6e260f095c3030
Oleksii Mospan <alexey.mospan@gmail.com>
2022-06-29 18:14:49 +0300

    Some minor fixes in main

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -12,17 +12,20 @@ public class Main {
         ManufacturerDao manufacturerDao =
                 (ManufacturerDao) injector.getInstance(ManufacturerDao.class);
         Manufacturer manufacturer = new Manufacturer();
+
         // getAll data from DataBase
         System.out.println("GetAll method testing");
         List<Manufacturer> manufacturerList = manufacturerDao.getAll();
         for (Manufacturer m : manufacturerList) {
             System.out.println(m);
         }
+
         // initialize field values using setters or constructor
         System.out.println("Initialize field values using setters or constructor");
         manufacturer.setName("TestManufacturer");
         manufacturer.setCountry("TestCountry");
         manufacturer = manufacturerDao.create(manufacturer);
+
         // test other methods from ManufacturerDao
         // get by id
         System.out.println("Get by id method testing");
@@ -34,13 +37,13 @@ public class Main {
         nonExistIdManufacturer = manufacturerDao.get(nonExistIdManufacturer.getId())
                                 .orElse(nonExistIdManufacturer);
         System.out.println(nonExistIdManufacturer);
+
         // update
         System.out.println("Update method testing");
         manufacturer.setName("TestManufacturer1");
         manufacturer.setCountry("TestCountry1");
         manufacturer.setId(6L);
         System.out.println(manufacturerDao.update(manufacturer));
-
         manufacturer.setName("TestManufacturer2");
         manufacturer.setCountry("TestCountry2");
         manufacturer.setId(2L);
@@ -48,7 +51,6 @@ public class Main {
 
         //delete
         System.out.println("Delete method testing");
-
         System.out.println(manufacturerDao.delete(4L));
 
         // getAll data from DataBase
