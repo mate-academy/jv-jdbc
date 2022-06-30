91e235890e92d677b06159c482b1e3f4e966d87a
agash <alexgash8@gmail.com>
2022-06-30 07:55:49 +0300

    fixed small inconvinience in db create script

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -2,12 +2,12 @@ CREATE SCHEMA `taxi_db` DEFAULT CHARACTER SET utf8 ;
 
 CREATE TABLE `taxi_db`.`manufacturers` (
   `id` BIGINT(10) NOT NULL AUTO_INCREMENT,
-  `make` VARCHAR(45) NULL,
+  `name` VARCHAR(45) NULL,
   `country` VARCHAR(45) NULL,
-  `is_deleted` TINYINT NULL,
+  `is_deleted` TINYINT NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
-INSERT INTO `taxi_db`.`manufacturers` (`make`, `country`, `is_deleted`) VALUES ('Toyota', 'Japan', '0');
-INSERT INTO `taxi_db`.`manufacturers` (`make`, `country`, `is_deleted`) VALUES ('Ford', 'USA', '0');
+INSERT INTO `taxi_db`.`manufacturers` (`name`, `country`, `is_deleted`) VALUES ('Toyota', 'Japan', '0');
+INSERT INTO `taxi_db`.`manufacturers` (`name`, `country`, `is_deleted`) VALUES ('Ford', 'USA', '0');
