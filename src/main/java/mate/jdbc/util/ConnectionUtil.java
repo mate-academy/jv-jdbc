2716cda27ea8704556857c8bc014c8c078468f96
nazarbaklanov <102661489+nazarbaklanov@users.noreply.github.com>
2022-07-03 19:53:56 +0300

    Update ConnectionUtil.java

--- src/main/java/mate/jdbc/util/ConnectionUtil.java
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -9,7 +9,7 @@ public class ConnectionUtil {
     private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String DB_URL = "jdbc:mysql://localhost:3306/taxi_service";
     private static final String USER_NAME = "root";
-    private static final String USER_PASSWORD = "go020481bna";
+    private static final String USER_PASSWORD = "bna123456";
 
     static {
         try {
