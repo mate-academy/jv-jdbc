9be82099d0321d33442535cba695f1067e24bf76
Mykola Yakymiv <mykola8426@gmail.com>
2022-07-04 12:48:54 +0300

    added fixes

--- src/main/java/mate/jdbc/util/ConnectionUtil.java
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -6,14 +6,14 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionUtil {
-    private static final String CLAZZ_NAME = "com.mysql.cj.jdbc.Driver";
+    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String USER_NAME_FOR_DB = "root";
     private static final String USER_PASSWORD_FOR_DB = "12345789";
-    private static final String WAY_TO_SCHEMA = "jdbc:mysql://localhost:3306/taxi_db";
+    private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/taxi_db";
 
     static {
         try {
-            Class.forName(CLAZZ_NAME);
+            Class.forName(JDBC_DRIVER);
         } catch (ClassNotFoundException e) {
             throw new RuntimeException("Can't load jdbc driver for MySQL", e);
         }
@@ -24,7 +24,7 @@ public class ConnectionUtil {
             Properties dbProperties = new Properties();
             dbProperties.put("user", USER_NAME_FOR_DB);
             dbProperties.put("password", USER_PASSWORD_FOR_DB);
-            return DriverManager.getConnection(WAY_TO_SCHEMA, dbProperties);
+            return DriverManager.getConnection(DATABASE_URL, dbProperties);
         } catch (SQLException e) {
             throw new RuntimeException("Can't creat connection to DB", e);
         }
