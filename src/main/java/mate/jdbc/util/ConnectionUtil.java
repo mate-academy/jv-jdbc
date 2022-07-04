5d558af09762436d5b9a2512d8e22fe35032df33
Yulii Lukhanin <julius.lukhanin@outlook.com>
2022-07-04 22:18:16 +0300

    add 4 const in connectionUtil

--- src/main/java/mate/jdbc/util/ConnectionUtil.java
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -6,9 +6,14 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionUtil {
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/taxi_service_db";
+    private static final String USER_NAME = "user";
+    private static final String PASSWORD = "1234";
+
     static {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName(DRIVER);
         } catch (ClassNotFoundException e) {
             throw new RuntimeException("Can't load JDBC driver for MySQL", e);
         }
@@ -16,11 +21,11 @@ public class ConnectionUtil {
 
     public static Connection getConnection() {
         Properties dbProperties = new Properties();
-        dbProperties.put("user", "user");
-        dbProperties.put("password", "1234");
+        dbProperties.put("user", USER_NAME);
+        dbProperties.put("password", PASSWORD);
         try {
             return DriverManager
-                    .getConnection("jdbc:mysql://localhost:3306/taxi_service_db", dbProperties);
+                    .getConnection(URL, dbProperties);
         } catch (SQLException e) {
             throw new RuntimeException("Can't create connection to DB", e);
         }
