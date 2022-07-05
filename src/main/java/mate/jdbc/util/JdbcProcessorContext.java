636b1a6404bc521f1bfc87d879796cf1ecde527c
Maksym Lutovinov <90464472+maxlutovinov@users.noreply.github.com>
2022-07-05 14:44:55 +0300

    Introduce constants for magic values in JdbcProcessorContext

--- src/main/java/mate/jdbc/util/JdbcProcessorContext.java
+++ src/main/java/mate/jdbc/util/JdbcProcessorContext.java
@@ -8,10 +8,12 @@ import java.sql.Statement;
 import mate.jdbc.exception.DataProcessingException;
 
 public class JdbcProcessorContext {
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/taxi_service_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "12345678";
+
     public Object processJdbc(String sql, Object[] sqlBindVariables, JdbcProcessor processor) {
-        try (Connection connection = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/taxi_service_db",
-                "root", "12345678");
+        try (Connection connection = DriverManager.getConnection(JDBC_URL, USER, PASSWORD);
                 PreparedStatement statement =
                         connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
             for (int i = 0; i < sqlBindVariables.length; i++) {
