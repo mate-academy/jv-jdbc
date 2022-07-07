98a31a22b4f040fb373fc757daaaade6b3f5e457
nazarbaklanov <102661489+nazarbaklanov@users.noreply.github.com>
2022-07-07 22:09:30 +0300

    Update ConnectionUtil.java
    Added changes

--- src/main/java/mate/jdbc/util/ConnectionUtil.java
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -6,10 +6,10 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionUtil {
-    private static final String URL = "YOUR DATABASE URL";
-    private static final String USERNAME = "YOUR USERNAME";
-    private static final String PASSWORD = "YOUR PASSWORD";
     private static final String JDBC_DRIVER = "YOUR DRIVER";
+    private static final String DB_URL = "YOUR DATABASE URL";
+    private static final String USER_NAME = "YOUR USERNAME";
+    private static final String USER_PASSWORD = "YOUR PASSWORD";
 
     static {
         try {
