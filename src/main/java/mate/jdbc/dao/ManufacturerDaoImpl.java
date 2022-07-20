c9bc51432496bd5a66088b7f76d8595c9a3f4fdc
Dmytro Lemeshchenko <dimmonishe@gmail.com>
2022-07-20 22:45:18 +0300

    Method update was corrected. Renamed some variables

--- src/main/java/mate/jdbc/dao/ManufacturerDaoImpl.java
+++ src/main/java/mate/jdbc/dao/ManufacturerDaoImpl.java
@@ -39,18 +39,18 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
     public Manufacturer update(Manufacturer manufacturer) {
         String updateRequest = "UPDATE manufacturers SET name = ?, country = ?"
                 + " WHERE id = ? AND is_deleted = FALSE;";
+        Manufacturer resultManufacturer = new Manufacturer();
         try (Connection connection = ConnectionUtil.getConnection();
                 PreparedStatement createManufacturerStatement = connection
-                        .prepareStatement(updateRequest, Statement.RETURN_GENERATED_KEYS)) {
+                        .prepareStatement(updateRequest)) {
             createManufacturerStatement.setString(1, manufacturer.getName());
             createManufacturerStatement.setString(2, manufacturer.getCountry());
             createManufacturerStatement.setLong(3, manufacturer.getId());
             createManufacturerStatement.executeUpdate();
-            ResultSet generatedKeys = createManufacturerStatement.getGeneratedKeys();
-            if (generatedKeys.next()) {
-                manufacturer = get(generatedKeys.getObject(1, Long.class)).get();
+            if (createManufacturerStatement.executeUpdate() != 1) {
+                throw new DataProcessingException("Can't insert manufacturer with "
+                        + "parameters: " + manufacturer + " to DB", new Throwable());
             }
-
         } catch (SQLException e) {
             throw new DataProcessingException("Can't insert manufacturer with "
                    + "parameters: " + manufacturer + " to DB", e);
@@ -61,14 +61,15 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
     @Override
     public Optional<Manufacturer> get(Long id) {
         String getRequest = "SELECT * FROM manufacturers WHERE id = ? AND is_deleted = FALSE;";
-        Manufacturer manufacturer;
+        Manufacturer manufacturer = new Manufacturer();
         try (Connection connection = ConnectionUtil.getConnection();
                 PreparedStatement getManufacturerStatement = connection
                         .prepareStatement(getRequest)) {
             getManufacturerStatement.setLong(1, id);
             ResultSet resultSet = getManufacturerStatement.executeQuery();
-            resultSet.next();
-            manufacturer = createRequest(resultSet);
+            if (resultSet.next()) {
+                manufacturer = parseManufacturer(resultSet);
+            }
         } catch (SQLException e) {
             throw new DataProcessingException("Can't get manufacturer "
                    + "with id " + id + " from DB", e);
@@ -78,19 +79,19 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
 
     @Override
     public List<Manufacturer> getAll() {
-        List<Manufacturer> manufacturerList = new ArrayList<>();
+        List<Manufacturer> manufacturers = new ArrayList<>();
         String getAllRequest = "SELECT * FROM manufacturers WHERE is_deleted = FALSE";
         try (Connection connection = ConnectionUtil.getConnection();
                 PreparedStatement getManufacturerListStatement
                         = connection.prepareStatement(getAllRequest)) {
             ResultSet resultSet = getManufacturerListStatement.executeQuery();
             while (resultSet.next()) {
-                manufacturerList.add(createRequest(resultSet));
+                manufacturers.add(parseManufacturer(resultSet));
             }
         } catch (SQLException e) {
             throw new DataProcessingException("Can't get manufacturers from DB", e);
         }
-        return manufacturerList;
+        return manufacturers;
     }
 
     @Override
@@ -107,7 +108,7 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
         }
     }
 
-    private Manufacturer createRequest(ResultSet resultSet) throws SQLException {
+    private Manufacturer parseManufacturer(ResultSet resultSet) throws SQLException {
         Manufacturer manufacturer = new Manufacturer();
         manufacturer.setId(resultSet.getObject("id", Long.class));
         manufacturer.setName(resultSet.getString("name"));
--- src/main/java/mate/jdbc/models/Car.java
+++
@@ -1,4 +0,0 @@
-package mate.jdbc.models;
-
-public class Car {
-}
