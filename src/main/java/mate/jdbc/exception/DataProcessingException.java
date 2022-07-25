f0d5ca6ed98c3a4a3946bac89fe99a4c23559dfe
l_osyash <los.nazar24@gmail.com>
2022-07-25 19:22:34 +0300

    DataProcessingException moved to separate package.

--- src/main/java/mate/jdbc/exception/DataProcessingException.java
+++ src/main/java/mate/jdbc/exception/DataProcessingException.java
@@ -1,4 +1,4 @@
-package mate.jdbc.dao;
+package mate.jdbc.exception;
 
 public class DataProcessingException extends RuntimeException {
     public DataProcessingException(String message, Throwable cause) {
