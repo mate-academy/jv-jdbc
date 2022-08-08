17b5267e7492aae79e49ef48f8032c6a13df9610
vadim9482 <vadim9482@gmail.com>
2022-08-08 21:53:08 +0300

    Manufacturer DAO was done. CheckStyle was updated

--- src/main/java/mate/jdbc/models/Car.java
+++ src/main/java/mate/jdbc/models/Car.java
@@ -1,14 +1,12 @@
 package mate.jdbc.models;
 
 import mate.jdbc.lib.Dao;
-import mate.jdbc.lib.Inject;
 
 @Dao
 public class Car {
     private Long id;
     private String name;
     private String color;
-    @Inject
     private Manufacturer manufacturer;
 
     public Car(Long id, String name, String color, Manufacturer manufacturer) {
