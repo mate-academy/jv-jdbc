f235190304ee5e87ebf95a0a57d43432410970ca
Vitaliy Ozhelivskiy <ozelivskijvitalij@gmail.com>
2022-08-16 17:33:00 +0300

    Changed AUTOINCREMENT in init_db.sql and removed comment,

--- src/main/java/mate/jdbc/exception/DataProcessingException.java
+++ src/main/java/mate/jdbc/exception/DataProcessingException.java
@@ -1,7 +1,6 @@
 package mate.jdbc.exception;
 
 public class DataProcessingException extends RuntimeException {
-
     public DataProcessingException(String message, Throwable ex) {
         throw new RuntimeException(message, ex);
     }
--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE `taxi_service` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE `taxi_service`;
 
 CREATE TABLE `manufacturers` (
   `id` bigint NOT NULL AUTO_INCREMENT,
@@ -6,4 +6,4 @@ CREATE TABLE `manufacturers` (
   `country` varchar(225) NOT NULL,
   `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
