95ecff045f3ecea69bc523578091b3d786613f70
Olesya Khalak <khalak.olesya.official@gmail.com>
2022-08-24 11:52:24 +0300

    the task is completed

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,29 @@
 package mate.jdbc;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Manufacturer;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
+    public static void main(String[] args) {
+        ManufacturerDao manufacturerDao =
+                (ManufacturerDao) injector.getInstance(ManufacturerDao.class);
+        Manufacturer manufacturerAudi = new Manufacturer(null, "Nissan", "Japan");
+        Manufacturer manufacturerBmw = new Manufacturer(null, "Porsche", "Germany");
+        manufacturerAudi = manufacturerDao.create(manufacturerAudi);
+        manufacturerBmw = manufacturerDao.create(manufacturerBmw);
+        System.out.println("Added two manufacturers");
+        manufacturerDao.getAll().forEach(System.out::println);
+        manufacturerAudi.setCountry("Ukraine");
+        manufacturerDao.update(manufacturerAudi);
+        System.out.println("Updated audi manufacturer");
+        manufacturerDao.getAll().forEach(System.out::println);
+        manufacturerDao.delete(manufacturerBmw.getId());
+        System.out.println("Deleted BMW manufacturer");
+        manufacturerDao.getAll().forEach(System.out::println);
+        System.out.println("Got audi manufacturer");
+        System.out.println(manufacturerDao.get(manufacturerAudi.getId()));
     }
 }
