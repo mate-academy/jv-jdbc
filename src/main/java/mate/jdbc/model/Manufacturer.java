1c0ce1c1b54715ba84c78b7e02496e42ab27a871
amarkelovUA <java.developer.aleksandr@gmail.com>
2022-08-29 16:33:06 +0300

    Base classes are created

---
+++ src/main/java/mate/jdbc/model/Manufacturer.java
@@ -0,0 +1,51 @@
+package mate.jdbc.model;
+
+import java.util.Objects;
+
+public class Manufacturer {
+    private Long id;
+    private String name;
+    private String country;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Manufacturer that = (Manufacturer) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(country, that.country);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, country);
+    }
+}
