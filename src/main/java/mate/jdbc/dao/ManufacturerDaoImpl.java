ba7e94f4a90bb194f945cd082f6b196cad6260ec
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-09-06 19:28:16 +0300

    move logic from main to ManufacturerDaoImpl to method getAll

---
+++ src/main/java/mate/jdbc/dao/ManufacturerDaoImpl.java
@@ -0,0 +1,54 @@
+package mate.jdbc.dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.util.ConnectionUtil;
+
+public class ManufacturerDaoImpl implements ManufacturerDao {
+    @Override
+    public Manufacturer create(Manufacturer manufacturer) {
+        return null;
+    }
+
+    @Override
+    public Optional<Manufacturer> get(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Manufacturer> getAll() {
+        List<Manufacturer> allManufacturers = new ArrayList<>();
+        Connection connection = ConnectionUtil.getConnection();
+        Statement getAllMnufacturersStatement = null;
+        try {
+            getAllMnufacturersStatement = connection.createStatement();
+            ResultSet resultSet = getAllMnufacturersStatement.executeQuery("SELECT * FROM manufacturers");
+            while (resultSet.next()) {
+                Long id = resultSet.getObject("id", Long.class);
+                String name = resultSet.getString("name");
+                String country = resultSet.getString("country");
+                Manufacturer manufacturer = new Manufacturer(id, name, country);
+                allManufacturers.add(manufacturer);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't ", e);
+        }
+        return allManufacturers;
+    }
+
+    @Override
+    public Manufacturer update(Manufacturer manufacturer) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return false;
+    }
+}
