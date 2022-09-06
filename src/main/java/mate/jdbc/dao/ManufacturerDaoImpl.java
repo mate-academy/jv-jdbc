216f93608650e78cbeb90da650bdde567d33db9a
Oleksandr-Tovstyi <raketaplaneta@gmail.com>
2022-09-06 18:15:55 +0300

    method getAll in ManufacturedDaoImpl was implemented

--- src/main/java/mate/jdbc/dao/ManufacturerDaoImpl.java
+++ src/main/java/mate/jdbc/dao/ManufacturerDaoImpl.java
@@ -7,6 +7,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,7 +26,6 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
                 Long id = generatedKeys.getObject("id", Long.class);
                 manufacturer.setId(id);
             }
-
         } catch (SQLException e) {
             throw new RuntimeException("Can't create manufacturer in DB. Manufacturer: " + manufacturer, e);
         }
@@ -39,7 +39,23 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
 
     @Override
     public List<Manufacturer> getAll() {
-        return null;
+        String getAllRequest = "SELECT * FROM manufacturers WHERE is_deleted = false";
+        List<Manufacturer> manufacturers = new ArrayList<>();
+        try (Connection connection = ConnectionUtil.getConnection();
+             PreparedStatement getAllStatement = connection.prepareStatement(getAllRequest)) {
+            getAllStatement.executeQuery();
+            ResultSet generatedKeys = getAllStatement.getGeneratedKeys();
+            while(generatedKeys.next()) {
+                Manufacturer manufacturer = new Manufacturer();
+                manufacturer.setId(generatedKeys.getObject("id", Long.class));
+                manufacturer.setName(generatedKeys.getString("name"));
+                manufacturer.setCountry(generatedKeys.getString("country"));
+                manufacturers.add(manufacturer);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Cant get all manufacturer from DB", e);
+        }
+        return manufacturers;
     }
 
     @Override
@@ -58,5 +74,12 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
         } catch (SQLException e) {
             throw new RuntimeException("Can't insert format to DB", e);
         }
+    }
+
+    private Manufacturer createManufacturer(ResultSet resultSet) {
+        Manufacturer manufacturer = new Manufacturer();
+        manufacturer.setId();
+        manufacturer.getName();
+        manufacturer.setCountry();
     }
 }
