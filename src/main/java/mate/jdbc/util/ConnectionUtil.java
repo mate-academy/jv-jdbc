ac0ff3d28fd3a470c229fed0b20eb3010f341ed4
Oleksandr-Tovstyi <raketaplaneta@gmail.com>
2022-09-06 14:35:55 +0300

    class ConnectionUtil was added

--- src/main/java/mate/jdbc/util/ConnectionUtil.java
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -1,9 +1,27 @@
 package mate.jdbc.util;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionUtil {
-    public static Connection getConnection() {
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Can't load JDBC driver for mySQL", e);
+        }
+    }
 
+    public static Connection getConnection() {
+        try {
+            Properties dbProperties = new Properties();
+            dbProperties.put("user", "root");
+            dbProperties.put("password", "32768");
+            return DriverManager.getConnection("jdbc:mysql://localhost:3306/library_db", dbProperties);
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't create connection to DB", e);
+        }
     }
 }
