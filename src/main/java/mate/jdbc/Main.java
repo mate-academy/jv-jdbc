29118075e16b0a19672e696b2f3b5e217c0c8487
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-09-06 17:37:30 +0300

    created connection

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,11 @@
 package mate.jdbc;
 
+import java.sql.Connection;
+import mate.jdbc.util.ConnectionUtil;
+
 public class Main {
     public static void main(String[] args) {
-
+        Connection connection = ConnectionUtil.getConnection();
+        System.out.println("connection = " + connection);
     }
 }
