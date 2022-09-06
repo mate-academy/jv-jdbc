3e93464b8bfb4d1bbed9ce3f01fac9f15a948955
Oleksandr-Tovstyi <raketaplaneta@gmail.com>
2022-09-06 16:12:45 +0300

    metod gelete in ManufacturerDaoImpl was implemented

--- src/main/java/mate/jdbc/dao/ManufacturerDaoImpl.java
+++ src/main/java/mate/jdbc/dao/ManufacturerDaoImpl.java
@@ -1,7 +1,12 @@
 package mate.jdbc.dao;
 
 import mate.jdbc.model.Manufacturer;
+import mate.jdbc.util.ConnectionUtil;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 import java.util.Optional;
 
@@ -28,6 +33,14 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
 
     @Override
     public boolean delete(Long id) {
-        return false;
+        String deleteRequest = "UPDATE taxi_service SET is_deleted = true where id = ?";
+        try (Connection connection = ConnectionUtil.getConnection();
+             PreparedStatement createFormatStatement =
+                     connection.prepareStatement(deleteRequest, Statement.RETURN_GENERATED_KEYS)) {
+            createFormatStatement.setLong(1, id);
+            return createFormatStatement.executeUpdate() >= 1;
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't insert format to DB", e);
+        }
     }
 }
