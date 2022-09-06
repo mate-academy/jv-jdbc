1cfd655ed428a817a136d46da6161830e7bea320
Blokitin Oleh <87024861+oleh-satoshi@users.noreply.github.com>
2022-09-06 19:24:06 +0200

    add dependency

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,11 +1,13 @@
 package mate.jdbc;
 
+import exception.DataProcessingException;
+
 public class Main {
     public static void main(String[] args) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
+            throw new DataProcessingException("Can't load JDBC driver for MySQL", e);
         }
 
     }
