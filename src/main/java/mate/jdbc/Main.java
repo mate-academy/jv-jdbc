1c3c1ff3a5080f6669f67e9f4238e3c11de66e2c
Blokitin Oleh <87024861+oleh-satoshi@users.noreply.github.com>
2022-09-06 19:19:34 +0200

    add DataProcessingException

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -2,6 +2,11 @@ package mate.jdbc;
 
 public class Main {
     public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
