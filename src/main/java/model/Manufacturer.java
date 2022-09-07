1cbf8adb1e7b843bc9e4edc3ca8a5e4c447945db
LubusUzh <lubus.uzh@gmail.com>
2022-09-07 21:20:26 +0300

    Add: dependencies for sql and create Manufacturer class

---
+++ src/main/java/model/Manufacturer.java
@@ -0,0 +1,31 @@
+package model;
+
+public class Manufacturer {
+    private Long id;
+    private String country;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
