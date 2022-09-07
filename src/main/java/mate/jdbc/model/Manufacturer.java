c255fce6f285fb82db00943ee98c155b78651fe0
Thousandfold <you@example.com>
2022-09-07 16:46:19 +0300

    create_packages_and_void_classes

---
+++ src/main/java/mate/jdbc/model/Manufacturer.java
@@ -0,0 +1,9 @@
+package mate.jdbc.model;
+
+public class Manufacturer {
+    private Long id;
+    private String name;
+    private String country;
+
+
+}
---
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -0,0 +1,13 @@
+package mate.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+public class ConnectionUtil {
+    public static Connection getConnection() {
+
+    }
+
+
+}
