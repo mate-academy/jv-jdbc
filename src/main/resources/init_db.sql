2262e464497dc2aae83b6e199043d8d7ff3973d9
Andrii Nesterenko <nesterenkoandrii23@gmail.com>
2022-09-08 00:01:52 +0300

    first commit

---
+++ src/main/resources/init_db.sql
@@ -0,0 +1,10 @@
+CREATE SCHEMA `taxi_db` DEFAULT CHARACTER SET utf8 ;
+
+CREATE TABLE `taxi_db`.`manufacturers` (
+  `id` BIGINT(10) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  `country` VARCHAR(45) NULL,
+  `is_deleted` TINYINT NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
