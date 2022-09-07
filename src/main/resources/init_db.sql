16e8c982611c513a0465a1d1dd29ed4e7d6b2bf6
Anna Popova <anitapopova1996@gmail.com>
2022-09-07 14:56:23 +0300

    Refactored init_db.sql

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE `taxi_service_db` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
-
+USE `taxi_service_db`;
 CREATE TABLE `manufacturers` (
                                  `id` bigint NOT NULL AUTO_INCREMENT,
                                  `name` varchar(45) DEFAULT NULL,
