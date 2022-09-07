9f656d37712d0438db113e0be7ec3fbee11a7cb6
Vasyl Pankiv <wasilius2@gmail.com>
2022-09-07 14:50:44 +0300

    rename all Manufacture to Manufacturer

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE `taxi_db` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
-CREATE TABLE `manufactures` (
+CREATE TABLE `manufacturers` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   `country` varchar(255) DEFAULT NULL,
