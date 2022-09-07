5c1124fe72ffcff3845f0bcfa2f525bdd9b65bdc
X3NON-135 <immort4ld3momo@gmail.com>
2022-09-07 17:37:52 +0100

    refactored init_db.sql

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,7 +1,12 @@
-CREATE TABLE `database`.`test`(
-    `id`         BIGINT NOT NULL AUTO_INCREMENT,
-    `name`       VARCHAR(45) NULL,
-    `country`    VARCHAR(45) NULL,
-    `is_deleted` boolean NULL DEFAULT 0,
+CREATE DATABASE `database` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+CREATE TABLE `manufacturers` (
+    `id`         bigint NOT NULL AUTO_INCREMENT,
+    `name`       varchar(45) DEFAULT NULL,
+    `country`    varchar(45) DEFAULT NULL,
+    `is_deleted` tinyint(1)  DEFAULT '0',
     PRIMARY KEY (`id`)
-);
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 19
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
