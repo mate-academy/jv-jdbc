77a0bab32cdf05bf1501bc2772ce0ac8043c5889
Vasyl Pankiv <wasilius2@gmail.com>
2022-09-07 15:27:13 +0300

    test all crud methods in main

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,61 @@
 package mate.jdbc;
 
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Manufacturer;
+
+import java.util.List;
+
 public class Main {
+    private static final String MAIN_PACKAGE_NAME = "mate.jdbc";
+    private static final Injector injector = Injector.getInstance(MAIN_PACKAGE_NAME);
+
     public static void main(String[] args) {
+        ManufacturerDao manufacturerDao = (ManufacturerDao)
+                injector.getInstance(ManufacturerDao.class);
+        Manufacturer audi = new Manufacturer();
+        audi.setName("Audi");
+        audi.setCountry("Germany");
+        manufacturerDao.create(audi);
+
+        Manufacturer bmw = new Manufacturer();
+        bmw.setName("bmw");
+        bmw.setCountry("Germany");
+        manufacturerDao.create(bmw);
+
+        Manufacturer tesla = new Manufacturer();
+        tesla.setName("Tesla");
+        tesla.setCountry("USA");
+        manufacturerDao.create(tesla);
+
+        Manufacturer ford = new Manufacturer();
+        ford.setName("Ford");
+        ford.setCountry("USA");
+        manufacturerDao.create(ford);
+
+        Manufacturer renault = new Manufacturer();
+        renault.setName("Renault");
+        renault.setCountry("France");
+        manufacturerDao.create(renault);
+        System.out.println("*************** Added: audi, bmw, tesla, ford, renault");
+
+        List<Manufacturer> manufacturers = manufacturerDao.getAll();
+        manufacturers.forEach(System.out::println);
+        System.out.println("*************** all Manufacturers from db was printed");
+
+        audi.setName("Porsche");
+        audi = manufacturerDao.update(audi);
+        System.out.println("Audi was changed to Porsche");
+
+        System.out.println(manufacturerDao.get(audi.getId()));
+        System.out.println("*************** Manufacturer with id = " + audi.getId() + " was printed");
+
+        manufacturerDao.delete(renault.getId());
+        System.out.println("Renault was deleted");
+
+        manufacturers = manufacturerDao.getAll();
+        manufacturers.forEach(System.out::println);
+        System.out.println("*************** all Manufacturers from db was printed");
 
     }
 }
