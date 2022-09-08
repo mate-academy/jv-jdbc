43f723522381a3e117c8aeafa743039b263120a5
Volodymyr Kachan <vladimirkachan1996@gmail.com>
2022-09-08 09:13:57 +0300

    my hw jdbc after students review

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,9 +1,10 @@
 CREATE DATABASE `manufacture_db` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
 CREATE TABLE `manufacturers_db` (
-                                    `id` int NOT NULL AUTO_INCREMENT,
+                                    `id` bigint NOT NULL AUTO_INCREMENT,
                                     `name` varchar(255) DEFAULT NULL,
                                     `country` varchar(255) DEFAULT NULL,
-                                    `is_deleted` varchar(45) NOT NULL DEFAULT '0',
+                                    `is_deleted` tinyint NOT NULL DEFAULT '0',
                                     PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb3;
+
