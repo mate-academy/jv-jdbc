51334c2d364ea598726f19769832e8a42ba02efc
mr-Claster <67782747+mr-Claster@users.noreply.github.com>
2022-09-08 15:52:40 +0300

    remove constant

--- src/main/java/mate/jdbc/util/ConnectionUtil.java
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -6,14 +6,10 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionUtil {
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "Sleep 42";
-    private static final String ADDRESS = "jdbc:mysql://localhost:3306/taxi_service_db";
-    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
 
     static {
         try {
-            Class.forName(JDBC_DRIVER);
+            Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             throw new RuntimeException("Can't load JDBC driver for MySQL", e);
         }
@@ -22,9 +18,10 @@ public class ConnectionUtil {
     public static Connection getConnection() {
         try {
             Properties dbProperties = new Properties();
-            dbProperties.put("user", USERNAME);
-            dbProperties.put("password", PASSWORD);
-            return DriverManager.getConnection(ADDRESS, dbProperties);
+            dbProperties.put("user", "root");
+            dbProperties.put("password", "Sleep 42");
+            return DriverManager.getConnection("jdbc:mysql://localhost:3306/taxi_service_db",
+                    dbProperties);
         } catch (SQLException e) {
             throw new RuntimeException("Can't create connection to DB",e);
         }
