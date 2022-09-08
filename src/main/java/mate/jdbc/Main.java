773d153caad2769acd7d0758e9bc0cab2fdfe328
Yevhen Kupriiuk <kupriuk@gmail.com>
2022-09-08 12:54:13 +0300

    JDBC completed

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,36 @@
 package mate.jdbc;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.Optional;
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Manufacturer;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
+    public static void main(String[] args) {
+        ManufacturerDao manufacturerDao = (ManufacturerDao) injector
+                .getInstance(ManufacturerDao.class);
+        Manufacturer mercedes = new Manufacturer(null, "Mercedes", "Germany");
+        Manufacturer audi = new Manufacturer(null, "Audi", "Germany");
+        Manufacturer tesla = new Manufacturer(null, "Tesla", "USA");
+        mercedes = manufacturerDao.create(mercedes);
+        audi = manufacturerDao.create(audi);
+        tesla = manufacturerDao.create(tesla);
+        System.out.println("Create 3 manufacturers");
+        manufacturerDao.getAll().forEach(System.out::println);
+        System.out.println("***");
+        tesla.setName("Toyota");
+        manufacturerDao.update(tesla);
+        System.out.println("Change manufacturer name Tesla to Audi");
+        manufacturerDao.getAll().forEach(System.out::println);
+        System.out.println("***");
+        manufacturerDao.delete(mercedes.getId());
+        System.out.println("Delete manufacturer mercedes from database");
+        manufacturerDao.getAll().forEach(System.out::println);
+        System.out.println("***");
+        Optional<Manufacturer> getManufacturer = manufacturerDao.get(audi.getId());
+        System.out.println("Get manufacturer Audi from database");
+        System.out.println(getManufacturer.get());
     }
 }
