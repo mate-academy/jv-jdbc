aaf1758909aac49d2bf686421cc25072218d44aa
rostyslav-berezhnytskyi <rostik2510@gmail.com>
2022-09-08 15:08:43 +0300

    First commit

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,33 @@
 package mate.jdbc;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Manufacturer;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
+    public static void main(String[] args) {
+        ManufacturerDao manufacturerDao = (ManufacturerDao) injector
+                .getInstance(ManufacturerDao.class);
+        Manufacturer toyota = new Manufacturer("Toyota", "Japan");
+        Manufacturer bmw = new Manufacturer("BMW", "Germany");
+        toyota = manufacturerDao.create(toyota);
+        bmw = manufacturerDao.create(bmw);
+        System.out.println("Create 2 manufacturers");
+
+        manufacturerDao.getAll().forEach(System.out::println);
+
+        manufacturerDao.delete(bmw.getId());
+        System.out.println("Delete BMW manufacturer");
+
+        manufacturerDao.getAll().forEach(System.out::println);
+
+        toyota.setCountry("Korea");
+        manufacturerDao.update(toyota);
+        System.out.println("Update Toyota manufacturer");
+        System.out.println(manufacturerDao.get(toyota.getId()));
+
+        manufacturerDao.getAll().forEach(System.out::println);
     }
 }
