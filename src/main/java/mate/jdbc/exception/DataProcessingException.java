16b996671f6ad1dcf2dda6b9af4d52b4ebc52d63
Aptypio13 <aptypio668@gmail.com>
2022-09-08 09:26:36 +0300

    create own exception for DB

---
+++ src/main/java/mate/jdbc/exception/DataProcessingException.java
@@ -0,0 +1,8 @@
+package mate.jdbc.exception;
+
+public class DataProcessingException extends RuntimeException {
+
+    public DataProcessingException(String message, Throwable exception) {
+        super(message, exception);
+    }
+}
