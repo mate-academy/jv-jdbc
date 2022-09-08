0e750b9c44b38de21e6c9c9a336af6e3cf29f671
Myroslav_Voskoboinyk <maestro.of.pain@gmail.com>
2022-09-08 16:15:43 +0300

    corrected after Ira Hurko review

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,5 +1,5 @@
 CREATE SCHEMA `manufacturer_db` DEFAULT CHARACTER SET utf8;
-
+USE `manufacturer_db`;
 CREATE TABLE `manufacturer_db`.`manufacturers` (
                                                    `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
                                                    `name` VARCHAR(255) NULL,
