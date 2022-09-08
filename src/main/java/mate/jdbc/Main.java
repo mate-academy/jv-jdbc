fc355f4295bf51a69adc1bfcc69811cc2f0e7fa8
Veers <raadch3011@gmail.com>
2022-09-08 14:46:58 +0300

    complete java dbc homework

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,34 @@
 package mate.jdbc;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Manufacturer;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
+    public static void main(String[] args) {
+        ManufacturerDao manufacturerDao =
+                (ManufacturerDao) injector.getInstance(ManufacturerDao.class);
+
+        Manufacturer opel = new Manufacturer(null, "Opel", "Germany");
+        manufacturerDao.create(opel);
+
+        Manufacturer volvo = new Manufacturer(null, "Volvo", "France");
+        manufacturerDao.create(volvo);
+
+        Manufacturer zaporozhets = new Manufacturer(null, "Zaporozhets", "Ukraine");
+        manufacturerDao.create(zaporozhets);
+
+        manufacturerDao.getAll().forEach(System.out::println);
+
+        volvo.setCountry("Ukraine");
+        manufacturerDao.update(volvo);
+
+        System.out.println(manufacturerDao.get(volvo.getId()));
+
+        manufacturerDao.delete(zaporozhets.getId());
+
+        manufacturerDao.getAll().forEach(System.out::println);
     }
 }
---
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -0,0 +1,28 @@
+package mate.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionUtil {
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Can't load driver JDBC", e);
+        }
+    }
+
+    public static Connection getConnection() {
+        try {
+            Properties dataBaseProperties = new Properties();
+            dataBaseProperties.put("user", "root");
+            dataBaseProperties.put("password", "qwerty123");
+            return DriverManager.getConnection("jdbc:mysql://localhost:3306/manufacturer_db",
+                    dataBaseProperties);
+        } catch (SQLException e) {
+            throw new RuntimeException("Can`t get connection to manufacturer_db", e);
+        }
+    }
+}
