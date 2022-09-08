2f325c9901ce146408d8ef0c420c016d4e6af1fb
mr-Claster <67782747+mr-Claster@users.noreply.github.com>
2022-09-08 15:02:57 +0300

    fix toString()

--- src/main/java/mate/jdbc/model/Manufacturer.java
+++ src/main/java/mate/jdbc/model/Manufacturer.java
@@ -56,6 +56,6 @@ public class Manufacturer {
                 + "id=" + id
                 + ", name='" + name + '\''
                 + ", country='" + country + '\''
-                + "}\n";
+                + '}';
     }
 }
