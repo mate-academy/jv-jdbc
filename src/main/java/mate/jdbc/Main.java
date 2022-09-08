09165c54674a5489378e024db9d8077378d9f3a6
Aptypio13 <aptypio668@gmail.com>
2022-09-08 09:25:00 +0300

    create simple testing for DAO

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,55 @@
 package mate.jdbc;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Manufacturer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import java.util.List;
+import java.util.Optional;
+
+public class Main {
+    static Logger loggerMain = LogManager.getLogger(Main.class);
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
+    public static void main(String[] args) {
+        Manufacturer manufacturer = new Manufacturer();
+        ManufacturerDao manufacturerDao = (ManufacturerDao) injector.getInstance(ManufacturerDao.class);
+
+        //set to columns
+        manufacturer.setName("Tavria");
+        manufacturer.setCountry("Ukraine");
+        Manufacturer manufacturerFromDb = manufacturerDao.create(manufacturer);
+        loggerMain.info(manufacturer);
+        loggerMain.info("--------------------");
+
+        //get from table
+        loggerMain.info("Get manufacturer");
+        Optional<Manufacturer> optional = manufacturerDao.get(manufacturerFromDb.getId());
+        loggerMain.info(optional);
+        loggerMain.info("--------------------");
+
+        //get all from table
+        loggerMain.info("GetAll manufacturer");
+        List<Manufacturer> allManufacturer = manufacturerDao.getAll();
+        for (Manufacturer item : allManufacturer) {
+            loggerMain.info(item);
+        }
+        loggerMain.info("--------------------");
+
+        //update table
+        loggerMain.info("Update manufacturer");
+        Manufacturer updateManufacturer = new Manufacturer();
+        updateManufacturer.setId(manufacturer.getId());
+        updateManufacturer.setName("Toyota");
+        updateManufacturer.setCountry("Japan");
+        loggerMain.info(manufacturerDao.update(updateManufacturer));
+        loggerMain.info("--------------------");
+
+        //delete from table
+        loggerMain.info("Delete manufacturer");
+        loggerMain.info(manufacturerDao.delete(manufacturer.getId()));
+        loggerMain.info("--------------------");
     }
 }
