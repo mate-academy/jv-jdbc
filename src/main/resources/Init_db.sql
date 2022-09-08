a9130469a88539ad2cdd70c131947f05d4dcfa18
Aptypio13 <aptypio668@gmail.com>
2022-09-08 09:26:22 +0300

    create file to initialize taxi_db

---
+++ src/main/resources/Init_db.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE `taxi_db` /* !40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /* !80016 DEFAULT ENCRYPTION='N' */;
+CREATE TABLE `manufacturers` (
+`id` bigint NOT NULL AUTO_INCREMENT,
+`name` varchar(100) DEFAULT NULL,
+`country` varchar(100) DEFAULT NULL,
+`is_deleted` tinyint(1) NOT NULL DEFAULT '0',
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
