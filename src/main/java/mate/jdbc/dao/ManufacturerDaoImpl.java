09a297376aec984a8ae1e1dfa161f7ecb1ef1b6d
azarovartur <99978155+azarovartur@users.noreply.github.com>
2022-09-10 22:32:53 +0300

    Update src/main/java/mate/jdbc/dao/ManufacturerDaoImpl.java
    Co-authored-by: Anastasia Len <81781479+aaananas@users.noreply.github.com>

--- src/main/java/mate/jdbc/dao/ManufacturerDaoImpl.java
+++ src/main/java/mate/jdbc/dao/ManufacturerDaoImpl.java
@@ -59,7 +59,7 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
     @Override
     public List<Manufacturer> getAll() {
         List<Manufacturer> allManufacturers = new ArrayList<>();
-        String readAllManufacturersRequest = "SELECT * FROM manufacturers WHERE is_deleted = false";
+        String readAllManufacturersRequest = "SELECT * FROM manufacturers WHERE is_deleted = FALSE";
         try (Connection connection = ConnectionUtil.getConnection();
                  PreparedStatement getAllManufacturedStatement
                          = connection.prepareStatement(readAllManufacturersRequest)) {
