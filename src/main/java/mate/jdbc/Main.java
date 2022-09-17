d4c1f697a5e7ca6599fba65318e095e658952b6d
Vitalii Shkliarskyi <shvetalio@gmail.com>
2022-09-17 21:52:32 +0300

    fixed mentor's comments

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -13,11 +13,11 @@ public class Main {
 
         System.out.println(manufacturerDao.create(Manufacturer.of("AUDI", "Germany")));
 
-        manufacturerDao.get(1L).ifPresent(System.out::println);
+        manufacturerDao.get(2L).ifPresent(System.out::println);
 
         manufacturerDao.getAll().forEach(System.out::println);
 
-        System.out.println(manufacturerDao.update(Manufacturer.of(8L, "ZAZ", "Ukraine")));
+        System.out.println(manufacturerDao.update(Manufacturer.of(8L, "Vepr", "Ukraine")));
 
         System.out.println(manufacturerDao.delete(4L));
     }
--- src/main/java/mate/jdbc/dao/impl/ManufacturerDaoImpl.java
+++ src/main/java/mate/jdbc/dao/impl/ManufacturerDaoImpl.java
@@ -44,43 +44,29 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
                         = connection.prepareStatement(getByIdQuery)) {
             getManufacturerStatement.setLong(1, id);
             ResultSet resultSet = getManufacturerStatement.executeQuery();
-            if (resultSet.next()) {
-                String name = resultSet.getString(2);
-                String country = resultSet.getString(3);
-                Manufacturer manufacturer = Manufacturer.of(id, name, country);
-                return Optional.of(manufacturer);
-            }
+            Manufacturer manufacturer = parseFromResultSet(resultSet);
+            return manufacturer != null ? Optional.of(manufacturer) : Optional.empty();
         } catch (SQLException ex) {
             throw new DataProcessingException("Can't get manufacturer from DB by id " + id, ex);
         }
-        return Optional.empty();
     }
 
     @Override
     public List<Manufacturer> getAll() {
-        List<Manufacturer> allManufacturers = new ArrayList<>();
         String getAllQuery = "SELECT * FROM manufacturers WHERE is_deleted = FALSE;";
         try (Connection connection = ConnectionUtil.getConnection();
-                Statement getAllManufacturers = connection.createStatement()) {
-            ResultSet resultSet = getAllManufacturers.executeQuery(getAllQuery);
-            while (resultSet.next()) {
-                Long id = resultSet.getObject(1, Long.class);
-                String name = resultSet.getString(2);
-                String country = resultSet.getString(3);
-                Manufacturer manufacturer = Manufacturer.of(id, name, country);
-                allManufacturers.add(manufacturer);
-            }
+                PreparedStatement getAllManufacturers = connection.prepareStatement(getAllQuery)) {
+            ResultSet resultSet = getAllManufacturers.executeQuery();
+            return collectFromResultSet(resultSet);
         } catch (SQLException ex) {
             throw new DataProcessingException("Can't get all manufacturers from DB", ex);
         }
-        return allManufacturers;
     }
 
     @Override
     public Manufacturer update(Manufacturer manufacturer) {
         String updateQuery = "UPDATE manufacturers SET name = ?, country = ? "
                 + "WHERE id = ? AND is_deleted = FALSE;";
-        Optional<Manufacturer> oldManufacturer = get(manufacturer.getId());
         try (Connection connection = ConnectionUtil.getConnection();
                 PreparedStatement updateManufacturerStatement
                         = connection.prepareStatement(updateQuery)) {
@@ -92,7 +78,7 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
             throw new DataProcessingException("Can't update manufacturer in DB"
                     + manufacturer.getId(), ex);
         }
-        return oldManufacturer.get();
+        return manufacturer;
     }
 
     @Override
@@ -105,6 +91,36 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
             return deleteManufacturerStatement.executeUpdate() > 0;
         } catch (SQLException ex) {
             throw new DataProcessingException("Can't delete manufacturer from DB by id " + id, ex);
+        }
+    }
+
+    private Manufacturer parseFromResultSet(ResultSet resultSet) {
+        try {
+            if (resultSet.next()) {
+                Long id = resultSet.getObject(1, Long.class);
+                String name = resultSet.getString(2);
+                String country = resultSet.getString(3);
+                return Manufacturer.of(id, name, country);
+            }
+            return null;
+        } catch (SQLException ex) {
+            throw new DataProcessingException("Can't parse manufacturer from ResultSet ", ex);
+        }
+    }
+
+    private List<Manufacturer> collectFromResultSet(ResultSet resultSet) {
+        List<Manufacturer> allManufacturers = new ArrayList<>();
+        try {
+            while (resultSet.next()) {
+                Long id = resultSet.getObject(1, Long.class);
+                String name = resultSet.getString(2);
+                String country = resultSet.getString(3);
+                Manufacturer manufacturer = Manufacturer.of(id, name, country);
+                allManufacturers.add(manufacturer);
+            }
+            return allManufacturers;
+        } catch (SQLException ex) {
+            throw new DataProcessingException("Can't collect manufacturers to list ", ex);
         }
     }
 }
