8ee4d361361b8c02be543f34f4bc5f129ef84806
Kate Chernyavska <ccarts.kpi@gmail.com>
2022-09-27 16:16:22 +0300

    set values for execute queries in MySql

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,26 @@
 package mate.jdbc;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Manufacturer;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
+    public static void main(String[] args) {
+        ManufacturerDao manufacturerDao = (ManufacturerDao) injector
+                .getInstance(ManufacturerDao.class);
+        Manufacturer manufacturer = new Manufacturer();
+        manufacturer.setName("Tesla");
+        manufacturer.setCountry("USA");
+        manufacturerDao.create(manufacturer);
+        manufacturerDao.get(manufacturer.getId());
+        manufacturerDao.getAll();
+        manufacturer.setName("Mazda");
+        manufacturer.setCountry("Japanese");
+        manufacturerDao.update(manufacturer);
+        manufacturer.setName("BMW");
+        manufacturer.setCountry("Germany");
+        manufacturerDao.delete(manufacturer.getId());
     }
 }
