a016127bbc6de50aab7a853afa4a7ee19540bed9
shmax <mshveda28@gmail.com>
2022-09-30 17:35:13 +0300

    Add init_db.sql

---
+++ src/main/resources/init_db.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE `taxi_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+CREATE TABLE `manufacturer` (
+                                `id` bigint NOT NULL AUTO_INCREMENT,
+                                `name` varchar(45) NOT NULL,
+                                `country` varchar(45) NOT NULL,
+                                PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
