da5a6492da5cd643861cd87f4c8f876c219e979a
shmax <mshveda28@gmail.com>
2022-10-05 14:34:42 +0300

    Add generated equals and hashcode for test purpose

--- src/main/java/mate/jdbc/model/Manufacturer.java
+++ src/main/java/mate/jdbc/model/Manufacturer.java
@@ -1,5 +1,7 @@
 package mate.jdbc.model;
 
+import java.util.Objects;
+
 public class Manufacturer {
     private Long id;
     private String name;
@@ -35,6 +37,27 @@ public class Manufacturer {
     public void setCountry(String country) {
         this.country = country;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Manufacturer that = (Manufacturer) o;
+
+        if (!Objects.equals(id, that.id)) return false;
+        if (!Objects.equals(name, that.name)) return false;
+        return Objects.equals(country, that.country);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (country != null ? country.hashCode() : 0);
+        return result;
+    }
+
     @Override
     public String toString() {
         return "Manufacturer{" +
