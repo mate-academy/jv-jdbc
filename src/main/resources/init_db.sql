4ee11a4ab77a5e5665c93a459e86dcab50f40e7c
shmax <mshveda28@gmail.com>
2022-10-05 12:35:31 +0300

    Update init_db.sql; add is_deleted column

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -4,5 +4,6 @@ CREATE TABLE `manufacturer` (
                                 `id` bigint NOT NULL AUTO_INCREMENT,
                                 `name` varchar(45) NOT NULL,
                                 `country` varchar(45) NOT NULL,
+                                `is_deleted` tinyint NOT NULL,
                                 PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
