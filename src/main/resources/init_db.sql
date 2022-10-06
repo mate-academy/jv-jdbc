b7c40b9ba120d778f0b39dd6ba24456165d49145
Dmytro Dmytrenko <dmitrynetfx@gmail.com>
2022-10-06 17:30:30 +0300

    corrected table manufacturers

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -17,6 +17,8 @@ CREATE TABLE `taxi_db`.`manufacturers` (
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);
 ALTER TABLE `taxi_db`.`manufacturers`
   ADD COLUMN `country` VARCHAR(45) NOT NULL AFTER `name`;
+ALTER TABLE `taxi_db`.`manufacturers`
+CHANGE COLUMN `is_deleted` `is_deleted` TINYINT NOT NULL DEFAULT 0 ;
 
 /* create table cars */
   CREATE TABLE `taxi_db`.`cars` (
