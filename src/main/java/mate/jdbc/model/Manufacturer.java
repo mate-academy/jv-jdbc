d1a8fda6ab8097c5b1c91ce7c1403e17bdadac60
Maksym Ivanov <ivanov1maxim@gmail.com>
2022-10-20 23:51:19 +0300

    Change id field initialization logic;

--- src/main/java/mate/jdbc/model/Manufacturer.java
+++ src/main/java/mate/jdbc/model/Manufacturer.java
@@ -3,19 +3,21 @@ package mate.jdbc.model;
 import java.util.Objects;
 
 public class Manufacturer {
-    private final Long id;
+    private Long id;
     private final String name;
-
     private final String country;
 
-    public Manufacturer(final Long id, final String name, final String country) {
-        this.id = id;
+    public Manufacturer(final String name, final String country) {
         this.name = name;
         this.country = country;
     }
 
     public Long getId() {
         return id;
+    }
+
+    public void setId(final Long id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -34,23 +36,5 @@ public class Manufacturer {
         sb.append(", country='").append(country).append('\'');
         sb.append('}');
         return sb.toString();
-    }
-
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        final Manufacturer that = (Manufacturer) o;
-        if (!Objects.equals(id, that.id)) return false;
-        if (!Objects.equals(name, that.name)) return false;
-        return Objects.equals(country, that.country);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        result = 31 * result + (country != null ? country.hashCode() : 0);
-        return result;
     }
 }
