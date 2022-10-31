251f27d5b40aaf84706e6cce0f4107aaf4a19229
shmax <mshveda28@gmail.com>
2022-10-31 17:30:48 +0200

    Clean init_db.sql

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,4 +1,5 @@
-CREATE DATABASE `taxi_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE IF NOT EXISTS `taxi_db` DEFAULT CHARACTER SET utf8;
+USE taxi_db;
 
 CREATE TABLE `manufacturer` (
                                 `id` bigint NOT NULL AUTO_INCREMENT,
@@ -6,4 +7,4 @@ CREATE TABLE `manufacturer` (
                                 `country` varchar(45) NOT NULL,
                                 `is_deleted` tinyint NOT NULL,
                                 PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
