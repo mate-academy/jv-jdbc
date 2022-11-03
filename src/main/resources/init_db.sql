1889cb6dd65e7cf183ee65fad5f5c724c18d35e9
Nazarii Pertsak <pertsak98@gmail.com>
2022-11-03 10:01:52 +0200

    changed init_db.sql

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE `taxi_service_db` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE IF NOT EXISTS `taxi_service_db`;
 
 CREATE TABLE `manufacturers` (
   `id` bigint NOT NULL AUTO_INCREMENT,
@@ -6,4 +6,5 @@ CREATE TABLE `manufacturers` (
   `country` varchar(255) NOT NULL,
   `is_deleted` tinyint NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
