3efedf58d4ec766ed981c20ba817d67b1f30bf04
unknown <streetreadger@gmail.com>
2022-11-04 13:16:19 +0200

    Change lineSeparator in files, and int to BIGINT

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE `taxi_service_db` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
 CREATE TABLE `manufacturers` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   `counrty` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
