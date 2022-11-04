b1ace377f599e016995f554cf243607776f9ebb6
unknown <streetreadger@gmail.com>
2022-11-04 13:18:43 +0200

    Add Dao generic interface

--- src/main/java/mate/jdbc/dao/Dao.java
+++ src/main/java/mate/jdbc/dao/Dao.java
@@ -1,12 +1,12 @@
 package mate.jdbc.dao;
 
-import mate.jdbc.models.Manufacturer;
-
 import java.util.List;
 import java.util.Optional;
 
 public interface Dao<T> {
     T create(T t);
     Optional<T> get(Long id);
-    List<Manufacturer> getALl();
+    List<T> getALl();
+    T update(T t);
+    boolean delete(Long id);
 }
