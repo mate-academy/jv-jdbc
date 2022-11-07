a4f9a972e0d5afff011bfd5d64474833555d8124
galenkoigor <galenkoigor@gmail.com>
2022-11-07 22:50:37 +0100

    Made ConnectionUtil and have tested a connection to the DB

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,16 @@
 package mate.jdbc;
 
+import mate.jdbc.exceptions.DataProcessingException;
+import mate.jdbc.util.ConnectionUtil;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-
+        try (Connection connection = ConnectionUtil.getConnection();) {
+        } catch (SQLException e) {
+            throw new DataProcessingException("", e);
+        }
     }
 }
