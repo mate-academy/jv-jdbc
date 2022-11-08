6e65f72a1928c65ec19a661599cd888503c4dd1e
galenkoigor <galenkoigor@gmail.com>
2022-11-08 14:03:52 +0100

    testing app via main

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -4,6 +4,8 @@ import mate.jdbc.dao.ManufacturerDao;
 import mate.jdbc.lib.Injector;
 import mate.jdbc.model.Manufacturer;
 
+import java.util.List;
+
 public class Main {
     private static final Injector injector = Injector.getInstance("mate.jdbc");
 
@@ -12,9 +14,14 @@ public class Main {
                 (ManufacturerDao) injector.getInstance(ManufacturerDao.class);
         Manufacturer lexus = new Manufacturer();
         lexus.setName("Lexus");
-        lexus.setCountry("Japan");
+        lexus.setCountry("USA");
         manufacturerDao.create(lexus);
         Manufacturer manufacturer = manufacturerDao.get(1L).orElse(new Manufacturer());
+        List<Manufacturer> manufacturerList = manufacturerDao.getAll();
+        manufacturerList.forEach(System.out::println);
+        lexus.setCountry("Japan");
+        manufacturerDao.update(lexus);
+        manufacturerDao.delete(1L);
         System.out.println(manufacturer);
     }
 }
