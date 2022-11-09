d1846b36e2fe95799ba66c8fa7f8f18e55d7b334
Alex Anofriev <alex.linkoln7391@gmail.com>
2022-11-09 14:42:46 +0200

    Manufacturer dao implementation

--- src/main/java/mate/jdbc/util/ConnectionUtil.java
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -6,10 +6,13 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionUtil {
+    private static final String MYSQL_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String USER_NAME = "root";
+    private static final String USER_PASSWORD = "mysql";
 
     static {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName(MYSQL_DRIVER);
         } catch (ClassNotFoundException e) {
             throw new RuntimeException("Can not load JDBC driver", e);
         }
@@ -17,8 +20,8 @@ public class ConnectionUtil {
 
     public static Connection getConnection() {
         Properties properties = new Properties();
-        properties.put("user", "root");
-        properties.put("password", "mysql");
+        properties.put("user", USER_NAME);
+        properties.put("password", USER_PASSWORD);
         try {
             return DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/taxi_service",
                     properties);
