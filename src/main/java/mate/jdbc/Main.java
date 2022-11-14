4b3c463dd40310ffcab77d79c5d0a183517180b4
thebitler <yurchyknikita@gmail.com>
2022-11-14 15:03:42 +0100

    added idx & ids constants

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -7,6 +7,10 @@ import mate.model.Manufacturer;
 
 public class Main {
     private static final Injector injector = Injector.getInstance("mate");
+    private static final long ID_TO_GET_MANUFACTURER = 2L;
+    private static final long ID_TO_SET_UPDATED = 257L;
+    private static final long ID_TO_DELETE = 1L;
+    private static final int INDEX_OF_MANUFACTURER_TO_UPDATE = 0;
 
     public static void main(String[] args) {
         ManufacturerDao manufacturerDao =
@@ -22,13 +26,13 @@ public class Main {
         }
         System.out.println("manufacturerDao.getAll() = "
                 + manufacturerDao.getAll());
-        System.out.println("manufacturerDao.get(2L) = "
-                + manufacturerDao.get(2L));
-        manufacturers.get(0).setId(257L);
+        System.out.println("manufacturerDao.get(" + ID_TO_GET_MANUFACTURER + ") = "
+                + manufacturerDao.get(ID_TO_GET_MANUFACTURER));
+        manufacturers.get(INDEX_OF_MANUFACTURER_TO_UPDATE).setId(ID_TO_SET_UPDATED);
         System.out.println(
-                "manufacturerDao.update(manufacturers.get(0)) = "
-                        + manufacturerDao.update(manufacturers.get(0)));
-        System.out.println("manufacturerDao.delete(1L) = "
-                + manufacturerDao.delete(1L));
+                "manufacturerDao.update(manufacturers.get(" + INDEX_OF_MANUFACTURER_TO_UPDATE + ")) = "
+                        + manufacturerDao.update(manufacturers.get(INDEX_OF_MANUFACTURER_TO_UPDATE)));
+        System.out.println("manufacturerDao.delete(" + ID_TO_DELETE + ") = "
+                + manufacturerDao.delete(ID_TO_DELETE));
     }
 }
