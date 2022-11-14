a8ca030cc16118c1f75242e089fe40d1e3d9a9da
Yaroslav Pelekhach <108753784+Yaroslav781@users.noreply.github.com>
2022-11-14 15:39:54 +0200

    moved to constant

--- src/main/java/mate/jdbc/util/ConnectionUtil.java
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -7,10 +7,13 @@ import java.util.Properties;
 
 public class ConnectionUtil {
     private static final String URL = "jdbc:mysql://localhost:3306/taxi_service_db";
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String NAME = "root";
+    private static final String PASSWORD = "123456";
 
     static {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName(DRIVER);
         } catch (ClassNotFoundException e) {
             throw new RuntimeException("Can`t load JDBC driver for MySQL", e);
         }
@@ -19,8 +22,8 @@ public class ConnectionUtil {
     public static Connection getConnection() {
         try {
             Properties dbProperties = new Properties();
-            dbProperties.put("user", "root");
-            dbProperties.put("password", "123456");
+            dbProperties.put("user", NAME);
+            dbProperties.put("password", PASSWORD);
             return DriverManager.getConnection(
                     URL, dbProperties);
         } catch (SQLException e) {
