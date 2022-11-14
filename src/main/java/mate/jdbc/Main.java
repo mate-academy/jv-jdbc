a12a3fa678a8a285b00f782f12c380e2afcf4be7
Serhii <romanchuk552@gmail.com>
2022-11-14 04:14:46 +0200

    taxi service - completed

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,32 @@
 package mate.jdbc;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.List;
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.models.Manufacturer;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc.dao");
+
+    public static void main(String[] args) {
+        ManufacturerDao manufacturerDao = (ManufacturerDao) injector
+                .getInstance(ManufacturerDao.class);
+        Manufacturer car1 = new Manufacturer("Car1", "SomeCountry");
+        manufacturerDao.create(car1);
+        Manufacturer car2 = new Manufacturer("Car2", "SomeCountry");
+        manufacturerDao.create(car2);
+        Manufacturer car3 = new Manufacturer("Car3", "SomeCountry");
+        manufacturerDao.create(car3);
+
+        List<Manufacturer> manufacturers = manufacturerDao.getAll();
+        for (Manufacturer manufacturer : manufacturers) {
+            System.out.println(manufacturer);
+        }
+
+        System.out.println(manufacturerDao.get(car1.getId()));
+
+        manufacturerDao.update(car1);
+
+        manufacturerDao.delete(car1.getId());
     }
 }
