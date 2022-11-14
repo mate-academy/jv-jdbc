d9ae2b7507774f8992bd9c638072b65282c66b76
yehbreus <yeh.breus@gmail.com>
2022-11-14 09:27:23 +0200

    fix after mentor review

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE `taxi_service_db` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
-CREATE TABLE `taxi_db` (
+CREATE TABLE `manufacturers` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   `country` varchar(255) DEFAULT NULL,
