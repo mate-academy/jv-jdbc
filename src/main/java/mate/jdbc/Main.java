ce8a707d5c35a50e95c91a261b0af2b5da4eb18e
thebitler <yurchyknikita@gmail.com>
2022-11-14 15:59:32 +0100

    fixed pass dynamic ids

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -7,10 +7,10 @@ import mate.model.Manufacturer;
 
 public class Main {
     private static final Injector injector = Injector.getInstance("mate");
-    private static final long ID_TO_GET_MANUFACTURER = 2L;
-    private static final long ID_TO_SET_UPDATED = 257L;
-    private static final long ID_TO_DELETE = 1L;
-    private static final int INDEX_OF_MANUFACTURER_TO_UPDATE = 0;
+    private static final int INDEX_TO_GET_MANUFACTURER = 2;
+    private static final int INDEX_TO_DELETE = 1;
+    private static final int INDEX_OF_MANUFACTURER_TO_UPDATE = 2;
+    private static final String NEW_NAME_TO_UPDATE = "Volkswagen G.A.";
 
     public static void main(String[] args) {
         ManufacturerDao manufacturerDao =
@@ -24,17 +24,21 @@ public class Main {
         for (Manufacturer manufacturer : manufacturers) {
             manufacturerDao.create(manufacturer);
         }
+        List<Manufacturer> manufacturersWithIds = manufacturerDao.getAll();
         System.out.println("manufacturerDao.getAll() = "
-                + manufacturerDao.getAll());
-        System.out.println("manufacturerDao.get(" + ID_TO_GET_MANUFACTURER + ") = "
-                + manufacturerDao.get(ID_TO_GET_MANUFACTURER));
-        manufacturers.get(INDEX_OF_MANUFACTURER_TO_UPDATE).setId(ID_TO_SET_UPDATED);
+                + manufacturersWithIds);
+        long idToGetManufacturer = manufacturersWithIds.get(INDEX_TO_GET_MANUFACTURER).getId();
+        System.out.println("manufacturerDao.get(" + idToGetManufacturer + ") = "
+                + manufacturerDao.get(idToGetManufacturer));
+        Manufacturer manufacturerToUpdate =
+                manufacturersWithIds.get(INDEX_OF_MANUFACTURER_TO_UPDATE);
+        manufacturerToUpdate.setName(NEW_NAME_TO_UPDATE);
         System.out.println(
                 "manufacturerDao.update(manufacturers.get("
                         + INDEX_OF_MANUFACTURER_TO_UPDATE + ")) = "
-                        + manufacturerDao.update(
-                                manufacturers.get(INDEX_OF_MANUFACTURER_TO_UPDATE)));
-        System.out.println("manufacturerDao.delete(" + ID_TO_DELETE + ") = "
-                + manufacturerDao.delete(ID_TO_DELETE));
+                        + manufacturerDao.update(manufacturerToUpdate));
+        long idToDelete = manufacturersWithIds.get(INDEX_TO_DELETE).getId();
+        System.out.println("manufacturerDao.delete(" + idToDelete + ") = "
+                + manufacturerDao.delete(idToDelete));
     }
 }
