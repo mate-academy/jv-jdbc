c206db7fe78592d97eec211ac151e7a37c712abc
DY-JS <delo1604@ukr.net>
2022-11-21 14:04:04 +0200

    Solution jv-jdbc

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -9,8 +9,8 @@ public class Main {
     private static Injector injector = Injector.getInstance("mate.jdbc");
 
     public static void main(String[] args) {
-        final ManufacturerDao manufacturerDao = (ManufacturerDao) injector.getInstance(
-                ManufacturerDao.class);
+        final ManufacturerDao manufacturerDao =
+                (ManufacturerDao) injector.getInstance(ManufacturerDao.class);
         Manufacturer toyota = new Manufacturer();
         toyota.setName("toyota");
         toyota.setCountry("Japan");
