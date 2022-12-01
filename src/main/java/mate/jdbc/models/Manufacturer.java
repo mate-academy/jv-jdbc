0c19aa19979167f0756a2dc35bdfa1da7c2c22c8
AndriySichniy <sichniy.andriy@gmail.com>
2022-12-01 21:58:31 +0200

    added builder in Manufacturer

--- src/main/java/mate/jdbc/models/Manufacturer.java
+++ src/main/java/mate/jdbc/models/Manufacturer.java
@@ -5,6 +5,41 @@ public class Manufacturer {
     private String name;
     private String country;
 
+    public Manufacturer() {
+
+    }
+
+    private Manufacturer(Builder builder) {
+        this.id = builder.id;
+        this.name = builder.name;
+        this.country = builder.country;
+    }
+
+        public static class Builder {
+            private Long id;
+            private String name;
+            private String country;
+
+            public Builder setId(Long id) {
+                this.id = id;
+                return this;
+            }
+
+            public Builder setName(String name) {
+                this.name = name;
+                return this;
+            }
+
+            public Builder setCountry(String country) {
+                this.country = country;
+                return this;
+            }
+
+            public Manufacturer build(){
+                return new Manufacturer(this);
+            }
+        }
+
     public Long getId() {
         return id;
     }
