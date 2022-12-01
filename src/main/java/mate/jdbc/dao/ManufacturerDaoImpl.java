b3c6604f2f9d4247c7c319fcbae0b8b0cec6a206
AndriySichniy <sichniy.andriy@gmail.com>
2022-12-01 21:59:18 +0200

    added create in ManufacturerDaoImpl

--- src/main/java/mate/jdbc/dao/ManufacturerDaoImpl.java
+++ src/main/java/mate/jdbc/dao/ManufacturerDaoImpl.java
@@ -1,16 +1,41 @@
 package mate.jdbc.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 import mate.jdbc.lib.Dao;
 import mate.jdbc.models.Manufacturer;
+import mate.jdbc.util.ConnectTo_DB;
+import mate.jdbc.util.ConnectTo_MySQL;
 
 @Dao
 public class ManufacturerDaoImpl implements ManufacturerDao {
+    ConnectTo_DB connectTo_db;
+
+    public ManufacturerDaoImpl() {
+        connectTo_db = new ConnectTo_MySQL();
+    }
 
     @Override
     public Manufacturer create(Manufacturer manufacturer) {
-        return null;
+        String createQueryString = "INSERT INTO manufacturers(name, manufacturer) values(?, ?)";
+        try (Connection connection = connectTo_db.getConnection();
+             PreparedStatement createRecordStatement =
+                     connection.prepareStatement(createQueryString, PreparedStatement.RETURN_GENERATED_KEYS)) {
+            createRecordStatement.setString(1, manufacturer.getName());
+            createRecordStatement.setString(2, manufacturer.getCountry());
+            createRecordStatement.executeUpdate();
+            ResultSet generatedKeys = createRecordStatement.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                manufacturer.setId(generatedKeys.getObject(1, Long.class));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Can`t insert record to manufacturers table", e);
+        }
+        return manufacturer;
     }
 
     @Override
