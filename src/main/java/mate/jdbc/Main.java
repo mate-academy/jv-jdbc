52926ad8f7e81aa2c413019ef8735d623c3a1a1e
Natalia Kurhanska <kurhnatali@gmail.com>
2022-12-25 23:05:13 +0200

    hw-jdbc done

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,23 @@
 package mate.jdbc;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Manufacturer;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
+    public static void main(String[] args) {
+        ManufacturerDao manufacturerDao
+                = (ManufacturerDao) injector.getInstance(ManufacturerDao.class);
+        Manufacturer manufacturer = new Manufacturer();
+        manufacturer.setName("Ford");
+        manufacturer.setCountry("USA");
+        manufacturerDao.create(manufacturer);
+        manufacturer = manufacturerDao.get(2L).get();
+        manufacturer.setName("Jeneral Motors");
+        manufacturerDao.update(manufacturer);
+        manufacturerDao.delete(1L);
+        manufacturerDao.getAll().forEach(System.out::println);
     }
 }
