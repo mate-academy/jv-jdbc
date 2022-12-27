254e182135758db5df32dfd1483657800d78b6da
Dmytro M <dmytro.malinnikov@gmail.com>
2022-12-27 15:14:03 +0200

    implement ManufacturerDao

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,33 @@
 package mate.jdbc;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.Arrays;
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Manufacturer;
 
+public class Main {
+    private static final Injector injector =
+            Injector.getInstance("mate.jdbc");
+    private static final ManufacturerDao manufacturerDao =
+            (ManufacturerDao) injector.getInstance(ManufacturerDao.class);
+
+    public static void main(String[] args) {
+        Manufacturer manufacturerOne = new Manufacturer();
+        manufacturerOne.setName("manufacturerOne");
+        manufacturerOne.setCountry("Germany");
+        System.out.println(manufacturerDao.create(manufacturerOne));
+        Manufacturer manufacturerTwo = new Manufacturer();
+        manufacturerOne.setName("manufacturerTwo");
+        manufacturerOne.setCountry("France");
+        System.out.println(manufacturerDao.create(manufacturerTwo));
+        System.out.println(manufacturerDao.create(manufacturerOne));
+        System.out.println(Arrays.toString(manufacturerDao.getAll().toArray()));
+        manufacturerTwo.setCountry("Italy");
+        System.out.println(manufacturerDao.update(manufacturerTwo));
+        System.out.println(manufacturerDao.delete(1L));
+        System.out.println(manufacturerDao.delete(33L));
+        System.out.println(manufacturerDao.get(1L));
+        System.out.println(manufacturerDao.get(3L));
+        System.out.println(Arrays.toString(manufacturerDao.getAll().toArray()));
     }
 }
