73e4ee6be87582a8c4fe91f846cb0eab6634b3f2
Sofiia <sofiakazzz@gmail.com>
2023-01-08 19:29:30 +0200

    Created Manufacturer(updated init_db.sql)

--- src/main/java/resources/init_db.sql
+++ src/main/java/resources/init_db.sql
@@ -1,11 +1,9 @@
-CREATE DATABASE `manufacture` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-
+CREATE DATABASE `taxi_service_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 CREATE TABLE `manufacturers` (
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) DEFAULT NOT NULL,
-  `country` varchar(45) DEFAULT NOT NULL,
-  `is_deleted` tinyint DEFAULT FALSE,
+  `name` varchar(45) NOT NULL,
+  `country` varchar(45) NOT NULL,
+  `is_deleted` tinyint DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
+) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
