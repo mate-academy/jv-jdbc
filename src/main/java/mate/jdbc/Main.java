0dcd5f02b2d4740a02c4b69148e8cd2b2872b18e
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2023-01-09 23:09:57 +0000

    Add create function

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -10,6 +10,7 @@ public class Main {
         manufacturer.setCountry("Ukraine");
         manufacturer.setName("VEPR");
         ManufacturerDao manufacturerDao = new ManufacturerDaoImpl();
+        manufacturerDao.create(manufacturer);
 
     }
 }
--- src/main/java/mate/jdbc/dao/ManufacturerDaoImpl.java
+++ src/main/java/mate/jdbc/dao/ManufacturerDaoImpl.java
@@ -1,16 +1,37 @@
 package mate.jdbc.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 import java.util.Optional;
 import mate.jdbc.model.Manufacturer;
+import mate.jdbc.util.ConnectionUtil;
 
 public class ManufacturerDaoImpl implements ManufacturerDao {
+
     @Override
     public Manufacturer create(Manufacturer manufacturer) {
-        return null;
+        String insertFormatRequest = "INSERT INTO manufacturers(name, country) values(?, ?);";
+        try (Connection connection = ConnectionUtil.getConnection();
+             PreparedStatement createFormatsStatement = connection
+                     .prepareStatement(insertFormatRequest, Statement
+                             .RETURN_GENERATED_KEYS)) {
+            createFormatsStatement.setString(1, manufacturer.getName());
+            createFormatsStatement.setString(2, manufacturer.getCountry());
+            createFormatsStatement.executeUpdate();
+            ResultSet generatedKeys = createFormatsStatement.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                Long id = generatedKeys.getObject(1, Long.class);
+                manufacturer.setId(id);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Can`t insert format to DB", e);
+        }
+        return manufacturer;
     }
-    //Use Statement.RETURN_GENERATED_KEYS only in
-    // create statement, it’s not needed in other methods.
 
     @Override
     public Optional<Manufacturer> get(Long id) {
