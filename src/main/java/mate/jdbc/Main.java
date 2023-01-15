16b00ed6ca3009577cfceb4ea33a082f67102303
Anastasiia Metelska <metelska.anastasiia@gmail.com>
2023-01-15 20:14:29 +0200

    added dependencies in pom.xml added sql file in resources added ConnectionUtil added custom Exception added Dao and its implementation added Manufacturer class

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,28 @@
 package mate.jdbc;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Manufacturer;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
+    public static void main(String[] args) {
+        ManufacturerDao manufacturerDao = (ManufacturerDao) injector
+                .getInstance(ManufacturerDao.class);
+        Manufacturer manufacturer = new Manufacturer();
+        manufacturer.setName("Volvo");
+        manufacturer.setCountry("China");
+        System.out.println(manufacturerDao.create(manufacturer));
+        System.out.println(manufacturerDao.get(3L));
+        System.out.println(manufacturerDao.getAll());
+        System.out.println(manufacturerDao.delete(8L));
+        System.out.println(manufacturerDao.getAll());
+        Manufacturer manufacturerUpdate = new Manufacturer();
+        manufacturerUpdate.setId(10L);
+        manufacturerUpdate.setName("Seat");
+        manufacturerUpdate.setCountry("France");
+        System.out.println(manufacturerDao.update(manufacturerUpdate));
+        System.out.println(manufacturerDao.getAll());
     }
 }
