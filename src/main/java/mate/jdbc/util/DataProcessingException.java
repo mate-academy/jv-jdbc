99fa9d78b52ea6616a8c914bf52b69f604d72906
vasyl.moshun <vasyl.moshun@gmail.com>
2023-01-15 13:32:54 +0200

    1st commit

--- src/main/java/mate/jdbc/util/DataProcessingException.java
+++ src/main/java/mate/jdbc/util/DataProcessingException.java
@@ -1,11 +1,12 @@
 package mate.jdbc.util;
 
+import java.sql.SQLException;
+
 public class DataProcessingException extends RuntimeException {
     private String message;
     private Throwable ex;
 
-    public DataProcessingException(String message, Throwable ex) {
-        this.message = message;
-        this.ex = ex;
+    public DataProcessingException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
