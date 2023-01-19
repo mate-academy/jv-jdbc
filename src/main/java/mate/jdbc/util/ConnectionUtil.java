5f0a37e0689a109f9615606eb8aa11efdf5bb827
Maksim Surzhik <kabaneri15@gmail.com>
2023-01-19 12:32:15 +0200

    change connectionUtil class

--- src/main/java/mate/jdbc/util/ConnectionUtil.java
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -7,9 +7,14 @@ import java.util.Properties;
 import mate.jdbc.exception.DataProgressingException;
 
 public class ConnectionUtil {
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String USER = "root";
+    private static final String PASSWORD = "maks";
+    private static final String URL_CONNECTION = "jdbc:mysql://localhost:3306/taxi_service";
+
     static {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName(DRIVER);
         } catch (ClassNotFoundException e) {
             throw new DataProgressingException("Can't load JDBC driver for MySQL", e);
         }
@@ -18,9 +23,9 @@ public class ConnectionUtil {
     public static Connection getConnection() {
         try {
             Properties propertiesDb = new Properties();
-            propertiesDb.put("user", "root");
-            propertiesDb.put("password", "maks");
-            return DriverManager.getConnection("jdbc:mysql://localhost:3306/taxi_service",
+            propertiesDb.put("user", USER);
+            propertiesDb.put("password", PASSWORD);
+            return DriverManager.getConnection(URL_CONNECTION,
                     propertiesDb);
         } catch (SQLException e) {
             throw new DataProgressingException("Can't create connection to DB", e);
