c1414988046ce3ccfd3641a536dbe0201418a336
Dmitriy Krasnopir <dmitriy.krasnopir@gmail.com>
2023-01-23 01:00:43 +0200

    connection to DB was established

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,30 @@
 package mate.jdbc;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Manufacturer;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
+    public static void main(String[] args) {
+        ManufacturerDao manufacturerDao =
+                (ManufacturerDao) injector.getInstance(ManufacturerDao.class);
+        Manufacturer manufacturerUsa = new Manufacturer();
+        manufacturerUsa.setName("JEEP");
+        manufacturerUsa.setCountry("USA");
+        System.out.println(manufacturerDao.create(manufacturerUsa));
+        Manufacturer manufacturerGermany = new Manufacturer();
+        manufacturerGermany.setName("BMW");
+        manufacturerGermany.setCountry("GERMANY");
+        System.out.println(manufacturerDao.create(manufacturerGermany));
+        System.out.println(manufacturerDao.get(1L));
+        Manufacturer manufacturerSweden = new Manufacturer();
+        manufacturerSweden.setId(1L);
+        manufacturerSweden.setName("VOLVO");
+        manufacturerSweden.setCountry("SWEDEN");
+        System.out.println(manufacturerDao.update(manufacturerSweden));
+        System.out.println(manufacturerDao.delete(1L));
+        manufacturerDao.getAll().forEach(System.out::println);
     }
 }
