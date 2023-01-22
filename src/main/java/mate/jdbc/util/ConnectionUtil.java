9eaa5f2b155cd94409d22616bc7d6de3b65745bf
Ihor Vynogradskyi <avassorv@gmail.com>
2023-01-22 16:01:19 +0200

    jdbc 2

--- src/main/java/mate/jdbc/util/ConnectionUtil.java
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -7,9 +7,14 @@ import java.util.Properties;
 import mate.jdbc.exception.DataProcessingException;
 
 public class ConnectionUtil {
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String CONNECTION = "jdbc:mysql://localhost:3306/taxi_service";
+    private static final String USER_NAME = "root";
+    private static final String PASSWORD = "11111111";
+
     static {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName(DRIVER);
         } catch (ClassNotFoundException e) {
             throw new DataProcessingException("Can't load driver", e);
         }
@@ -18,10 +23,10 @@ public class ConnectionUtil {
     public static Connection getConnection() {
         try {
             Properties dbProperties = new Properties();
-            dbProperties.put("user", "root");
-            dbProperties.put("password", "11111111");
+            dbProperties.put("user", USER_NAME);
+            dbProperties.put("password", PASSWORD);
             return DriverManager
-                    .getConnection("jdbc:mysql://localhost:3306/taxi_service", dbProperties);
+                    .getConnection(CONNECTION, dbProperties);
         } catch (SQLException e) {
             throw new DataProcessingException("Can't get connection with DataBase", e);
         }
