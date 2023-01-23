33659cc1b25b178712f294b5b885765ea986e6f4
Your NameMykola <mykola.osolinskyi@gmail.com>
2023-01-23 16:52:15 +0100

    add some classes

---
+++ src/main/java/mate/jdbc/Exception/DataProcessingException.java
@@ -0,0 +1,7 @@
+package mate.jdbc.Exception;
+
+public class DataProcessingException extends RuntimeException {
+    public DataProcessingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
