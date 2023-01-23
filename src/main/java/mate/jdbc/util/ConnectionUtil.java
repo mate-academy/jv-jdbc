568b6b6c7428707fe194208adb7092875e025292
Dmitriy Krasnopir <dmitriy.krasnopir@gmail.com>
2023-01-23 19:04:57 +0200

    last commit

--- src/main/java/mate/jdbc/util/ConnectionUtil.java
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -9,7 +9,7 @@ import mate.jdbc.exception.DataProcessingException;
 public class ConnectionUtil {
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String USER_NAME = "root";
-    private static final String USER_PASSWORD = "PERFARATOR4ik";
+    private static final String USER_PASSWORD = "********";
     private static final String URL = "jdbc:mysql://localhost:3306/taxi_service";
 
     static {
