273e20c64ac34195dcfbee21a4925f2ff2531200
Inna Diachkina <innadyachkina@gmail.com>
2023-01-23 14:40:43 +0200

    fixed mistakes

--- src/main/java/mate/jdbc/util/ConnectionUtil.java
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -9,10 +9,11 @@ public class ConnectionUtil {
     private static final String USER_NAME = "root";
     private static final String USER_PASSWORD = "inna1974";
     private static final String URL_DB = "jdbc:mysql://localhost:3306/taxi_servise_db";
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
 
     static {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName(DRIVER);
         } catch (ClassNotFoundException e) {
             throw new RuntimeException("Can't load JDBC driver for MySQL",e);
         }
