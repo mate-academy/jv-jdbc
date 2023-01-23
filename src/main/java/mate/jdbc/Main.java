a0ff859c611397d4eebee4014d848f2ad882e7a8
Dmitriy Krasnopir <dmitriy.krasnopir@gmail.com>
2023-01-23 15:52:12 +0200

    minor mistake

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -6,7 +6,6 @@ import mate.jdbc.model.Manufacturer;
 
 public class Main {
     private static final Injector injector = Injector.getInstance("mate.jdbc");
-    private static final Long ELEMENT_ID = 1L;
 
     public static void main(String[] args) {
         ManufacturerDao manufacturerDao =
@@ -19,7 +18,7 @@ public class Main {
         manufacturerGermany.setName("BMW");
         manufacturerGermany.setCountry("GERMANY");
         System.out.println(manufacturerDao.create(manufacturerGermany));
-        System.out.println(manufacturerDao.get(ELEMENT_ID));
+        System.out.println(manufacturerDao.get(manufacturerUsa.getId()));
         manufacturerUsa.setName("VOLVO");
         manufacturerUsa.setCountry("SWEDEN");
         System.out.println(manufacturerDao.update(manufacturerUsa));
