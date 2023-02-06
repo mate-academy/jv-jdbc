5f16cf94795c4e1b6956e403a4f4a5f393ed7bf7
Denis Pugovkin <den.pugovkin@gmail.com>
2023-02-06 20:34:27 +0200

    1. Dao interface was created with implementation. 2. Methods from dao were implemented. 3. Own exception was created. 4. Created db and file init_db.sql. 5. Model Manufacturer was created.

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,21 @@
 package mate.jdbc;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Manufacturer;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
+    public static void main(String[] args) {
+        Manufacturer manufacturer = new Manufacturer();
+        // initialize field values using setters or constructor
+        manufacturer.setName("Peugeot");
+        manufacturer.setCountry("Hungary");
+        manufacturer.setId(2L);
+        ManufacturerDao manufacturerDao = (ManufacturerDao) injector
+                .getInstance(ManufacturerDao.class);
+        manufacturerDao.update(manufacturer);
+        // test other methods from ManufacturerDao
     }
 }
