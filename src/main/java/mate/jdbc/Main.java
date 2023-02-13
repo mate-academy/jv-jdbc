f09d841187577ee238f9af584530796d3cc1caf8
Dmytro Vyshnivskyi <d.vyshnivskyi@gmail.com>
2023-02-13 21:53:11 +0200

    jdbc_hw2_first_commit

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,23 @@
 package mate.jdbc;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Manufacturer;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
+    public static void main(String[] args) throws NoSuchFieldException {
+        ManufacturerDao manufacturerDao =
+                (ManufacturerDao) injector.getInstance(ManufacturerDao.class);
+        Manufacturer manufacturerBmw = new Manufacturer("BMW", "Germany");
+        manufacturerDao.create(manufacturerBmw);
+        manufacturerDao.getAll().forEach(System.out::println);
+        System.out.println(manufacturerDao.get(7L));
+        Manufacturer manufacturerFord = new Manufacturer(8L, "Ford", "USA");
+        manufacturerDao.update(manufacturerFord);
+        System.out.println(manufacturerDao.get(8L));
+        manufacturerDao.delete(6L);
+        manufacturerDao.getAll().forEach(System.out::println);
     }
 }
