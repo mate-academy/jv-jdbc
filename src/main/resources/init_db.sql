fa48ceefb7b252f22dd6f50b1ef932171f7f6fa1
Andrii Malenko <andmalenko@gmail.com>
2023-02-15 23:05:41 +0200

    Here is my solution for hw-jdbc-taxi-service

---
+++ src/main/resources/init_db.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE `taxi_db` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+CREATE TABLE `taxi_manufacturers` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT 'null',
+  `country` varchar(255) DEFAULT 'null',
+  `is_deleted` tinyint NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
