10b344c8874e918c1848bcbe78b3525f0766f0e7
Nazarii Kharyshyn <nazariykharyshyn@gmail.com>
2023-02-17 00:12:52 +0200

    -created taxi_driver_db scheme with manufacturers table (columns id, name, country, is_deleted) and connected this DB to project -created Manufacturer model with id, name and country fields -created ManufacturerDao interface and its implementation class with CRUD methods -used created @Dao annotation and Injector class to create an instance of ManufacturerDaoImpl class -created custom DataProcessingException that extends RuntimeException

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,42 @@
 package mate.jdbc;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.List;
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.models.Manufacturer;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
+    public static void main(String[] args) {
+        ManufacturerDao manufacturerDao = (ManufacturerDao) injector
+                .getInstance(ManufacturerDao.class);
+        Manufacturer frenchManufacturer = new Manufacturer();
+        frenchManufacturer.setCountry("France");
+        frenchManufacturer.setName("Renault");
+
+        Manufacturer germanManufacturer = new Manufacturer();
+        germanManufacturer.setCountry("Germany");
+        germanManufacturer.setName("Volkswagen");
+
+        Manufacturer italianManufacturer = new Manufacturer();
+        italianManufacturer.setCountry("Italy");
+        italianManufacturer.setName("Fiat");
+
+        List<Manufacturer> manufacturers = List
+                .of(frenchManufacturer, germanManufacturer, italianManufacturer);
+        for (Manufacturer manufacturer : manufacturers) {
+            System.out.println(manufacturerDao.create(manufacturer));
+        }
+
+        manufacturerDao.get(2L).ifPresent(System.out::println);
+        manufacturerDao.get(10L).ifPresent(System.out::println);
+
+        manufacturerDao.getAll().forEach(System.out::println);
+
+        germanManufacturer.setName("Audi");
+        manufacturerDao.update(germanManufacturer);
+        manufacturerDao.delete(3L);
+        manufacturerDao.getAll().forEach(System.out::println);
     }
 }
