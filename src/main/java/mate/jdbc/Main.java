6b68acd7d534f2e2b59619c5a1c255bb4d17582c
Andrii Shevchuk <zovten156@gmail.com>
2023-02-28 09:29:16 +0200

    established a connection with the DB and implemented CRUD

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,32 @@
 package mate.jdbc;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.dao.ManufacturerDaoImpl;
+import mate.jdbc.lib.Injector;
+import mate.models.Manufacturer;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
+    public static void main(String[] args) {
+        final ManufacturerDaoImpl manufacturerDao = (ManufacturerDaoImpl) injector.getInstance(ManufacturerDao.class);
+        Manufacturer fordManufacturer = new Manufacturer();
+        fordManufacturer.setName("Ford");
+        fordManufacturer.setCountry("USA");
+        Manufacturer dodgeManufacturer = new Manufacturer();
+        dodgeManufacturer.setName("Dodge");
+        dodgeManufacturer.setCountry("USA");
+        //create
+        manufacturerDao.create(fordManufacturer);
+        manufacturerDao.create(dodgeManufacturer);
+        //get all
+        manufacturerDao.getAll().forEach(System.out::println);
+        //get by index
+        System.out.println(manufacturerDao.get(dodgeManufacturer.getId()));
+        //update
+        System.out.println(manufacturerDao.update(dodgeManufacturer));
+        //delete
+        manufacturerDao.delete(7L);
+        manufacturerDao.getAll().forEach(System.out::println);
     }
 }
