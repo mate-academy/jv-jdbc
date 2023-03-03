81df1e834c7ce0a29f4cd767a48765d7fb7076cb
Vadym Khodorovskyi <vadimhodor@gmail.com>
2023-03-03 20:07:52 +0200

    create connection to taxi_service db

---
+++ src/main/java/mate/jdbc/utils/ConnectionUtil.java
@@ -0,0 +1,27 @@
+package mate.jdbc.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionUtil {
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Can't load a MySQL Driver", e);
+        }
+    }
+
+    public Connection getConnection() {
+        Properties properties = new Properties();
+        properties.put("user", "root");
+        properties.put("password", "123456");
+        try {
+            return DriverManager.getConnection("jdbc:mysql://localhost:3306/taxi_service", properties);
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't connect to Taxi Service DB", e);
+        }
+    }
+}
