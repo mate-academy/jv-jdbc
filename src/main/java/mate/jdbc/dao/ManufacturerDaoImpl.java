0bf687f54bf505e611b94732048a9fc4f7ff8abb
Vadym Khodorovskyi <vadimhodor@gmail.com>
2023-03-06 18:46:46 +0200

    create getAll method in Dao

--- src/main/java/mate/jdbc/dao/ManufacturerDaoImpl.java
+++ src/main/java/mate/jdbc/dao/ManufacturerDaoImpl.java
@@ -10,15 +10,19 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Dao
 public class ManufacturerDaoImpl implements ManufacturerDao {
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_NAME = "name";
+    private static final String COLUMN_COUNTRY = "country";
 
     @Override
     public Manufacturer create(Manufacturer manufacturer) {
-        String insertNewManufacturer = "INSERT INTO manufacturers(name, country) VALUES (?, ?)";
+        String insertNewManufacturer = "INSERT INTO manufacturers(name, country) VALUES (?, ?);";
         try(Connection connection = ConnectionUtil.getConnection();
             PreparedStatement createManufacturerStatement =
                     connection.prepareStatement(insertNewManufacturer,
@@ -39,30 +43,43 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
 
     @Override
     public Optional<Manufacturer> get(Long id) {
-        String getManufacturerById = "SELECT * FROM manufacturers WHERE id = ? AND is_deleted = FALSE";
+        String getManufacturerById = "SELECT * FROM manufacturers WHERE id = ? AND is_deleted = FALSE;";
         try (Connection connection = ConnectionUtil.getConnection();
              PreparedStatement getManufacturerByIdStatement =
                      connection.prepareStatement(getManufacturerById)) {
             getManufacturerByIdStatement.setLong(1, id);
             ResultSet resultSet = getManufacturerByIdStatement.executeQuery();
             if (resultSet.next()) {
-                String name = resultSet.getString("name");
-                String country = resultSet.getString("country");
-                Manufacturer manufacturer = new Manufacturer();
-                manufacturer.setId(id);
-                manufacturer.setName(name);
-                manufacturer.setCountry(country);
+                String name = resultSet.getString(COLUMN_NAME);
+                String country = resultSet.getString(COLUMN_COUNTRY);
+                Manufacturer manufacturer = convertToManufacturer(id, name, country);
                 return Optional.of(manufacturer);
             }
         } catch (SQLException e) {
-            throw new DataProcessingException("Can't get manufacturer with id " + id, e);
+            throw new DataProcessingException("Can't get manufacturer by id " + id, e);
         }
         return Optional.empty();
     }
 
         @Override
     public List<Manufacturer> getAll() {
-        return null;
+        String getAllManufacturers = "SELECT * FROM manufacturers;";
+        List<Manufacturer> manufacturers = new ArrayList<>();
+        try (Connection connection = ConnectionUtil.getConnection();
+             PreparedStatement getAllManufacturersStatement =
+                     connection.prepareStatement(getAllManufacturers)) {
+            ResultSet resultSet = getAllManufacturersStatement.executeQuery();
+            while (resultSet.next()) {
+                Long id = resultSet.getObject(COLUMN_ID, Long.class);
+                String name = resultSet.getString(COLUMN_NAME);
+                String country = resultSet.getString(COLUMN_COUNTRY);
+                Manufacturer manufacturer = convertToManufacturer(id, name, country);
+                manufacturers.add(manufacturer);
+            }
+        } catch (SQLException e) {
+            throw new DataProcessingException("Can't get all manufacturers", e);
+        }
+        return manufacturers;
     }
 
     @Override
@@ -73,5 +90,13 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
     @Override
     public boolean delete(Long id) {
         return false;
+    }
+
+    private Manufacturer convertToManufacturer(Long id, String name, String country) {
+        Manufacturer manufacturer = new Manufacturer();
+        manufacturer.setId(id);
+        manufacturer.setName(name);
+        manufacturer.setCountry(country);
+        return  manufacturer;
     }
 }
