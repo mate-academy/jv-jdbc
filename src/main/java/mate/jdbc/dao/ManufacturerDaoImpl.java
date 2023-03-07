2fe9ef74f1fc3c508fab6615788bb6f08bd4d359
Nikita Kolesnikov <kit1datasvit@gmail.com>
2023-03-07 20:27:32 +0200

    hw-solution fix mistakes ci

--- src/main/java/mate/jdbc/dao/ManufacturerDaoImpl.java
+++ src/main/java/mate/jdbc/dao/ManufacturerDaoImpl.java
@@ -1,10 +1,13 @@
 package mate.jdbc.dao;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
 import mate.jdbc.exeption.DataProcessingException;
 import mate.jdbc.lib.Dao;
 import mate.jdbc.model.Manufacturer;
