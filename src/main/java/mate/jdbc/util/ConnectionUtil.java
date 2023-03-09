1ebb715dd286a7d4bb7f0cad6698e3e04c7dddb3
Sergiy Golubchenko <galuba1@gmail.com>
2023-03-09 17:26:34 +0200

    add constants to ConnectionUtil

--- src/main/java/mate/jdbc/util/ConnectionUtil.java
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionUtil {
-    private static final  String CONNECTION_PATH = "jdbc:mysql://localhost:3306/taxi_services";
-    private static final  String USER_NAME = "root";
-    private static final  String USER_PASSWORD = "Galuba1234567890";
+    private static final String CONNECTION_PATH = "jdbc:mysql://localhost:3306/taxi_services";
+    private static final String USER_NAME = "root";
+    private static final String USER_PASSWORD = "Galuba1234567890";
 
     static {
         try {
