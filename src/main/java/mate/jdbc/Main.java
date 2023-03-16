4083fc89aacc6d28634e2598e9865ed48a431e85
iskamele <iskamele@gmail.com>
2023-03-16 17:05:44 +0200

    Added case for .update().

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -7,22 +7,51 @@ import mate.jdbc.model.Manufacturer;
 public class Main {
     public static void main(String[] args) {
         ManufacturerDao manufacturerDao = new ManufacturerDaoImpl();
+        /* Test data
+        Manufacturer firstDummyManufacturer = new Manufacturer();
+        firstDummyManufacturer.setId(1L);
+        firstDummyManufacturer.setName("Porsche");
+        firstDummyManufacturer.setCountry("Germany");
+
+        Manufacturer secondDummyManufacturer = new Manufacturer();
+        secondDummyManufacturer.setId(2L);
+        secondDummyManufacturer.setName("Mitsubishi");
+        secondDummyManufacturer.setCountry("Japan");
+
+        Manufacturer thirdDummyManufacturer = new Manufacturer();
+        thirdDummyManufacturer.setId(3L);
+        thirdDummyManufacturer.setName("Zaz");
+        thirdDummyManufacturer.setCountry("Ukraine");
+
+        manufacturerDao.create(firstDummyManufacturer);
+        manufacturerDao.create(secondDummyManufacturer);
+        manufacturerDao.create(thirdDummyManufacturer);
+         */
+
         Manufacturer newManufacturer = new Manufacturer();
         newManufacturer.setName("BMW");
         newManufacturer.setCountry("Germany");
         Manufacturer createdManufacturer = manufacturerDao.create(newManufacturer);
-        System.out.println(".create(): " + createdManufacturer + System.lineSeparator());
+        System.out.println(".create() new: " + createdManufacturer + System.lineSeparator());
 
         Long createdManufacturerId = createdManufacturer.getId();
         Manufacturer gotManufacturerFromDb = manufacturerDao.get(createdManufacturerId).get();
-        System.out.println(".get(): " + gotManufacturerFromDb + System.lineSeparator());
+        System.out.println(".get() by id: " + gotManufacturerFromDb + System.lineSeparator());
 
-        System.out.println(".getAll(): ");
+        System.out.println(".getAll() from DB:");
         manufacturerDao.getAll().forEach(System.out::println);
-        System.out.println(System.lineSeparator());
+
+        Manufacturer updateManufacturer = new Manufacturer();
+        updateManufacturer.setName("Ford");
+        updateManufacturer.setCountry("USA");
+        updateManufacturer.setId(createdManufacturerId);
+        System.out.println(System.lineSeparator() + ".update(): with " + updateManufacturer);
+        System.out.println("Old manufacturer: " + gotManufacturerFromDb);
+        Manufacturer updatedManufacturer = manufacturerDao.update(updateManufacturer);
+        System.out.println("Updated manufacturer: " + updatedManufacturer + System.lineSeparator());
 
         boolean delete = manufacturerDao.delete(createdManufacturerId);
-        System.out.println(".delete(): for " + createdManufacturer
+        System.out.println(".delete(): for " + updatedManufacturer
                 + " is " + delete + System.lineSeparator());
 
         System.out.println(".getAll() after .delete() operation: ");
