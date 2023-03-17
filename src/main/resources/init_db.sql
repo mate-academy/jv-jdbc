64fbe67ef56a93adf70e2230ea279aea17ab545b
Nazar Zvarych <zvarych45@gmail.com>
2023-03-17 15:48:04 +0200

    I create DB taxi_service_db. I create connection to db and implement ManufacturerDao.

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -6,7 +6,6 @@ CREATE TABLE `manufacturers` (
   `name` varchar(255) DEFAULT NULL,
   `country` varchar(255) DEFAULT NULL,
   `is_deleted` tinyint NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8mb3;
+  PRIMARY KEY (`id`));
 
 
