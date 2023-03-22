5060b173462056aaa060d37cb70a5c544fd9e1e2
Nadiia Huryk <nadia11huryk@gmail.com>
2023-03-22 18:46:07 +0200

    JDBC hw solution

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,30 @@
 package mate.jdbc;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.List;
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Manufacturer;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
+    public static void main(String[] args) {
+        Manufacturer firstCar = new Manufacturer();
+        firstCar.setName("Reno");
+        firstCar.setCountry("France");
+        Manufacturer secondCar = new Manufacturer();
+        secondCar.setName("Toyota");
+        secondCar.setCountry("Japan");
+        ManufacturerDao manufacturerDao =
+                (ManufacturerDao) injector.getInstance(ManufacturerDao.class);
+        manufacturerDao.create(firstCar);
+        manufacturerDao.create(secondCar);
+        List<Manufacturer> allManufacturers = manufacturerDao.getAll();
+        allManufacturers.forEach(System.out::println);
+        System.out.println(manufacturerDao.get(firstCar.getId()));
+        secondCar.setName("Mazda");
+        manufacturerDao.update(secondCar);
+        System.out.println(secondCar);
+        manufacturerDao.delete(secondCar.getId());
     }
 }
