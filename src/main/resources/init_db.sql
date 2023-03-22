f9cb177a4933bcecafe434d22b8887a64d6214d6
Nadiia Huryk <nadia11huryk@gmail.com>
2023-03-22 18:49:10 +0200

    JDBC hw solution

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -6,8 +6,3 @@ CREATE TABLE `manufacturers` (
                                  `is_deleted` tinyint NOT NULL DEFAULT '0',
                                  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
-
-
-
-
-
