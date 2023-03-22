a54068a9e8bb944fa4cf44932ea243dbfde42817
Viacheslav Kyslyi <kyslyiviacheslav@gmail.com>
2023-03-22 18:47:54 +0200

    solved tasks 1) Established connection to your Database. 2) Created init_db.sql file in src/main/resources folder. 3)Created Manufacturer model. 4)Created DAO layer for Manufacturer model. 5)In the main method called CRUD methods.

---
+++ src/main/java/mate/jdbc/dao/ManufacturerDaoImpl.java
@@ -0,0 +1,115 @@
+package mate.jdbc.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import mate.jdbc.exception.DataProcessingException;
+import mate.jdbc.lib.Dao;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.util.ConnectionUtil;
+
+@Dao
+public class ManufacturerDaoImpl implements ManufacturerDao {
+    @Override
+    public Manufacturer create(Manufacturer manufacturer) {
+        String createManufacturerQuery = "INSERT INTO manufacturers(name,country) values(?,?)";
+        try (Connection connection = ConnectionUtil.getConnection();
+                 PreparedStatement createManufacturerStatement = connection.prepareStatement(
+                         createManufacturerQuery, Statement.RETURN_GENERATED_KEYS)) {
+            createManufacturerStatement.setString(1,manufacturer.getName());
+            createManufacturerStatement.setString(2,manufacturer.getCountry());
+            createManufacturerStatement.executeUpdate();
+            ResultSet generatedKeys = createManufacturerStatement.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                Long id = generatedKeys.getObject(1, Long.class);
+                manufacturer.setId(id);
+            }
+        } catch (SQLException e) {
+            throw new DataProcessingException("Can't insert data manufacturer "
+                    + manufacturer + " to DB", e);
+        }
+        return manufacturer;
+    }
+
+    @Override
+    public Optional<Manufacturer> get(Long id) {
+        Optional<Manufacturer> optionalManufacturer = Optional.empty();
+        String getQuery = "SELECT * FROM manufacturers WHERE id = " + id
+                + " AND is_deleted = false";
+        try (Connection connection = ConnectionUtil.getConnection();
+                     PreparedStatement getManufacturerStatement =
+                             connection.prepareStatement(getQuery)) {
+            ResultSet resultSet = getManufacturerStatement.executeQuery();
+            if (resultSet.next()) {
+                optionalManufacturer = Optional.of(createManufacturer(resultSet));
+            }
+        } catch (SQLException e) {
+            throw new DataProcessingException("Can't get manufacturer with id = "
+                    + id + " from DB", e);
+        }
+        return optionalManufacturer;
+    }
+
+    @Override
+    public List<Manufacturer> getAll() {
+        List<Manufacturer> manufacturers = new ArrayList<>();
+        String getAllQuery = "SELECT * FROM manufacturers WHERE is_deleted = false";
+        try (Connection connection = ConnectionUtil.getConnection();
+                     PreparedStatement getAllManufacturersStatement =
+                                 connection.prepareStatement(getAllQuery)) {
+            ResultSet resultSet =
+                    getAllManufacturersStatement.executeQuery();
+            while (resultSet.next()) {
+                manufacturers.add(createManufacturer(resultSet));
+            }
+        } catch (SQLException e) {
+            throw new DataProcessingException("Can't get all manufacturers from DB", e);
+        }
+        return manufacturers;
+    }
+
+    @Override
+    public Manufacturer update(Manufacturer manufacturer) {
+        String updateQuery = "UPDATE manufacturers SET name = ? , country = ? "
+                + "WHERE id = ? AND is_deleted = false";
+        try (Connection connection = ConnectionUtil.getConnection();
+                     PreparedStatement updateManufacturerStatement =
+                                connection.prepareStatement(updateQuery)) {
+            updateManufacturerStatement.setString(1,manufacturer.getName());
+            updateManufacturerStatement.setString(2,manufacturer.getCountry());
+            updateManufacturerStatement.setLong(3,manufacturer.getId());
+            return updateManufacturerStatement.executeUpdate() > 0
+                    ? manufacturer : new Manufacturer();
+        } catch (SQLException e) {
+            throw new DataProcessingException("Can't update manufacturer : "
+                    + manufacturer + " in DB", e);
+        }
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        String deleteQuery = "UPDATE manufacturers SET is_deleted = true WHERE id = ?";
+        try (Connection connection = ConnectionUtil.getConnection();
+                     PreparedStatement deleteManufacturerStatement = connection.prepareStatement(
+                             deleteQuery)) {
+            deleteManufacturerStatement.setLong(1,id);
+            return deleteManufacturerStatement.executeUpdate() > 0;
+        } catch (SQLException e) {
+            throw new DataProcessingException("Can't delete manufacturer with id = "
+                    + id + " format to DB", e);
+        }
+    }
+
+    private Manufacturer createManufacturer(ResultSet resultSet) throws SQLException {
+        Manufacturer manufacturer = new Manufacturer();
+        manufacturer.setId(resultSet.getObject("id", Long.class));
+        manufacturer.setName(resultSet.getString("name"));
+        manufacturer.setCountry(resultSet.getString("country"));
+        return manufacturer;
+    }
+}
---
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -0,0 +1,33 @@
+package mate.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionUtil {
+
+    private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/taxi_service_db";
+    private static final String USER_NAME = "root";
+    private static final String PASSWORD = "1111";
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Can't load JDBC driver for MySQL",e);
+        }
+    }
+
+    public static Connection getConnection() {
+        try {
+            Properties dbProperties = new Properties();
+            dbProperties.put("user", USER_NAME);
+            dbProperties.put("password", PASSWORD);
+            return DriverManager.getConnection(DATABASE_URL,
+                    dbProperties);
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't create connection to DB.", e);
+        }
+    }
+}
