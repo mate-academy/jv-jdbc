62edac2b344ebee8455af3491e65368d043c7703
Mariana Polishchuk <tonksmaryan@gmail.com>
2023-03-23 11:51:08 +0200

    [HW-JDBC] add infrastructure for Database connection

---
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -0,0 +1,33 @@
+package mate.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionUtil {
+    private static final String JDBC_DRIVER_PATH = "com.mysql.cj.jdbc.Driver";
+    private static final String USER_NAME = "root";
+    private static final String USER_PASSWORD = "vfhmzirf3434";
+    private static final String CONNECTION_URL =
+            "jdbc:mysql://localhost:3306/taxi_services?useSSL=false";
+
+    static {
+        try {
+            Class.forName(JDBC_DRIVER_PATH);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Can't load JDBC driver for MySQL", e);
+        }
+    }
+
+    public static Connection getConnection() {
+        try {
+            Properties dbProperties = new Properties();
+            dbProperties.put("user", USER_NAME);
+            dbProperties.put("password", USER_PASSWORD);
+            return DriverManager.getConnection(CONNECTION_URL, dbProperties);
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't create connection to DB", e);
+        }
+    }
+}
