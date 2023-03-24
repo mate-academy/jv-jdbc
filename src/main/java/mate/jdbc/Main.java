fab2f9b6f8309686ffb7b051b5149abe76e37e6d
Olexandr Leshtaiev <leshtaev13.a@gmail.com>
2023-03-24 12:11:09 +0200

    fixed issues2

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -2,7 +2,6 @@ package mate.jdbc;
 
 import java.util.List;
 import mate.jdbc.dao.ManufacturerDao;
-import mate.jdbc.dao.ManufacturerDaoImpl;
 import mate.jdbc.lib.Injector;
 import mate.jdbc.model.Manufacturer;
 
