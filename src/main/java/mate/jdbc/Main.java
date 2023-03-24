ab7da6e402652f596a6d3fdba809b3a5c04e440d
Ditkovskyi Pasha <pashkadit@gmail.com>
2023-03-24 18:14:38 +0200

    after student review

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,15 +1,32 @@
 package mate.jdbc;
 
 import mate.jdbc.dao.ManufacturerDao;
-import mate.jdbc.dao.ManufacturerDaoImpl;
 import mate.jdbc.lib.Injector;
+import mate.jdbc.models.Manufacturer;
 
 public class Main {
     private static final Injector injector = Injector.getInstance("mate.jdbc");
 
     public static void main(String[] args) {
         ManufacturerDao manufacturerDao = (ManufacturerDao)
-                injector.getInstance(ManufacturerDaoImpl.class);
+                injector.getInstance(ManufacturerDao.class);
+
+        Manufacturer testManufacturer = new Manufacturer();
+        testManufacturer.setName("test");
+        testManufacturer.setCountry("Ukraine");
+
+        System.out.println(manufacturerDao.create(testManufacturer));
+
+        testManufacturer.setCountry("USA");
+        System.out.println(manufacturerDao.create(testManufacturer));
+
+        System.out.println(manufacturerDao.get(1L));
+
+        testManufacturer.setId(1L);
+        System.out.println(manufacturerDao.update(testManufacturer));
+
+        System.out.println(manufacturerDao.delete(1L));
+
         manufacturerDao.getAll().forEach(System.out::println);
     }
 }
