3fdc2c51faa2a5a4344b8c38559f465870b0fd0b
Ilya Samilyak <samilyakilya@gmail.com>
2023-03-27 17:11:54 +0300

    HW-jdbc-2

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -8,13 +8,13 @@ public class Main {
     private static final Injector injector = Injector.getInstance("mate.jdbc");
 
     public static void main(String[] args) {
-        ManufacturerDao manufacturerDao = (ManufacturerDao) injector
-                .getInstance(ManufacturerDao.class);
         Manufacturer manufacturer = new Manufacturer();
         manufacturer.setName("Toyota");
         manufacturer.setCountry("Ukraine");
         manufacturer.setName("BMW");
         manufacturer.setCountry("Germany");
+        ManufacturerDao manufacturerDao = (ManufacturerDao) injector
+                .getInstance(ManufacturerDao.class);
         manufacturerDao.delete(2L);
         manufacturerDao.create(manufacturer);
         System.out.println(manufacturerDao.get(4L));
