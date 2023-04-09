6ae9f03cd55faec51759fb629a950169ad10e764
Oleksandr Rolik <rolik.alexx@gmail.com>
2023-04-09 13:27:58 +0300

    fix table name

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE `car_db` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
-CREATE TABLE `car_db` (
+CREATE TABLE `car` (
     `id` bigint NOT NULL AUTO_INCREMENT,
     `name` varchar(255) NOT NULL,
     `country` varchar(255) NOT NULL,
@@ -7,5 +7,3 @@ CREATE TABLE `car_db` (
      PRIMARY KEY (`id`,`name`,`country`),
      UNIQUE KEY `id_UNIQUE` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb3;
-
-
