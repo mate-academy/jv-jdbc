6242533f8249db1345e384c21d16d9380c033e80
oleksandr-petrunovskyi <102808605+oleksandr-petrunovskyi@users.noreply.github.com>
2023-04-15 09:06:27 +0300

    ConnectionUtil constants added

--- src/main/java/mate/jdbc/util/ConnectionUtil.java
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -6,9 +6,14 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionUtil {
+    private static final String URL = "jdbc:mysql://localhost:3306/taxi_service";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "Dtc3mvb6h_";
+    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+
     static {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName(JDBC_DRIVER);
         } catch (ClassNotFoundException e) {
             throw new RuntimeException("Can’t load JDBC driver for MySQL", e);
         }
@@ -17,12 +22,11 @@ public class ConnectionUtil {
     public static Connection getConnection() {
         try {
             Properties dbProperties = new Properties();
-            dbProperties.put("user", "root");
-            dbProperties.put("password", "Dtc3mvb6h_");
-            return DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/taxi_service", dbProperties);
+            dbProperties.put("user", USERNAME);
+            dbProperties.put("password", PASSWORD);
+            return DriverManager.getConnection(URL, dbProperties);
         } catch (SQLException e) {
-            throw new RuntimeException("Can’t createthe connection from DB", e);
+            throw new RuntimeException("Can’t create the connection from DB", e);
         }
     }
 }
