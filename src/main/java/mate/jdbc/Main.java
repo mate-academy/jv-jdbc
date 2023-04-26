7d61707f404b7b0d7356ee0496433338dbfb55bd
Ihor Sydorenko <sidorenko.igorek@gmail.com>
2023-04-26 22:32:21 +0300

    valid solution

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,22 @@
 package mate.jdbc;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Manufacturer;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+    private static ManufacturerDao manufacturerDao =
+            (ManufacturerDao) injector.getInstance(ManufacturerDao.class);
+
+    public static void main(String[] args) {
+        Manufacturer manufacturer = new Manufacturer();
+        manufacturer.setId(6L);
+        manufacturer.setName("Ford");
+        manufacturer.setCountry("USA(2)");
+        System.out.println(manufacturerDao.get(5L));
+        manufacturerDao.getAll().forEach(System.out::println);
+        System.out.println(manufacturerDao.update(manufacturer));
+        System.out.println(manufacturerDao.delete(manufacturer.getId()));
     }
 }
