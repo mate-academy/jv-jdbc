39c45d1b74cab32b52da70cb0dc48ac48c6004f4
antonsablev <live.incntr@gmail.com>
2023-05-01 15:51:37 +0200

    My implementation of ManufacturerDaoImpl

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,31 @@
 package mate.jdbc;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.List;
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Manufacturer;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
+    public static void main(String[] args) {
+        ManufacturerDao manufacturerDao =
+                (ManufacturerDao) injector.getInstance(ManufacturerDao.class);
+        Manufacturer mercedes = new Manufacturer("Mercedes", "Germany");
+        manufacturerDao.create(mercedes);
+        Manufacturer toyota = new Manufacturer("Toyota", "Japan");
+        manufacturerDao.create(toyota);
+        Manufacturer hyundai = new Manufacturer("Hyundai", "Korea");
+        manufacturerDao.create(hyundai);
+        Manufacturer manufacturerFromDb = manufacturerDao.get(2L).get();
+        System.out.println(manufacturerFromDb);
+        manufacturerFromDb.setName("Audi");
+        manufacturerFromDb.setCountry("Germany");
+        manufacturerDao.update(manufacturerFromDb);
+        manufacturerDao.delete(3L);
+        List<Manufacturer> manufacturersList = manufacturerDao.getAll();
+        for (Manufacturer manufacturer : manufacturersList) {
+            System.out.println(manufacturer);
+        }
     }
 }
