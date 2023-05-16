1a17604f4087de39fe47db44e5a3bdaea7a88dad
Oleksii Parkhomenko <kcas.pilot@gmail.com>
2023-05-16 22:57:25 +0300

    jv-jdbc hw first solution

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,33 @@
 package mate.jdbc;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Manufacturer;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
+    public static void main(String[] args) {
+        ManufacturerDao manufacturerDao =
+                (ManufacturerDao) injector.getInstance(ManufacturerDao.class);
+        Manufacturer fiat = new Manufacturer();
+        fiat.setName("Fiat");
+        fiat.setCountry("Italy");
+        System.out.println(manufacturerDao.create(fiat));
+        Manufacturer audi = new Manufacturer();
+        audi.setName("Audi");
+        audi.setCountry("Germany");
+        System.out.println(manufacturerDao.create(audi));
+        System.out.println(audi.equals(manufacturerDao.get(audi.getId()).get()));
+        manufacturerDao.getAll().forEach(System.out::println);
+        Manufacturer manufacturer =
+                manufacturerDao.getAll().stream()
+                        .filter(m -> m.getName().equals("Audi"))
+                        .findFirst()
+                        .get();
+        manufacturer.setName("Audi Q7");
+        System.out.println(manufacturerDao.update(manufacturer));
+        System.out.println(manufacturerDao.delete(manufacturer.getId()));
+        manufacturerDao.getAll().forEach(System.out::println);
     }
 }
