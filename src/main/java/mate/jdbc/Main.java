75da323ce6b0979a28c72f2a920c85e9fce0f655
Stas Zlishchev <zlishchev@gmail.com>
2023-05-17 16:18:24 +0300

    completed task

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,32 @@
 package mate.jdbc;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.List;
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.models.Manufacturer;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
+    public static void main(String[] args) {
+        ManufacturerDao manufacturerDao = (ManufacturerDao) injector
+                .getInstance(ManufacturerDao.class);
+        printAll(manufacturerDao);
+        manufacturerDao.delete(10L);
+        printAll(manufacturerDao);
+        Manufacturer volkswagen = new Manufacturer("Volkswagen", "Germany");
+        manufacturerDao.create(volkswagen);
+        printAll(manufacturerDao);
+        Manufacturer manufacturerFromDB = manufacturerDao.get(11L).get();
+        System.out.println("Manufacturer from DB by id: " + manufacturerFromDB);
+        Manufacturer toyota = new Manufacturer(13L, "Toyota", "Japan");
+        manufacturerDao.update(toyota);
+        printAll(manufacturerDao);
+
+    }
+
+    private static void printAll(ManufacturerDao manufacturerDao) {
+        List<Manufacturer> allManufacturers = manufacturerDao.getAll();
+        allManufacturers.forEach(System.out::println);
     }
 }
