13d0efd82d7c29fb9083e2eff1e1949f31d5c576
Nikita Ostrenskyi <nikostrik2003@gmail.com>
2023-05-20 11:50:54 +0300

    tested methods in the Main class

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,29 @@
 package mate.jdbc;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Manufacturer;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
+    public static void main(String[] args) {
+        Manufacturer manufacturer = new Manufacturer();
+        manufacturer.setName("Hyundai");
+        manufacturer.setCountry("Ukraine");
+
+        Manufacturer updatedManufacturer = new Manufacturer();
+        updatedManufacturer.setId(1L);
+        updatedManufacturer.setName("Audi");
+        updatedManufacturer.setCountry("Germany");
+
+        ManufacturerDao manufacturerDao = (ManufacturerDao) injector
+                .getInstance(ManufacturerDao.class);
+        manufacturerDao.create(manufacturer);
+        manufacturerDao.update(updatedManufacturer);
+        manufacturerDao.getAll()
+                .forEach(System.out::println);
+        System.out.println(manufacturerDao.get(2L));
+        System.out.println(manufacturerDao.delete(3L));
     }
 }
