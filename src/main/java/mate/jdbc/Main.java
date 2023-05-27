50ef4358891076d1506f53947a938baa0eeaaf7c
Max Skory <devisrush@gmail.com>
2023-05-28 01:03:21 +0300

    checkstyle was fixed

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,19 +1,20 @@
 package mate.jdbc;
 
+import java.util.List;
 import mate.jdbc.dao.ManufacturerDao;
 import mate.jdbc.lib.Injector;
 import mate.jdbc.models.Manufacturer;
-
-import java.util.List;
 
 public class Main {
     private static final Injector injector = Injector.getInstance("mate.jdbc");
 
     public static void main(String[] args) {
-        List<Manufacturer> manufacturers = List.of(new Manufacturer().setName("Mazda").setCountry("Japan"),
+        List<Manufacturer> manufacturers = List.of(
+                new Manufacturer().setName("Mazda").setCountry("Japan"),
                 new Manufacturer().setName("Ford").setCountry("USA"),
                 new Manufacturer().setName("Mercedes").setCountry("Germany"));
-        ManufacturerDao manufacturerDao = (ManufacturerDao) injector.getInstance(ManufacturerDao.class);
+        ManufacturerDao manufacturerDao =
+                (ManufacturerDao) injector.getInstance(ManufacturerDao.class);
         manufacturers.forEach(manufacturerDao::create);
         Manufacturer manufacturerFromDb = manufacturerDao.get(manufacturers.get(0).getId())
                 .orElseGet(Manufacturer::new);
--- src/main/java/mate/jdbc/dao/ManufacturerDaoImpl.java
+++ src/main/java/mate/jdbc/dao/ManufacturerDaoImpl.java
@@ -1,16 +1,19 @@
 package mate.jdbc.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import mate.jdbc.exceptions.DataProcessingException;
 import mate.jdbc.lib.Dao;
 import mate.jdbc.models.Manufacturer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.ConnectionUtil;
-
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
 @Dao
 public class ManufacturerDaoImpl implements ManufacturerDao {
@@ -20,7 +23,7 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
         String sqlRequest = "SELECT * FROM manufacturers WHERE is_deleted = FALSE;";
         List<Manufacturer> manufacturers = new ArrayList<>();
         try (Connection connection = ConnectionUtil.createConnection();
-             PreparedStatement statement = connection.prepareStatement(sqlRequest)) {
+                 PreparedStatement statement = connection.prepareStatement(sqlRequest)) {
             ResultSet resultSet = statement.executeQuery();
             while (resultSet.next()) {
                 Long id = resultSet.getObject("id", Long.class);
@@ -37,7 +40,7 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
     public Optional<Manufacturer> get(Long id) {
         String sqlRequest = "SELECT * FROM manufacturers WHERE id = ? AND is_deleted = FALSE;";
         try (Connection connection = ConnectionUtil.createConnection();
-             PreparedStatement statement = connection.prepareStatement(sqlRequest)) {
+                 PreparedStatement statement = connection.prepareStatement(sqlRequest)) {
             statement.setObject(1, id);
             ResultSet resultSet = statement.executeQuery();
             if (resultSet.next()) {
@@ -46,7 +49,8 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
                 return Optional.of(new Manufacturer().setId(id).setName(name).setCountry(country));
             }
         } catch (SQLException e) {
-            throw new DataProcessingException("Can't retrieve record with id " + id + " form DB", e);
+            throw new DataProcessingException("Can't retrieve record with id "
+                    + id + " form DB", e);
         }
         logger.info("There is no record for id {} in the database", id);
         return Optional.empty();
@@ -55,7 +59,8 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
     public Manufacturer create(Manufacturer manufacturer) {
         String sqlRequest = "INSERT INTO manufacturers(name, country) values(?,?)";
         try (Connection connection = ConnectionUtil.createConnection();
-             PreparedStatement statement = connection.prepareStatement(sqlRequest, Statement.RETURN_GENERATED_KEYS)) {
+                 PreparedStatement statement = connection.prepareStatement(sqlRequest,
+                         Statement.RETURN_GENERATED_KEYS)) {
             statement.setString(1, manufacturer.getName());
             statement.setString(2, manufacturer.getCountry());
             statement.executeUpdate();
@@ -72,7 +77,7 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
     public Manufacturer update(Manufacturer manufacturer) {
         String sqlRequest = "UPDATE manufacturers SET name = ?, country = ? WHERE id = ?;";
         try (Connection connection = ConnectionUtil.createConnection();
-             PreparedStatement statement = connection.prepareStatement(sqlRequest)) {
+                 PreparedStatement statement = connection.prepareStatement(sqlRequest)) {
             statement.setString(1, manufacturer.getName());
             statement.setString(2, manufacturer.getCountry());
             statement.setObject(3, manufacturer.getId());
@@ -89,7 +94,7 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
     public boolean delete(Long id) {
         String sqlRequest = "UPDATE manufacturers SET is_deleted = TRUE WHERE id = ?;";
         try (Connection connection = ConnectionUtil.createConnection();
-             PreparedStatement statement = connection.prepareStatement(sqlRequest)) {
+                 PreparedStatement statement = connection.prepareStatement(sqlRequest)) {
             statement.setObject(1, id);
             if (statement.executeUpdate() != 1) {
                 logger.info("There is no record with id {} in the database", id);
