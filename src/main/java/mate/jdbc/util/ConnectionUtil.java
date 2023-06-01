f5c2d922eb8cfe418325fe4544f1952da6be8c7a
Ivan Yuriv <yuriv1024@gmail.com>
2023-06-01 19:39:49 +0300

    Project structure was created. Connection to database was implemented.

---
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -0,0 +1,29 @@
+package mate.jdbc.util;
+
+import mate.jdbc.exception.DataProcessingException;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionUtil {
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new DataProcessingException("Can`t load JDBC driver for MySQL", e);
+        }
+    }
+
+    public static Connection getConnection() {
+        try {
+            Properties dbProperties = new Properties();
+            dbProperties.put("user", "root");
+            dbProperties.put("password", "1234567890");
+            return DriverManager.getConnection("jdbc:mysql://localhost:3306/taxi_service_db", dbProperties);
+        } catch (SQLException e) {
+            throw new DataProcessingException("Can`t create connection to DB", e);
+        }
+    }
+}
