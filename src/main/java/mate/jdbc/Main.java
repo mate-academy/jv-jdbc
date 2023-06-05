0d3913784c15f2f114fd59921c153d6114a89001
Vladyslav Krutyus <kijdron@gmail.com>
2023-06-05 11:47:02 +0300

    ManufacturerDao implemented

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,31 @@
 package mate.jdbc;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.List;
+import java.util.Optional;
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Manufacturer;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+    private static final ManufacturerDao manufacturerDao =
+            (ManufacturerDao) injector.getInstance(ManufacturerDao.class);
+
+    public static void main(String[] args) {
+        Manufacturer newManufacturer = new Manufacturer("Nissan", "Japan");
+        Manufacturer createdManufacturer = manufacturerDao.create(newManufacturer);
+        System.out.println(createdManufacturer);
+
+        boolean delete = manufacturerDao.delete(2L);
+        System.out.println(delete);
+
+        Optional<Manufacturer> manufacturer = manufacturerDao.get(1L);
+        manufacturer.ifPresent(System.out::println);
+
+        List<Manufacturer> manufacturers = manufacturerDao.getAll();
+        manufacturers.forEach(System.out::println);
+
+        Manufacturer updatedManufacturer = manufacturerDao.update(createdManufacturer);
+        System.out.println(updatedManufacturer);
     }
 }
