b85ce998938ab2436cc65141653b1475a31ac8e9
Mykyta Udovycehnko <nikitaudav123@gmail.com>
2023-06-09 18:18:21 +0300

    improve solution jv-jdbc 2

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -8,17 +8,17 @@ public class Main {
     private static final Injector injector = Injector.getInstance("mate.jdbc");
 
     public static void main(String[] args) {
-        Long get_index = 3L;
-        Long delete_index = 5L;
         ManufacturerDao manufacturerDao = (ManufacturerDao) injector
                 .getInstance(ManufacturerDao.class);
         Manufacturer manufacturer = new Manufacturer("Volkswagen", "Germany");
         System.out.println(manufacturerDao.create(manufacturer));
         manufacturer.setName("Toyota");
         manufacturer.setCountry("Japan");
+        Long getId = 3L;
+        Long deleteId = 5L;
         System.out.println(manufacturerDao.update(manufacturer));
-        System.out.println(manufacturerDao.get(get_index));
-        System.out.println(manufacturerDao.delete(delete_index));
+        System.out.println(manufacturerDao.get(getId));
+        System.out.println(manufacturerDao.delete(deleteId));
         System.out.println(manufacturerDao.getAll());
     }
 }
