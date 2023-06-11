1bb26628a38511872ca2954434331940b57128ff
Mykyta Naumov <nneed4use@gmail.com>
2023-06-11 18:56:59 +0300

    main() method works correctly, you may not believe but looks like everything ok wow :o

--- src/main/java/mate/jdbc/util/ConnectionUtil.java
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -9,7 +9,7 @@ public class ConnectionUtil {
     private static final String CLASS_NAME = "com.mysql.cj.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3306/taxi_service_db";
     private static final String USER_NAME = "root";
-    private static final String PASSWORD = "285582KOBEin";
+    private static final String PASSWORD = "qwerty12345";
 
     static {
         try {
