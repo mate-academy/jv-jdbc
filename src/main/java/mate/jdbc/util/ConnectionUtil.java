f26f65baeefe51d939c05cf44f984e04ed36ea8a
Volodymyr Davidov <volodymyrdavidov1@gmail.com>
2023-06-12 09:54:24 +0300

    my implementation hw jv-jdbc

---
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -0,0 +1,28 @@
+package mate.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionUtil {
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Can`t load JDBC driver for MySQL", e);
+        }
+    }
+
+    public static Connection getConnection() {
+        Properties dbProperties = new Properties();
+        String connectionUrl = "jdbc:mysql://localhost:3306/taxi_db";
+        dbProperties.put("user", "root");
+        dbProperties.put("password", "000000");
+        try {
+            return DriverManager.getConnection(connectionUrl, dbProperties);
+        } catch (SQLException throwables) {
+            throw new RuntimeException("Can`t create connections to manufacturers", throwables);
+        }
+    }
+}
