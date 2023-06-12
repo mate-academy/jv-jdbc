a827db5adc27d334fd9bc4d3cbec88bf77436d82
Taras Machyta <taras.machyta.sa@gmail.com>
2023-06-13 01:48:04 +0300

    Solution jdbc

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,41 @@
 package mate.jdbc;
 
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Manufacturer;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
 
+        ManufacturerDao manufacturerDao = (ManufacturerDao)
+                injector.getInstance(ManufacturerDao.class);
+        // getAll list
+        manufacturerDao.getAll().forEach(System.out::println);
+        Manufacturer manufacturer = new Manufacturer();
+        System.out.println("------------");
+        // create new
+        manufacturer.setName("Lamborghini");
+        manufacturer.setCountry("Italy");
+        Manufacturer savedManufacturer = manufacturerDao.create(manufacturer);
+        System.out.println(savedManufacturer);
+        System.out.println("------------");
+        manufacturerDao.getAll().forEach(System.out::println);
+        System.out.println("------------");
+        // update
+        manufacturer.setName("Mercedes-Benz");
+        manufacturer.setId(4L);
+        manufacturer.setCountry("Germany");
+        Manufacturer updateManufacturer = manufacturerDao.update(manufacturer);
+        System.out.println(updateManufacturer);
+        System.out.println("------------");
+        manufacturerDao.getAll().forEach(System.out::println);
+        System.out.println("------------");
+        // delete
+        System.out.println(manufacturerDao.delete(savedManufacturer.getId()));
+        System.out.println("---------");
+        // get all
+        manufacturerDao.getAll().forEach(System.out::println);
     }
 }
---
+++ src/main/java/mate/jdbc/resourses/init.db.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE `taxi_service_db` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+CREATE TABLE `manufacturers` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `country` varchar(45) NOT NULL,
+  `is_deleted` tinyint NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb3;
+
