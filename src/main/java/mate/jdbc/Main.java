370ea42c3ea1feebee10f51bd94e5abc53e39110
Artem <cr7realartem@gmail.com>
2023-06-14 14:58:13 +0300

    done

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,34 @@
 package mate.jdbc;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Manufacturer;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
+    public static void main(String[] args) {
+        ManufacturerDao manufacturerDao = (ManufacturerDao)
+                injector.getInstance(ManufacturerDao.class);
+        System.out.println(manufacturerDao.getAll());
+        //create first car
+        Manufacturer manufacturer = new Manufacturer("mercedes-benz", "Germany");
+        System.out.println(manufacturerDao.create(manufacturer));
+        //create second car
+        Manufacturer manufacturer2 = new Manufacturer("fiat", "italy");
+        System.out.println(manufacturerDao.create(manufacturer2));
+        //get all manufacturers
+        System.out.println(manufacturerDao.getAll());
+        //update name for first car
+        manufacturer.setName("volkswagen");
+        manufacturerDao.update(manufacturer);
+        //get all manufacturers
+        System.out.println(manufacturerDao.getAll());;
+        //get manufacturer by id
+        System.out.println(manufacturerDao.get(manufacturer.getId()));
+        //delete manufacturer by id
+        System.out.println(manufacturerDao.delete(manufacturer.getId()));
+        //get all manufacturers
+        System.out.println(manufacturerDao.getAll());
     }
 }
