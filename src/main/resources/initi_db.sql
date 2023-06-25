19072ec93c24499850af66b81ec08f63634c348d
Gagadu <bobimyko@gmail.com>
2023-06-25 23:01:37 +0300

    init file changes done

--- src/main/resources/initi_db.sql
+++ src/main/resources/initi_db.sql
@@ -2,6 +2,6 @@ CREATE TABLE `manufacturers` (
                                  `id` int NOT NULL AUTO_INCREMENT,
                                  `name` varchar(45) DEFAULT NULL,
                                  `country` varchar(45) DEFAULT NULL,
-                                 `isDeleted` tinyint DEFAULT '0',
+                                 `is_deleted` tinyint DEFAULT '0',
                                  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb3
