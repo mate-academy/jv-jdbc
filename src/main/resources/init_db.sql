fd652424cd25e891434018f8cd52cf3bad092b29
pochtalon <pochtalon1010@gmail.com>
2023-07-05 16:36:21 +0300

    Created folder resources and init_db.sql file; Created custom exception; Created Manufacturer class; Created package dao, interface ManufacturerDao and class ManufacturerDaoImpl; Created package util and class ConnectionUtil; Implemented methods in ManufacturerDaoImpl.

---
+++ src/main/resources/init_db.sql
@@ -0,0 +1,9 @@
+CREATE SCHEMA `taxi_db` DEFAULT CHARACTER SET utf8 ;
+
+CREATE TABLE `manufacturers` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `country` varchar(45) DEFAULT NULL,
+  `is_deleted` tinyint DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3
