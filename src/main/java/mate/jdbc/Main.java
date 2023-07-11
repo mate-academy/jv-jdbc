3a9d1c4d3109d32ff89123801fd51f731e2efd05
Oleg Ravlyk <oleg.ravlyk2005@gmail.com>
2023-07-11 04:03:38 +0300

    added connection to db

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,21 @@
 package mate.jdbc;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class Main {
     public static void main(String[] args) {
-
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Properties db = new Properties();
+            db.put("user", "root");
+            db.put("password", "PASSWORD HERE");
+            DriverManager.getConnection("jdbc:mysql://localhost:3306/library_db", db);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Can't load JDBC driver", e);
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't get connection", e);
+        }
     }
 }
