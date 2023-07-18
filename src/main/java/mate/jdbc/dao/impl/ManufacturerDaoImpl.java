1fc558958b80b87c015eece52366843f3692e236
Anton Uzhva <anton.itec91@gmail.com>
2023-07-18 18:49:35 +0300

    Intial commit

---
+++ src/main/java/mate/jdbc/dao/impl/ManufacturerDaoImpl.java
@@ -0,0 +1,33 @@
+package mate.jdbc.dao.impl;
+
+import java.util.List;
+import java.util.Optional;
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.model.Manufacturer;
+
+public class ManufacturerDaoImpl  implements ManufacturerDao {
+    @Override
+    public Manufacturer create(Manufacturer manufacturer) {
+        return null;
+    }
+
+    @Override
+    public Optional<Manufacturer> get(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Manufacturer> getAll() {
+        return null;
+    }
+
+    @Override
+    public Manufacturer update(Manufacturer manufacturer) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return false;
+    }
+}
---
+++ src/main/java/mate/jdbc/utility/ConnectionUtil.java
@@ -0,0 +1,27 @@
+package mate.jdbc.utility;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionUtil {
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Connection getConnection() {
+        Properties dbProperties = new Properties();
+        dbProperties.put("user", "root");
+        dbProperties.put("password", "matebest");
+        try {
+            return DriverManager.getConnection("jdbc:mysql://localhost:3306/library_db");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
