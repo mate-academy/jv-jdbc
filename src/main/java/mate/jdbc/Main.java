5c50190ee939b84bdef1def81a0dc07a409f5e9e
Bakhmet Ivan <bahmetvanya1290@gmail.com>
2023-07-20 20:28:46 +0300

    completed task

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,25 @@
 package mate.jdbc;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Manufacturer;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
+    public static void main(String[] args) {
+        ManufacturerDao manufacturerDao = (ManufacturerDao) injector.getInstance(ManufacturerDao.class);
+        Manufacturer ford = new Manufacturer("Ford", "USA");
+        Manufacturer mustang = new Manufacturer("Mustang", "USA");
+        Manufacturer hyundai = new Manufacturer("Hyundai", "Korea");
+        ford = manufacturerDao.create(ford);
+        mustang = manufacturerDao.create(mustang);
+        hyundai = manufacturerDao.create(hyundai);
+        System.out.println(manufacturerDao.getAll());
+        System.out.println(manufacturerDao.get(ford.getId()));
+        System.out.println(manufacturerDao.delete(mustang.getId()));
+        System.out.println(manufacturerDao.get(mustang.getId()));
+        hyundai.setCountry("Japan");
+        System.out.println(manufacturerDao.update(hyundai));
     }
 }
