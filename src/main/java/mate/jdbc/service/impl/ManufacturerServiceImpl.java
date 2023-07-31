11a94a385e1c112335ca86dfea4491c40ec0a4b7
Volodymyr Mykychak <leadervlo.d@gmail.com>
2023-07-31 16:45:49 +0300

    commit

--- src/main/java/mate/jdbc/service/impl/ManufacturerServiceImpl.java
+++ src/main/java/mate/jdbc/service/impl/ManufacturerServiceImpl.java
@@ -1,12 +1,18 @@
 package mate.jdbc.service.impl;
 
+import lombok.extern.log4j.Log4j2;
 import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Dao;
+import mate.jdbc.lib.Inject;
 import mate.jdbc.model.Manufacturer;
 import mate.jdbc.service.ManufacturerService;
 import mate.jdbc.service.ManufacturerValidationService;
 
 import java.util.List;
 
+@Dao
+@Inject
+@Log4j2
 public class ManufacturerServiceImpl implements ManufacturerService {
     ManufacturerDao manufacturerDao;
     ManufacturerValidationService validationService;
