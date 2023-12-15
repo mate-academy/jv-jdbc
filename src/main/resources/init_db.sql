27dae0deba72d7226e2d0acf994d943362201ad2
Anton Kurylo <antonkurilo9@gmail.com>
2023-12-15 21:21:50 +0200

    added changes to init_db.sql

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,9 +1,11 @@
-CREATE DATABASE `taxi_service_db` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE `taxi_service_db`;
 
-CREATE TABLE `manufacturers` (
-                                 `id` bigint NOT NULL AUTO_INCREMENT,
-                                 `name` varchar(255) NOT NULL,
-                                 `country` varchar(255) NOT NULL,
-                                 `is_deleted` tinyint NOT NULL DEFAULT '0',
-                                 PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+CREATE TABLE `manufacturers`
+(
+    `id`         bigint       NOT NULL AUTO_INCREMENT,
+    `name`       varchar(255) NOT NULL,
+    `country`    varchar(255) NOT NULL,
+    `is_deleted` tinyint      NOT NULL DEFAULT '0',
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb3;
