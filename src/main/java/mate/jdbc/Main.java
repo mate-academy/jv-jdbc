f958e7e13c0b405010760241aacda1b43759b9f6
Sergii Nosachenko <54940595+sergii-nosachenko@users.noreply.github.com>
2025-04-17 15:16:21 +0300

    Update Main.java

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -6,21 +6,4 @@ import mate.jdbc.lib.Injector;
 import mate.jdbc.model.Manufacturer;
 
 public class Main {
-    private static final Injector INJECTOR = Injector.getInstance("mate.jdbc");
-
-    public static void main(String[] args) {
-        ManufacturerDao manufacturerDao = (ManufacturerDao)
-                INJECTOR.getInstance(ManufacturerDao.class);
-        Manufacturer manufacturerReno = manufacturerDao
-                .create(new Manufacturer("Reno", "France"));
-        Manufacturer manufacturerBmw = manufacturerDao
-                .create(new Manufacturer("BMW", "Germany"));
-        Manufacturer manufacturerByd = manufacturerDao
-                .create(new Manufacturer("BYD", "China"));
-        manufacturerReno.setName("Dacia");
-        manufacturerDao.update(manufacturerReno);
-        manufacturerDao.delete(manufacturerBmw.getId());
-        List<Manufacturer> manufacturerList = manufacturerDao.getAll();
-        manufacturerList.forEach(System.out::println);
-    }
 }
